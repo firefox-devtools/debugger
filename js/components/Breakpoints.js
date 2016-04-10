df9d685b010583944c0d9be08506a3e5aac7b99d
Natalie Coley <nataliecoley@Natalies-MacBook-Pro.local>
2016-04-10 15:51:59 -0400

    Update breakpoint styles

--- js/components/Breakpoints.js
+++ js/components/Breakpoints.js
@@ -1,24 +1,40 @@
 const React = require("react");
 const dom = React.DOM;
+const { connect } = require("react-redux");
+const { getSources } = require("../queries");
 
-function Breakpoints({ breakpoints }) {
+require("./Breakpoints.css");
+require("../lib/variables.css");
 
-  function onResumeClick() {
-    console.log('click')
-    gThreadClient.resume();
-  }
+const Breakpoints = React.createClass({
 
-  return dom.div(
-    {},
-    dom.button({ onClick: onResumeClick }, 'resume'),
-    dom.ul(
-      null,
-      breakpoints.map(bp => dom.li(
+  render() {
+    function onResumeClick() {
+      console.log('click')
+      gThreadClient.resume();
+    }
+
+    function getFilenameFromSources(sources, actor) {
+      var source = sources[actor];
+      var url = new URL(source.url);
+      var filename = url.pathname.substring(url.pathname.lastIndexOf('/')+1);
+      return filename;
+    }
+
+    return dom.div(
+      {className: 'breakpoints'},
+      dom.button({ onClick: onResumeClick }, 'resume'),
+      dom.ul(
         null,
-        bp.location.actor + ": " + bp.location.line
-      ))
-    )
-  );
-}
+        this.props.breakpoints.map(bp => dom.li(
+          null,
+          getFilenameFromSources(this.props.sources, bp.location.actor) + ", line " + bp.location.line
+        ))
+      )
+    );
+  }
+})
 
-module.exports = Breakpoints;
+module.exports = connect(
+  (state, props) => ({ sources: getSources(state)})
+)(Breakpoints);
