62ba60458fee0bd62f3e27def11f0c8699c2802c
Jason Laster <jlaster@mozilla.com>
2016-04-12 16:02:27 -0400

    Add selectable sources

--- js/components/Sources.js
+++ js/components/Sources.js
@@ -1,25 +1,58 @@
 "use strict";
 
 const React = require("react");
+const {DOM: dom} = React;
 const { bindActionCreators } = require("redux");
 const { connect } = require("react-redux");
 const actions = require("../actions");
-const dom = React.DOM;
-
+const { getSelectedSource } = require("../queries");
 require("./Sources.css");
 
-function Sources({ sources, selectSource }) {
+function renderSource({source, selectSource, selectedSource}) {
+  const pathname = (new URL(source.get("url"))).pathname;
+  const isSelected = (selectedSource &&
+                      selectedSource.actor == source.get("actor"))
+                      ? "selected" : "";
+
+  return dom.li(
+    { onClick: () => selectSource(source.toJS()),
+      className: "source-item " + isSelected,
+      style: { paddingLeft: "40px" },
+      key: source.get("url")},
+    pathname
+  );
+}
+
+/**
+ * Takes a sources object indexed by actor and
+ * returns a sources object indexed by source domain.
+ *
+ * @returns Object
+ */
+function groupSourcesByDomain(sources) {
+  return sources.valueSeq()
+    .filter(source => !!source.get("url"))
+    .groupBy(source => (new URL(source.get("url"))).hostname);
+}
+
+function Sources({ sources, selectSource, selectedSource }) {
+  const sourcesByDomain = groupSourcesByDomain(sources);
+
   return dom.ul(
-    {className: "sources-pane"},
-    sources.valueSeq().map(source => {
-      return dom.li({ onClick: () => selectSource(source.toJS()),
-                      className: "sources-pane-item" },
-                    source.get("url") || source.get("introductionType"));
+    {className: "sources-list"},
+    sourcesByDomain.map((domainSources, domain) => {
+      return dom.li({ className: "domain" },
+        dom.span({style: { paddingLeft: "20px" }}, domain),
+        dom.ul(null,
+          domainSources.map(source => renderSource({
+            source, selectSource, selectedSource }))
+        )
+      );
     })
   );
 }
 
 module.exports = connect(
-  null,
+  state => ({ selectedSource: getSelectedSource(state) }),
   dispatch => bindActionCreators(actions, dispatch)
 )(Sources);
