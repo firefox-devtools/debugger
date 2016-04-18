727239815c501fb0e13dc11cb3435b8dd9fcdad4
Jason Laster <jlaster@mozilla.com>
2016-04-18 15:24:03 -0400

    Fix chrome param destructuring bug

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,14 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
+let environment;
+try {
+  environment = require("./environment");
+} catch (e) {
+  environment = {};
+}
+
+let config = {
   entry: "./js/main.js",
   devtool: "source-map",
   output: {
@@ -34,3 +41,18 @@ module.exports = {
     new ExtractTextPlugin("styles.css")
   ]
 };
+
+// NOTE: This is only needed to fix a bug with chrome devtools' debugger and
+// destructuring params https://github.com/jlongster/debugger.html/issues/67
+if (environment.transformParameters) {
+  config.module.loaders.push({
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: "babel",
+    query: {
+      plugins: ["transform-es2015-parameters"]
+    }
+  });
+}
+
+module.exports = config;
