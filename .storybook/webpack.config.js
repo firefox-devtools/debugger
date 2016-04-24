28bc6d4f9c0d519b56433750199413ff9d1b7259
Jason Laster <jlaster@mozilla.com>
2016-04-24 19:03:56 -0400

    Add storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  resolve: {
+    alias: {
+      "devtools": "ff-devtools-libs",
+      "sdk": "ff-devtools-libs/sdk"
+    },
+    extensions: ["", ".js", ".jsm"],
+    root: path.join(__dirname, "node_modules")
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  }
+};
---
+++ js/components/stories/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+require("./accordion");
+require("./Breakpoints");
+require("./editor");
+require("./SplitBox");
