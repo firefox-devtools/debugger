1bbf38ab1ec742cd46826655100681ddc5f71b8c
Jason Laster <jlaster@mozilla.com>
2016-05-16 14:40:19 -0400

    Start debugging chrome tabs
    This is the first step towards debugging a chrome tab.
    
    + Pulls in the chrome api layer, which is a lot like our
    client/shared/main.js
    + adds a debugTab to the chrome client for debugging a tab. It knows how
    to start the inspector backend, connect the websocket, and enable the
    debugger and runtime.

--- public/js/clients/chrome.js
+++ public/js/clients/chrome.js
@@ -1,5 +1,18 @@
 "use strict";
 
+const {
+  WebSocketConnection,
+  generateCommands
+} = require("./chrome/api");
+
+const protocol = require("./chrome/protocol.json");
+
+let connectionAgents;
+
+function getAgent(name) {
+  return connectionAgents[name];
+}
+
 function presentTabs(tabs) {
   const blacklist = ["New Tab", "Inspectable pages"];
 
@@ -28,6 +41,53 @@ function chromeTabs(callback) {
   });
 }
 
+function onConnection(connection) {
+  const ws = connection._socket;
+  connectionAgents = connection._agents;
+  ws.onopen = function() {};
+
+  ws.onmessage = function(e) {
+    console.log("response: ", e.data);
+  };
+
+  const debuggerAgent = getAgent("Debugger");
+  debuggerAgent.enable();
+  debuggerAgent.setPauseOnExceptions({ state: "none" });
+  debuggerAgent.setAsyncCallStackDepth({ maxDepth: 0 });
+
+  const runtimeAgent = getAgent("Runtime");
+  runtimeAgent.enable();
+  runtimeAgent.run();
+}
+
+function debugTab(tab) {
+  function toTitleCase() {
+    return this.substring(0, 1).toUpperCase() + this.substring(1);
+  }
+
+  // NOTE: this is a serious performance issue.
+  // If we decide to productionize the chrome debugger,
+  // we will want to build the bootstrap commands in advance require them in.
+
+  // NOTE: toTitleCase is temporarily monkey patched onto String.prototype
+  // to conform to what generateCommands expects. I
+  // If String.prototype.toTitleCase is defined somwhere else,
+  // it will be replaced.
+  String.prototype.toTitleCase = toTitleCase; /* eslint no-extend-native: 0 */
+  let code = generateCommands(protocol);
+  delete String.prototype.toTitleCase;
+  eval(code); /* eslint no-eval: 0 */
+
+  WebSocketConnection.Create(
+    tab.chrome.webSocketDebuggerUrl,
+    onConnection
+  );
+}
+
+window.debugTab = debugTab;
+
 module.exports = {
-  chromeTabs
+  chromeTabs,
+  debugTab,
+  getAgent
 };
