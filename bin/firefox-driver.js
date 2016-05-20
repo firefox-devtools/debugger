1f61815998df5e9ffbfab6bd5b809504f27fdbe4
Jason Laster <jlaster@mozilla.com>
2016-05-20 09:09:53 -0400

    start todomvc

--- bin/firefox-driver.js
+++ bin/firefox-driver.js
@@ -36,7 +36,7 @@ function start() {
 
 if (shouldStart) {
   const driver = start();
-  driver.get("http://localhost:9002/todomvc/examples/backbone/");
+  driver.get("http://todomvc.com/examples/backbone/");
   setInterval(() => {}, 100);
 }
 
