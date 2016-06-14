82b700a2048c8a587ecda517475376502a350e8a
Jason Laster <jlaster@mozilla.com>
2016-06-14 11:58:09 -0400

    fix storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,8 +5,9 @@ const path = require("path");
 module.exports = {
   resolve: {
     alias: {
-      "devtools": "ff-devtools-libs",
-      "sdk": "ff-devtools-libs/sdk"
+      "devtools": path.join(__dirname, "./public/js/lib/devtools"),
+      "devtools-sham": path.join(__dirname, "./public/js/lib/devtools-sham"),
+      "sdk": path.join(__dirname, "./public/js/lib/devtools-sham/sdk")
     },
     extensions: ["", ".js", ".jsm"],
     root: path.join(__dirname, "node_modules")
