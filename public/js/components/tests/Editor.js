f3fa6f5e05f461a11e83eac98d1ad2d98ea33072
Jason Laster <jlaster@mozilla.com>
2016-06-14 07:58:14 -0400

    Fix linting issues

--- public/js/components/tests/Editor.js
+++ public/js/components/tests/Editor.js
@@ -1,21 +1,19 @@
 "use strict";
 
 if (typeof window == "object") {
+  const Editor = require("../Editor");
+  const { renderComponent } = require("../test-utils");
 
-const Editor = require("../Editor");
-const { renderComponent } = require("../test-utils");
+  function getEditorLines($el) {
+    return $el.querySelectorAll(".codemirror-line");
+  }
 
-function getEditorLines($el) {
-  return $el.querySelectorAll(".codemirror-line");
-}
-
-describe("Editor", function() {
-  it("todomvc", function() {
-    const $el = renderComponent(Editor, "todomvc");
-    const lines = getEditorLines($el);
-    expect(lines.length).to.equal(46);
-    expect(lines[2].innerText).to.equal("var app = app || {};");
+  describe("Editor", function() {
+    it("todomvc", function() {
+      const $el = renderComponent(Editor, "todomvc");
+      const lines = getEditorLines($el);
+      expect(lines.length).to.equal(46);
+      expect(lines[2].innerText).to.equal("var app = app || {};");
+    });
   });
-});
-
 }
