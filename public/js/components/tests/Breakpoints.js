ccd6920322d828b43deb759c31d7fb216128ecc9
Bryan Clark <clarkbw@gmail.com>
2016-07-06 12:11:07 -0700

    Style disabled breakpoints in the list (#287)
    * add disabled breakpoints styling
    
    * also add a yet unused paused class
    
    * fix lint errors, add tests, update fixtures
    
    css linting errors have been fixed
    removed superfluous true attribute
    added additional tests for Breakpoints
    updated the fixtures with cypress
    
    * update fixtures and frames test
    
    regenerate the latest fixtures
    change frames length values to pass test

--- public/js/components/tests/Breakpoints.js
+++ public/js/components/tests/Breakpoints.js
@@ -11,6 +11,10 @@ function getBreakpointLabel($breakpoint) {
   return $breakpoint.querySelector(".breakpoint-label").innerText;
 }
 
+function getBreakpointClasses($breakpoint) {
+  return Array.from($breakpoint.classList);
+}
+
 describe("Breakpoint Component", function() {
   it("Not Paused", function() {
     if (typeof window != "object") {
@@ -21,14 +25,20 @@ describe("Breakpoint Component", function() {
     expect($el.innerText).to.equal("No Breakpoints");
   });
 
-  it("1 Breakpoint", function() {
+  it("3 Breakpoints", function() {
     if (typeof window != "object") {
       return;
     }
 
     const $el = renderComponent(Breakpoints, "todomvcUpdateOnEnter");
     const breakpoints = getBreakpoints($el);
-    expect(breakpoints.length).to.equal(1);
+    expect(breakpoints.length).to.equal(3);
     expect(getBreakpointLabel(breakpoints[0])).to.equal("113 this.close();");
+    expect(getBreakpointClasses(breakpoints[0])).to.contain("paused");
+    expect(getBreakpointLabel(breakpoints[1])).to
+      .equal("119 revertOnEscape: function (e) {");
+    expect(getBreakpointLabel(breakpoints[2])).to
+      .equal("121 this.$el.removeClass('editing'...");
+    expect(getBreakpointClasses(breakpoints[2])).to.contain("disabled");
   });
 });
