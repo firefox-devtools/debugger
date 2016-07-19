e10e60c18a5238beeacb2631b240d275e12f63c6
Jason Laster <jlaster@mozilla.com>
2016-07-19 16:24:24 -0400

    fix UI unit tests

--- public/js/test/utils/renderComponentFromFixture.js
+++ public/js/test/utils/renderComponentFromFixture.js
@@ -5,7 +5,7 @@ const { combineReducers } = require("redux");
 const dehydrate = require("../../util/dehydrate-state");
 
 const fixtures = require("../fixtures");
-const configureStore = require("../../util/create-store");
+const { configureStore } = require("../../util/create-store");
 const reducers = require("../../reducers");
 
 function createStore(state = {}) {
