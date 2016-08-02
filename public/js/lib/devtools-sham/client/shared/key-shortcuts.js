02816fe74bd4c547ae2bdd94d44d2e7d4aaee2c1
Jason Laster <jlaster@mozilla.com>
2016-08-02 12:05:16 -0400

    A couple of key-shortcut fixes to make it work

--- public/js/lib/devtools-sham/client/shared/key-shortcuts.js
+++ public/js/lib/devtools-sham/client/shared/key-shortcuts.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-const Services = require("Services");
+const { Services } = require("Services");
 const EventEmitter = require("devtools/shared/event-emitter");
 const isOSX = Services.appinfo.OS === "Darwin";
 
