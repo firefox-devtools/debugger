37b6c5bbcc4ec1d4fbd0e5a3849f8f07ace546e2
Jason Laster <jlaster@mozilla.com>
2016-08-11 14:38:42 -0400

    Replace karma w/ firefox driver
    This PR replaces the complicated karma overhead with a simple firefox
    driver approach that loads the mocha server page and reads the results.
    
    We can always expand the output by reading the test results or setting
    up a separate reporter, but for now this gives us guidance on whether
    the unit tests are running successfully.
    
    Leaving Karma:
    
    * we've already concluded that we are not looking to test specific
       browser apis
    * karma introduces a lot of overhead, which is unecessary for what we
      are trying to do.

--- bin/firefox-driver.js
+++ bin/firefox-driver.js
@@ -5,9 +5,17 @@ const firefox = require("selenium-webdriver/firefox");
 const By = webdriver.By;
 const until = webdriver.until;
 const Key = webdriver.Key;
+const minimist = require("minimist");
 
-const shouldStart = process.argv.indexOf("--start") > 0;
-const useWebSocket = process.argv.indexOf("--websocket") > 0;
+const args = minimist(process.argv.slice(2), {
+  boolean: "start",
+  boolean: "tests",
+  boolean: "websocket"
+});
+
+const shouldStart = args.start;
+const isTests = args.tests;
+const useWebSocket = args.websocket;
 
 function firefoxBinary() {
   let binary = new firefox.Binary();
@@ -41,6 +49,23 @@ if (shouldStart) {
   const driver = start();
   driver.get("http://localhost:7999/todomvc");
   setInterval(() => {}, 100);
+}
+
+function getResults(driver) {
+  driver
+    .findElement(By.id("mocha-stats"))
+    .getText().then(results => {
+      console.log("results ", results);
+      const match = results.match(/failures: (\d*)/);
+      const resultCode = parseInt(match[1], 10) > 0 ? 1 : 0;
+      process.exit(resultCode);
+    });
+}
+
+if (isTests) {
+  const driver = start();
+  driver.get("http://localhost:8003");
+  setTimeout(() => getResults(driver), 5000);
 }
 
 module.exports = { start, By, Key, until };
