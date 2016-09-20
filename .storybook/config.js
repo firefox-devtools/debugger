5320c837511496fb90ddf28f13a6ef7d3ae13c9e
Jason Laster <jlaster@mozilla.com>
2016-09-20 19:13:36 -0400

    Upgrade Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,8 @@
-"use strict";
-
 const { configure } = require("@kadira/storybook");
 const { setConfig } = require("../config/feature");
 
 require("../public/js/lib/themes/light-theme.css");
-
 setConfig(DebuggerConfig);
-
 
 function loadStories() {
   require("../public/js/components/stories");
