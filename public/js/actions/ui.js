49f7de8bc1a4ae437012a54ff0631146542251c8
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2016-10-26 09:29:08 -0600

    Add a test for the ui reducer.

--- public/js/actions/ui.js
+++ public/js/actions/ui.js
@@ -1,8 +1,9 @@
 // @flow
 const constants = require("../constants");
+import type { ThunkArgs } from "./types";
 
 function toggleFileSearch(searchOn: boolean) {
-  return ({ dispatch }) => {
+  return ({ dispatch }: ThunkArgs) => {
     dispatch({
       type: constants.TOGGLE_FILE_SEARCH,
       searchOn
