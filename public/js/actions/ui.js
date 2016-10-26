45c59ea566de338f8d0b21b77c7ebf121d7ef784
Jason Laster <jason.laster.11@gmail.com>
2016-10-26 12:38:38 -0400

    small tweaks

--- public/js/actions/ui.js
+++ public/js/actions/ui.js
@@ -1,13 +1,10 @@
 // @flow
 const constants = require("../constants");
-import type { ThunkArgs } from "./types";
 
 function toggleFileSearch(searchOn: boolean) {
-  return ({ dispatch }: ThunkArgs) => {
-    dispatch({
-      type: constants.TOGGLE_FILE_SEARCH,
-      searchOn
-    });
+  return {
+    type: constants.TOGGLE_FILE_SEARCH,
+    searchOn
   };
 }
 
