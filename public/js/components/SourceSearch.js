c4a17322c7c3f389c186e7cadb281a23b7b6196f
Jacob J. Zhang <jzhang@movableink.com>
2016-10-28 15:52:14 -0400

    combining file search filters into one

--- public/js/components/SourceSearch.js
+++ public/js/components/SourceSearch.js
@@ -23,7 +23,7 @@ function searchResults(sources) {
   }
 
   return sources.valueSeq()
-    .filter(source => !isPretty(source.toJS()))
+    .filter(source => !isPretty(source.toJS() || source.get("url")))
     .filter(source => source.get("url"))
     .map(source => ({
       value: getSourcePath(source),
