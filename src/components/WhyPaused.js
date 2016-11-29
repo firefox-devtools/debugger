a7815e4d8b06725ed2109d86a3783bb405735a59
Bryan Clark <clarkbw@gmail.com>
2016-11-29 08:03:00 -0800

    A decent start to this

---
+++ src/components/WhyPaused.js
@@ -0,0 +1,50 @@
+const React = require("react");
+const { bindActionCreators } = require("redux");
+const { connect } = require("react-redux");
+const ImPropTypes = require("react-immutable-proptypes");
+const actions = require("../actions");
+const { getPause } = require("../selectors");
+const { DOM: dom } = React;
+
+require("./WhyPaused.css");
+
+function getPauseReason(pauseInfo) {
+  if (!pauseInfo) {
+    return null;
+  }
+
+  let reasonType = pauseInfo.getIn(["why"]).get("type");
+  if (!reasons[reasonType]) {
+    console.log("reasonType", reasonType);
+  }
+  return reasons[reasonType];
+}
+
+const reasons = {
+  "debuggerStatement": "Paused on a debugger; statement in the source code",
+  "breakpoint": "Paused on a breakpoint"
+};
+
+const WhyPaused = React.createClass({
+  propTypes: {
+    pauseInfo: ImPropTypes.map
+  },
+
+  displayName: "WhyPaused",
+
+  render() {
+    const { pauseInfo } = this.props;
+    const reason = getPauseReason(pauseInfo);
+
+    return pauseInfo ?
+      dom.div({ className: "pane why-paused" }, reason)
+      : null;
+  }
+});
+
+module.exports = connect(
+  state => ({
+    pauseInfo: getPause(state)
+  }),
+  dispatch => bindActionCreators(actions, dispatch)
+)(WhyPaused);
