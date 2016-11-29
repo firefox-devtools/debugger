5dce42db24b8016e864b957f3d6f8e1acf7f615f
I-Chun Liu <arthur801031@gmail.com>
2016-11-29 12:15:31 +0800

    Try fixing failed tests

--- src/components/EditorSearchBar.js
+++ src/components/EditorSearchBar.js
@@ -70,15 +70,15 @@ const EditorSearchBar = React.createClass({
 
     if (sourceText && sourceText.get("text") &&
       ((selectedSource != prevProps.selectedSource) ||
-      this.shouldLoad)) {
+      this.context.shouldLoad)) {
       const query = this.state.query;
       const count = countMatches(query, sourceText.get("text"));
       // eslint-disable-next-line react/no-did-update-set-state
       this.setState({ count: count, index: 0 });
-      this.shouldLoad = false;
+      this.context.shouldLoad = false;
       this.search(query);
     } else if (selectedSource != prevProps.selectedSource) {
-      this.shouldLoad = true;
+      this.context.shouldLoad = true;
     }
   },
 
