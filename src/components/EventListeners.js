4eb530434fa89dbbf26073c4c7a8e8a8b4972e8f
Jason Laster <jason.laster.11@gmail.com>
2016-12-04 14:04:26 -0500

    add checkbox

--- src/components/EventListeners.js
+++ src/components/EventListeners.js
@@ -5,34 +5,84 @@ const { bindActionCreators } = require("redux");
 const { connect } = require("react-redux");
 const ImPropTypes = require("react-immutable-proptypes");
 const actions = require("../actions");
-const { getEventListeners } = require("../selectors");
+const { getEventListeners, getBreakpoint } = require("../selectors");
+const CloseButton = require("./CloseButton");
 
 require("./EventListeners.css");
 
-function renderListener({ type, selector, line, sourceId }, selectSource) {
-  return dom.div({
-    className: "listener",
-    onClick: () => selectSource(sourceId, { line }),
-    key: `${type}`
-  },
-    dom.span({ className: "type" }, type),
-    dom.span({ className: "selector" }, selector),
-  );
+function EventListeners({ selectSource, enableBreakpoint, disableBreakpoint, listeners }) {
 }
 
-function EventListeners({ selectSource, listeners }) {
-  return dom.div({
-    className: "pane event-listeners"
+const EventListeners = React.createClass({
+  propTypes: {
+    listeners: PropTypes.array
   },
-    listeners.map((l) => renderListener(l, selectSource))
-  );
-}
 
-EventListeners.propTypes = {
-  listeners: PropTypes.array
-};
+  displayName: "EventListeners",
+
+  renderListener({ type, selector, line, sourceId, breakpoint }) {
+    const checked = breakpoint && !breakpoint.disabled;
+    const location = { sourceId, line };
+
+    return dom.div({
+      className: "listener",
+      onClick: () => this.props.selectSource(sourceId, { line }),
+      key: `${type}`
+    },
+      dom.input({
+        type: "checkbox",
+        className: "listener-checkbox",
+        checked,
+        onChange: () => this.handleCheckbox(breakpoint, location),
+      }),
+      dom.span({ className: "type" }, type),
+      dom.span({ className: "selector" }, selector),
+      breakpoint ?
+      CloseButton({
+        handleClick: (ev) => this.removeBreakpoint(ev, breakpoint)
+      }) : ""
+    );
+  },
+
+  handleCheckbox(breakpoint, location) {
+    if (!breakpoint) {
+      return this.props.addBreakpoint(location);
+    }
+
+    if (breakpoint.loading) {
+      return;
+    }
+
+    if (breakpoint.disabled) {
+      this.props.enableBreakpoint(breakpoint.location);
+    } else {
+      this.props.disableBreakpoint(breakpoint.location);
+    }
+  },
+
+  removeBreakpoint(event, breakpoint) {
+    event.stopPropagation();
+    this.props.removeBreakpoint(breakpoint.location);
+  },
+
+  render() {
+    const { listeners } = this.props;
+    return dom.div({
+      className: "pane event-listeners"
+    },
+      listeners.map(this.renderListener)
+    );
+  }
+});
 
 module.exports = connect(
-  state => ({ listeners: getEventListeners(state) }),
+  state => {
+    const listeners = getEventListeners(state)
+      .map(l => Object.assign({}, l, {
+        breakpoint: getBreakpoint(state, { sourceId: l.sourceId, line: l.line })
+      }));
+
+    return { listeners };
+  },
   dispatch => bindActionCreators(actions, dispatch)
 )(EventListeners);
