fa9aa3c1968fbf3cca73522e3eec6f64dd32d7cd
Bryan Clark <clarkbw@gmail.com>
2017-01-12 16:09:18 +0800

    missed Autocomplete using Button/Close

--- src/components/shared/Autocomplete.js
+++ src/components/shared/Autocomplete.js
@@ -3,7 +3,7 @@ const { DOM: dom, PropTypes } = React;
 const { filter } = require("fuzzaldrin-plus");
 const classnames = require("classnames");
 const Svg = require("./Svg");
-const CloseButton = require("../shared/Button/Close");
+const CloseButton = require("./Button/Close");
 
 require("./Autocomplete.css");
 
