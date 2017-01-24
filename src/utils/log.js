272f18a65dfcee8bb7e75eaa5344460f9413a240
Stefan Khan-Kernahan <skhanker@gmail.com>
2017-01-23 21:05:45 -0800

    add a doc (#1819)

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,7 +1,24 @@
 /* @flow */
 
+/**
+ *
+ * Utils for logging to the console
+ * Suppresses logging in non-development environment
+ *
+ * @module utils/log
+ */
+
 const { isDevelopment } = require("devtools-config");
 
+/**
+ * Produces a formatted console log line by imploding args, prefixed by [log]
+ *
+ * function input: log(["hello", "world"])
+ * console output: [log] hello world
+ *
+ * @memberof utils/log
+ * @static
+ */
 function log(...args: any[]) {
   if (!isDevelopment()) {
     return;
