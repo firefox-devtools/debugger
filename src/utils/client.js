64049da8fba54da0bbdcf60fda1628f88af2e4bb
Yury Delendik <ydelendik@mozilla.com>
2017-01-26 17:22:37 -0600

    Reconfigures Firefox to disable asm.js AOT for debuggger.

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,7 +1,7 @@
 const { createSource, firefox } = require("devtools-client-adapters");
 const { prefs } = require("./prefs");
 
-function onFirefoxConnect(actions) {
+async function onFirefoxConnect(actions) {
   const tabTarget = firefox.getTabTarget();
   const threadClient = firefox.getThreadClient();
 
@@ -12,25 +12,26 @@ function onFirefoxConnect(actions) {
   tabTarget.on("will-navigate", actions.willNavigate);
   tabTarget.on("navigate", actions.navigated);
 
+  await threadClient.reconfigure({ observeAsmJS: true });
+
   // In Firefox, we need to initially request all of the sources. This
   // usually fires off individual `newSource` notifications as the
   // debugger finds them, but there may be existing sources already in
   // the debugger (if it's paused already, or if loading the page from
   // bfcache) so explicity fire `newSource` events for all returned
   // sources.
-  return threadClient.getSources().then(({ sources }) => {
-    actions.newSources(sources.map(createSource));
+  const { sources } = await threadClient.getSources();
+  actions.newSources(sources.map(createSource));
 
-    // If the threadClient is already paused, make sure to show a
-    // paused state.
-    const pausedPacket = threadClient.getLastPausePacket();
-    if (pausedPacket) {
-      firefox.clientEvents.paused(null, pausedPacket);
-    }
-  });
+  // If the threadClient is already paused, make sure to show a
+  // paused state.
+  const pausedPacket = threadClient.getLastPausePacket();
+  if (pausedPacket) {
+    firefox.clientEvents.paused(null, pausedPacket);
+  }
 }
 
-function onConnect(connection, actions) {
+async function onConnect(connection, actions) {
   // NOTE: the landing page does not connect to a JS process
   if (!connection) {
     return;
