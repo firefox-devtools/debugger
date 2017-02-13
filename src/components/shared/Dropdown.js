a7434ac66d78168277bbf56eef3831eab0de5f3d
princiya <princiya777@yahoo.co.in>
2017-02-13 11:43:57 +0100

    dropdown button reverted

--- src/components/shared/Dropdown.js
+++ src/components/shared/Dropdown.js
@@ -1,6 +1,5 @@
 const React = require("react");
 const { DOM: dom, PropTypes } = React;
-const Svg = require("./Svg");
 require("./Dropdown.css");
 
 const Dropdown = React.createClass({
@@ -34,12 +33,12 @@ const Dropdown = React.createClass({
   },
 
   renderButton() {
-    return dom.div(
+    return dom.button(
       {
         className: "dropdown-button",
         onClick: this.toggleDropdown
       },
-      Svg("fastForward")
+      ">>"
     );
   },
 
