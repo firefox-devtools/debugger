2a7463df7a8fc0aa649d5ee006a52456d3829b8b
Irfan Hudda <irfanhudda@gmail.com>
2017-02-13 21:21:48 +0530

    Fix lint errors

--- src/components/shared/ObjectInspector.js
+++ src/components/shared/ObjectInspector.js
@@ -64,12 +64,11 @@ function isDefault(item) {
 function sortProperties(properties) {
   return properties.sort((a, b) => {
     // Sort numbers in ascending order and sort strings lexicographically
-    const aInt = parseInt(a), bInt = parseInt(b);
+    const aInt = parseInt(a, 10), bInt = parseInt(b, 10);
     if (isNaN(aInt) || isNaN(bInt)) {
       return a > b ? 1 : -1;
-    } else {
-      return aInt - bInt;
     }
+    return aInt - bInt;
   });
 }
 
