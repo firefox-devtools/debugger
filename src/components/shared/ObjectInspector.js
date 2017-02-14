4970d871e9d7928621dd0b2acfc7a2ec965a6b63
Jason Laster <jason.laster.11@gmail.com>
2017-02-14 17:31:28 -0500

    extract helpers

--- src/components/shared/ObjectInspector.js
+++ src/components/shared/ObjectInspector.js
@@ -5,9 +5,18 @@ const Svg = require("./Svg");
 const Rep = require("./Rep");
 const { MODE } = require("devtools-reps");
 
-const { DOM: dom, PropTypes } = React;
+const {
+  nodeHasChildren,
+  nodeIsOptimizedOut,
+  nodeIsMissingArguments,
+  nodeHasProperties,
+  nodeIsPrimitive,
+  isDefault,
+  makeNodesForProperties,
+  createNode
+} = require("../../utils/object-inspector");
 
-const WINDOW_PROPERTIES = Object.getOwnPropertyNames(window);
+const { DOM: dom, PropTypes } = React;
 
 // This implements a component that renders an interactive inspector
 // for looking at JavaScript objects. It expects descriptions of
@@ -35,77 +44,6 @@ const WINDOW_PROPERTIES = Object.getOwnPropertyNames(window);
 // object that has properties that should be children when they are
 // fetched, and a primitive value that should be displayed with no
 // children.
-
-function nodeHasChildren(item) {
-  return Array.isArray(item.contents);
-}
-
-function nodeIsOptimizedOut(item) {
-  return !nodeHasChildren(item) && item.contents.value.optimizedOut === true;
-}
-
-function nodeIsMissingArguments(item) {
-  return !nodeHasChildren(item) &&
-    item.contents.value.missingArguments === true;
-}
-
-function nodeHasProperties(item) {
-  return !nodeHasChildren(item) && item.contents.value.type === "object";
-}
-
-function nodeIsPrimitive(item) {
-  return !nodeHasChildren(item) && !nodeHasProperties(item);
-}
-
-function isDefault(item) {
-  return WINDOW_PROPERTIES.includes(item.name);
-}
-
-function sortProperties(properties) {
-  return properties.sort((a, b) => {
-    // Sort numbers in ascending order and sort strings lexicographically
-    const aInt = parseInt(a, 10);
-    const bInt = parseInt(b, 10);
-
-    if (isNaN(aInt) || isNaN(bInt)) {
-      return a > b ? 1 : -1;
-    }
-
-    return aInt - bInt;
-  });
-}
-
-function makeNodesForProperties(objProps, parentPath) {
-  const { ownProperties, prototype } = objProps;
-
-  const nodes = sortProperties(Object.keys(ownProperties)).filter(name => {
-    // Ignore non-concrete values like getters and setters
-    // for now by making sure we have a value.
-    return "value" in ownProperties[name];
-  }).map(name => {
-    return createNode(name, `${parentPath}/${name}`, ownProperties[name]);
-  });
-
-  // Add the prototype if it exists and is not null
-  if (prototype && prototype.type !== "null") {
-    nodes.push(createNode(
-      "__proto__",
-      `${parentPath}/__proto__`,
-      { value: prototype }
-    ));
-  }
-
-  return nodes;
-}
-
-function createNode(name, path, contents) {
-  // The path is important to uniquely identify the item in the entire
-  // tree. This helps debugging & optimizes React's rendering of large
-  // lists. The path will be separated by property name,
-  // i.e. `{ foo: { bar: { baz: 5 }}}` will have a path of `foo/bar/baz`
-  // for the inner object.
-  return { name, path, contents };
-}
 
 const ObjectInspector = React.createClass({
   propTypes: {
