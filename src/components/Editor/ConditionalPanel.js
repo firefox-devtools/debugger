c101e647666790f7ec0ef49688f928d84ea6fe1f
Ofer Segev <ofers@wix.com>
2017-02-15 21:55:45 +0200

    Add Conditional Breakpoint Panel guillemet (issue 1946) + add ability to exit breakpoint mode using keyboard. TODO: add guillemet icon

--- src/components/Editor/ConditionalPanel.js
+++ src/components/Editor/ConditionalPanel.js
@@ -1,6 +1,7 @@
 // @flow
 const React = require("react");
 const { DOM: dom } = React;
+const Svg = require("../shared/Svg");
 
 const ReactDOM = require("react-dom");
 
@@ -9,24 +10,33 @@ function renderConditionalPanel({ condition, closePanel, setBreakpoint }:
   let panel = document.createElement("div");
 
   function onKey(e: SyntheticKeyboardEvent) {
-    if (e.key != "Enter") {
-      return;
-    }
-
-    if (e.target && e.target.value) {
-      setBreakpoint(e.target.value);
-      closePanel();
+    switch (e.key) {
+      case "Enter":
+        if (e.target && e.target.value) {
+          setBreakpoint(e.target.value);
+          closePanel();
+        }
+        break;
+      case "Escape":
+        closePanel();
+        break;
     }
   }
+
+  const inputA = dom.input({
+    defaultValue: condition,
+    placeholder: L10N.getStr("editor.conditionalPanel.placeholder"),
+    onKeyDown: onKey.bind(this)
+  });
 
   ReactDOM.render(
     dom.div(
       { className: "conditional-breakpoint-panel" },
-      dom.input({
-        defaultValue: condition,
-        placeholder: L10N.getStr("editor.conditionalPanel.placeholder"),
-        onKeyPress: onKey
-      })
+      Svg('guillemet'),
+      inputA,
+      dom.a({ onClick: closePanel }, L10N.getStr("editor.conditionalPanel.cancel"))
+      // TODO: add 'set' link, and add a state for input and callback for clicking this link.
+      // dom.a({}, L10N.getStr("editor.conditionalPanel.setBreakpoint"))
     ),
     panel
   );
