3ad220c060749517a59c84d7a688bc601da99f7e
Jason Laster <jason.laster.11@gmail.com>
2017-02-16 17:27:42 -0500

    fix history indentation

--- src/utils/redux/middleware/history.js
+++ src/utils/redux/middleware/history.js
@@ -9,14 +9,15 @@ import type { ThunkArgs } from "../../../actions/types";
  * in logging object. Should only be used for tests, as it collects all
  * action information, which will cause memory bloat.
  */
-exports.history = (log: Object[] = []) => ({ dispatch, getState }: ThunkArgs) => {
-  if (isDevelopment()) {
-    console.warn("Using history middleware stores all actions in state for " +
+exports.history = (log: Object[] = []) =>
+  ({ dispatch, getState }: ThunkArgs) => {
+    if (isDevelopment()) {
+      console.warn("Using history middleware stores all actions in state for " +
                   "testing and devtools is not currently running in test " +
                   "mode. Be sure this is intentional.");
-  }
-  return (next: Function) => (action: Object) => {
-    log.push(action);
-    next(action);
+    }
+    return (next: Function) => (action: Object) => {
+      log.push(action);
+      next(action);
+    };
   };
-};
