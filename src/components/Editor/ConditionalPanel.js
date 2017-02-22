f2d21db269ade2aeb82ce524bf2e4447e30b8faa
Andre Alves Garzia <andre@andregarzia.com>
2017-02-22 23:19:38 +0000

    issue(1946): Added guillemet to conditional breakpoint panel

--- src/components/Editor/ConditionalPanel.js
+++ src/components/Editor/ConditionalPanel.js
@@ -4,6 +4,9 @@ const { DOM: dom } = React;
 
 const ReactDOM = require("react-dom");
 
+require("./ConditionalPanel.css");
+
+
 function renderConditionalPanel({ condition, closePanel, setBreakpoint }:
   { condition: boolean, closePanel: Function, setBreakpoint: Function }) {
   let panel = document.createElement("div");
@@ -22,6 +25,10 @@ function renderConditionalPanel({ condition, closePanel, setBreakpoint }:
   ReactDOM.render(
     dom.div(
       { className: "conditional-breakpoint-panel" },
+      dom.div({
+        className: "prompt",
+        dangerouslySetInnerHTML: { __html: "&raquo;" }
+      }),
       dom.input({
         defaultValue: condition,
         placeholder: L10N.getStr("editor.conditionalPanel.placeholder"),
