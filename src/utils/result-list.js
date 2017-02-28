e541dfb14b96881c54205b745ac806cbbe6ec277
Jason Laster <jason.laster.11@gmail.com>
2017-02-28 10:53:53 -0500

    add chrome scroll polyfill (#2202)

--- src/utils/result-list.js
+++ src/utils/result-list.js
@@ -1,6 +1,29 @@
+const { isFirefox } = require("devtools-config");
+
 function scrollList(resultList, index) {
   const resultEl = resultList[index];
-  resultEl.scrollIntoView({ block: "end", behavior: "smooth" });
+
+  if (isFirefox()) {
+    resultEl.scrollIntoView({ block: "end", behavior: "smooth" });
+  } else {
+    chromeScrollList(resultEl, index);
+  }
+}
+
+function chromeScrollList(elem, index) {
+  const resultsEl = elem.parentNode;
+  if (!resultsEl || resultsEl.children.length === 0) {
+    return;
+  }
+
+  const resultsHeight = resultsEl.clientHeight;
+  const itemHeight = resultsEl.children[0].clientHeight;
+  const numVisible = resultsHeight / itemHeight;
+  const positionsToScroll = index - numVisible + 1;
+  const itemOffset = resultsHeight % itemHeight;
+  const scroll = positionsToScroll * (itemHeight + 2) + itemOffset;
+
+  resultsEl.scrollTop = Math.max(0, scroll);
 }
 
 function handleKeyDown(e: SyntheticKeyboardEvent) {
