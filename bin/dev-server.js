af91086b14192e8ea4519a209b198f3325173092
Jason Laster <jason.laster.11@gmail.com>
2017-03-10 04:49:03 -0500

    kill mocha unit test runner

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -4,14 +4,12 @@ const path = require("path");
 const toolbox = require("devtools-launchpad/index");
 const feature = require("devtools-config");
 const getConfig = require("./getConfig");
-const { mochaWebpackConfig, startMochaServer } = require("./mocha-server")
 const express = require("express");
 
 const envConfig = getConfig();
 feature.setConfig(envConfig);
 
 let webpackConfig = require("../webpack.config");
-webpackConfig = mochaWebpackConfig(webpackConfig);
 
 let { app } = toolbox.startDevServer(envConfig, webpackConfig, __dirname);
 
@@ -20,15 +18,9 @@ app.use(
   express.static("src/test/mochitest/examples")
 );
 
-app.get("/integration/mocha.css", function(req, res) {
-  res.sendFile(path.join(__dirname, "../node_modules/mocha/mocha.css"));
-});
-
 app.get("/integration", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/test/integration/index.html"));
 });
-
-startMochaServer(app)
 
 console.log("View debugger examples here:")
 console.log("https://github.com/devtools-html/debugger-examples")
