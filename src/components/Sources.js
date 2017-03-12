4ab91d2548b9b86f3ff73828833657aaabd1fcff
Anshul <malikanshul29@gmail.com>
2017-03-12 20:36:17 +0530

    Refactor(ES6) src/components/Sources.js

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -1,26 +1,16 @@
 // @flow
-const React = require("react");
-const { DOM: dom, PropTypes } = React;
-const ImPropTypes = require("react-immutable-proptypes");
-const { bindActionCreators } = require("redux");
-const { connect } = require("react-redux");
-const { formatKeyShortcut } = require("../utils/text");
-const SourcesTree = React.createFactory(require("./SourcesTree"));
-const actions = require("../actions");
-const { getSelectedSource, getSources } = require("../selectors");
 
-require("./Sources.css");
+import { DOM as dom, PropTypes, Component, createFactory } from "react";
+import ImPropTypes from "react-immutable-proptypes";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { formatKeyShortcut } from "../utils/text";
+const SourcesTree = createFactory(require("./SourcesTree"));
+import actions from "../actions";
+import { getSelectedSource, getSources } from "../selectors";
+import "./Sources.css";
 
-const Sources = React.createClass({
-  propTypes: {
-    sources: ImPropTypes.map.isRequired,
-    selectSource: PropTypes.func.isRequired,
-    horizontal: PropTypes.bool.isRequired,
-    toggleFileSearch: PropTypes.func.isRequired
-  },
-
-  displayName: "Sources",
-
+class Sources extends Component {
   renderShortcut() {
     if (this.props.horizontal) {
       return dom.span(
@@ -33,7 +23,7 @@ const Sources = React.createClass({
           formatKeyShortcut(`CmdOrCtrl+${L10N.getStr("sources.search.key")}`))
       );
     }
-  },
+  }
 
   render() {
     const { sources, selectSource } = this.props;
@@ -46,9 +36,18 @@ const Sources = React.createClass({
       SourcesTree({ sources, selectSource })
     );
   }
-});
+}
 
-module.exports = connect(
+Sources.propTypes = {
+  sources: ImPropTypes.map.isRequired,
+  selectSource: PropTypes.func.isRequired,
+  horizontal: PropTypes.bool.isRequired,
+  toggleFileSearch: PropTypes.func.isRequired
+};
+
+Sources.displayName = "Sources";
+
+export default connect(
   state => ({ selectedSource: getSelectedSource(state),
     sources: getSources(state) }),
   dispatch => bindActionCreators(actions, dispatch)
