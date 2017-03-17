8ba7180706fd6488475602cb50b56432c83df447
Jason Laster <jason.laster.11@gmail.com>
2017-03-17 13:36:37 -0400

    Add prettier

---
+++ bin/prettier.js
@@ -0,0 +1,26 @@
+
+const path = require("path");
+const spawn  = require("child_process").spawn;
+
+const isWindows = /^win/.test(process.platform);
+const prettierCmd = path.resolve(__dirname, "../node_modules/.bin/" + (isWindows ? "prettier.cmd" : "prettier"));
+
+const args = [
+  "--trailing-comma=all",
+  "--bracket-spacing=true"
+];
+
+const prettierArgs = process.argv.slice(2).concat(args);
+
+const prettierProc = spawn(prettierCmd, prettierArgs)
+
+prettierProc.stdout.on('data', data => console.log(`${data}`));
+prettierProc.stderr.on('data', data => console.log(`stderr: ${data}`));
+prettierProc.on('close', code => console.log(`prettier exited with code ${code}`));
+prettierProc.on('error', error => {
+  if (error.code == "ENOENT") {
+    console.log(`Hmm, could not find the path ${cmd}.`)
+    return;
+  }
+  console.log(error)
+});
