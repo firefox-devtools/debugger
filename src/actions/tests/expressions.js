0a75c9bb060b5e7a0a7f8faa0116f14e698af1cf
bomsy <b4bomsy@gmail.com>
2017-04-05 23:09:03 +0100

    fix quotes

--- src/actions/tests/expressions.js
+++ src/actions/tests/expressions.js
@@ -1,4 +1,4 @@
-import expect from 'expect.js';
+import expect from "expect.js";
 import {
   actions,
   selectors,
@@ -9,62 +9,62 @@ import {
 const mockThreadClient = {
   evaluate: (script, { frameId }) => {
     return new Promise((resolve, reject) => {
-      resolve('bla');
+      resolve("bla");
     });
   }
 }
 
 describe("expressions", () => {
-  it('should add an expression', () => {
+  it("should add an expression", () => {
     const { dispatch, getState } = createStore();
 
-    dispatch(actions.addExpression('foo'));
-    dispatch(actions.addExpression('bar', { visible: false }));
+    dispatch(actions.addExpression("foo"));
+    dispatch(actions.addExpression("bar", { visible: false }));
 
     expect(selectors.getExpressions(getState()).size).to.be(2);
     expect(selectors.getVisibleExpressions(getState()).size).to.be(1);
   });
 
-  it('should update an expression', () => {
+  it("should update an expression", () => {
     const { dispatch, getState } = createStore();
 
-    dispatch(actions.addExpression('foo'));
-    const expression = selectors.getExpression(getState(), 'foo');
-    dispatch(actions.updateExpression('bar', expression));
+    dispatch(actions.addExpression("foo"));
+    const expression = selectors.getExpression(getState(), "foo");
+    dispatch(actions.updateExpression("bar", expression));
 
-    expect(selectors.getExpression(getState(), 'bar').input).to.be('bar');
+    expect(selectors.getExpression(getState(), "bar").input).to.be("bar");
 
   });
 
-  it('should delete an expression', () => {
+  it("should delete an expression", () => {
     const { dispatch, getState } = createStore();
 
-    dispatch(actions.addExpression('foo'));
-    dispatch(actions.addExpression('bar', { visible: false }));
+    dispatch(actions.addExpression("foo"));
+    dispatch(actions.addExpression("bar", { visible: false }));
 
     expect(selectors.getExpressions(getState()).size).to.be(2);
 
-    const expression = selectors.getExpression(getState(), 'foo');
+    const expression = selectors.getExpression(getState(), "foo");
     dispatch(actions.deleteExpression(expression));
 
     expect(selectors.getExpressions(getState()).size).to.be(1);
-    expect(selectors.getExpression(getState(), 'bar').input).to.be('bar');
+    expect(selectors.getExpression(getState(), "bar").input).to.be("bar");
   });
 
-  it('should evaluate expressions', async () => {
+  it("should evaluate expressions", async () => {
     const { dispatch, getState } = createStore(mockThreadClient);
-    const frameId = 'baa';
+    const frameId = "baa";
 
-    dispatch(actions.addExpression('foo'));
-    dispatch(actions.addExpression('bar', { visible: false }));
+    dispatch(actions.addExpression("foo"));
+    dispatch(actions.addExpression("bar", { visible: false }));
 
-    expect(selectors.getExpression(getState(), 'foo').value).to.be(null);
-    expect(selectors.getExpression(getState(), 'bar').value).to.be(null);
+    expect(selectors.getExpression(getState(), "foo").value).to.be(null);
+    expect(selectors.getExpression(getState(), "bar").value).to.be(null);
 
     await dispatch(actions.evaluateExpressions(frameId))
 
-    expect(selectors.getExpression(getState(), 'foo').value).to.be('bla');
-    expect(selectors.getExpression(getState(), 'bar').value).to.be('bla');
+    expect(selectors.getExpression(getState(), "foo").value).to.be("bla");
+    expect(selectors.getExpression(getState(), "bar").value).to.be("bla");
 
   });
 
