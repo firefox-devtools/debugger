2417227a64e615806b14c8445b48e3c00ea97f35
Jason Laster <jason.laster.11@gmail.com>
2017-04-14 10:36:30 -0400

    Map Display Names (#2657)

---
+++ src/utils/frame.js
@@ -0,0 +1,98 @@
+import get from "lodash/get";
+import { isEnabled } from "devtools-config";
+import { endTruncateStr } from "./utils";
+
+import type { Frame } from "debugger-html";
+
+function isBackbone(frame) {
+  const url = get(frame, "source.url", "");
+  return url.match(/backbone/i);
+}
+
+function isJQuery(frame) {
+  const url = get(frame, "source.url", "");
+  return url.match(/jquery/i);
+}
+
+function isReact(frame) {
+  const url = get(frame, "source.url", "");
+  return url.match(/react/i);
+}
+
+const displayNameMap = {
+  Backbone: {
+    "extend/child": "Create Class",
+    ".create": "Create Model"
+  },
+  jQuery: {
+    "jQuery.event.dispatch": "Dispatch Event"
+  },
+  React: {}
+};
+
+function mapDisplayNames(frame, library) {
+  const map = displayNameMap[library];
+  const { displayName } = frame;
+  return (map && map[displayName]) || displayName;
+}
+
+export function annotateFrame(frame) {
+  if (!isEnabled("collapseFrame")) {
+    return frame;
+  }
+
+  if (isBackbone(frame)) {
+    return Object.assign({}, frame, { library: "Backbone" });
+  }
+
+  if (isJQuery(frame)) {
+    return Object.assign({}, frame, { library: "jQuery" });
+  }
+
+  if (isReact(frame)) {
+    return Object.assign({}, frame, { library: "React" });
+  }
+
+  return frame;
+}
+
+// Decodes an anonymous naming scheme that
+// spider monkey implements based on "Naming Anonymous JavaScript Functions"
+// http://johnjbarton.github.io/nonymous/index.html
+const objectProperty = /([\w\d]+)$/;
+const arrayProperty = /\[(.*?)\]$/;
+const functionProperty = /([\w\d]+)[\/\.<]*?$/;
+const annonymousProperty = /([\w\d]+)\(\^\)$/;
+
+export function simplifyDisplayName(displayName) {
+  // if the display name has a space it has already been mapped
+  if (/\s/.exec(displayName)) {
+    return displayName;
+  }
+
+  const scenarios = [
+    objectProperty,
+    arrayProperty,
+    functionProperty,
+    annonymousProperty
+  ];
+
+  for (let reg of scenarios) {
+    let match = reg.exec(displayName);
+    if (match) {
+      return match[1];
+    }
+  }
+
+  return displayName;
+}
+
+export function formatDisplayName(frame: Frame) {
+  const { displayName, library } = frame;
+  if (library) {
+    displayName = mapDisplayNames(frame, library);
+  }
+
+  displayName = simplifyDisplayName(displayName);
+  return endTruncateStr(displayName, 25);
+}
---
+++ src/utils/tests/frame.js
@@ -0,0 +1,82 @@
+/* eslint max-nested-callbacks: ["error", 6] */
+
+import { simplifyDisplayName, formatDisplayName } from "../frame";
+
+const cases = {
+  defaultCase: [["define", "define"]],
+
+  objectProperty: [
+    ["z.foz", "foz"],
+    ["z.foz/baz", "baz"],
+    ["z.foz/baz/y.bay", "bay"],
+    ["outer/x.fox.bax.nx", "nx"],
+    ["outer/fow.baw", "baw"],
+    ["fromYUI._attach", "_attach"],
+    ["Y.ClassNameManager</getClassName", "getClassName"],
+    ["orion.textview.TextView</addHandler", "addHandler"],
+    ["this.eventPool_.createObject", "createObject"]
+  ],
+
+  arrayProperty: [
+    ["this.eventPool_[createObject]", "createObject"],
+    ["jQuery.each(^)/jQuery.fn[o]", "o"],
+    ["viewport[get+D]", "get+D"],
+    ["arr[0]", "0"]
+  ],
+
+  functionProperty: [
+    ["fromYUI._attach/<.", "_attach"],
+    ["Y.ClassNameManager<", "ClassNameManager"],
+    ["fromExtJS.setVisible/cb<", "cb"],
+    ["fromDojo.registerWin/<", "registerWin"]
+  ],
+
+  annonymousProperty: [["jQuery.each(^)", "each"]]
+};
+
+describe("function names", () => {
+  describe("simplifying display names", () => {
+    Object.keys(cases).forEach(type => {
+      cases[type].forEach(([kase, expected]) => {
+        it(`${type} - ${kase}`, () =>
+          expect(simplifyDisplayName(kase)).toEqual(expected));
+      });
+    });
+  });
+
+  describe("formatting display names", () => {
+    it("uses a library description", () => {
+      const frame = {
+        library: "Backbone",
+        displayName: "extend/child",
+        source: {
+          url: "assets/backbone.js"
+        }
+      };
+
+      expect(formatDisplayName(frame)).toEqual("Create Class");
+    });
+
+    it("shortens an anonymous function", () => {
+      const frame = {
+        displayName: "extend/child/bar/baz",
+        source: {
+          url: "assets/bar.js"
+        }
+      };
+
+      expect(formatDisplayName(frame)).toEqual("baz");
+    });
+
+    it("truncates long function names", () => {
+      const frame = {
+        displayName: "bazbazbazbazbazbazbazbazbazbazbazbazbaz",
+        source: {
+          url: "assets/bar.js"
+        }
+      };
+
+      expect(formatDisplayName(frame)).toEqual("...zbazbazbazbazbazbazbazbaz");
+    });
+  });
+});
