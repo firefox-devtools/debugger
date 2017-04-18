5ad1785387cf96befe242fd330af210d6cc0c34f
I-Chun (Arthur) Liu <arthur801031@gmail.com>
2017-04-19 03:50:46 +0800

    Fix Location reps property (#2674)
    * Fix Location reps property
    
    * Revert index.js, SearchBar.js, sources-tree.js
    
    * Revert
    
    * add tests

---
+++ src/client/firefox/tests/commands.js
@@ -0,0 +1,92 @@
+import { setupCommands, clientCommands } from "../commands";
+
+function makeThreadCLient(resp) {
+  return {
+    pauseGrip: () => ({
+      getPrototypeAndProperties: async () => resp
+    })
+  };
+}
+
+describe("firefox commands", () => {
+  describe("getProperties", () => {
+    it("empty response", async () => {
+      const { getProperties } = clientCommands;
+      const threadClient = makeThreadCLient({
+        ownProperties: {},
+        safeGetterValues: {}
+      });
+
+      setupCommands({ threadClient });
+      const props = await getProperties({});
+      expect(props).toEqual({ ownProperties: {}, safeGetterValues: {} });
+    });
+
+    it("simple properties", async () => {
+      const { getProperties } = clientCommands;
+      const threadClient = makeThreadCLient({
+        ownProperties: {
+          obj: { value: "obj" },
+          foo: { value: "foo" }
+        },
+        safeGetterValues: {}
+      });
+
+      const expected = {
+        ownProperties: { foo: { value: "foo" }, obj: { value: "obj" } },
+        safeGetterValues: {}
+      };
+
+      setupCommands({ threadClient });
+      const props = await getProperties({});
+      expect(props).toEqual(expected);
+    });
+
+    it("getter values", async () => {
+      const { getProperties } = clientCommands;
+      const threadClient = makeThreadCLient({
+        ownProperties: {
+          obj: { value: "obj" },
+          foo: { value: "foo" }
+        },
+        safeGetterValues: {
+          obj: { getterValue: "getter" }
+        }
+      });
+
+      const expected = {
+        ownProperties: { foo: { value: "foo" }, obj: { value: "getter" } },
+        safeGetterValues: {
+          obj: { getterValue: "getter" }
+        }
+      };
+
+      setupCommands({ threadClient });
+      const props = await getProperties({});
+      expect(props).toEqual(expected);
+    });
+
+    it("new getter values", async () => {
+      const { getProperties } = clientCommands;
+      const threadClient = makeThreadCLient({
+        ownProperties: {
+          foo: { value: "foo" }
+        },
+        safeGetterValues: {
+          obj: { value: "getter" }
+        }
+      });
+
+      const expected = {
+        ownProperties: { foo: { value: "foo" }, obj: { value: "getter" } },
+        safeGetterValues: {
+          obj: { value: "getter" }
+        }
+      };
+
+      setupCommands({ threadClient });
+      const props = await getProperties({});
+      expect(props).toEqual(expected);
+    });
+  });
+});
