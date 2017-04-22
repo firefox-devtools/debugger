e33814b737416bdf7f55b3898fb1b55ba08e5d81
Hubert Boma Manilla <b4bomsy@gmail.com>
2017-04-22 16:05:44 +0100

    add tests for breakpoint actions (#2714)

--- src/actions/tests/breakpoints.js
+++ src/actions/tests/breakpoints.js
@@ -6,6 +6,18 @@ const simpleMockThreadClient = {
     return new Promise((resolve, reject) => {
       resolve({ id: "hi", actualLocation: location });
     });
+  },
+
+  removeBreakpoint: id => {
+    return new Promise((resolve, reject) => {
+      resolve({ status: "done" });
+    });
+  },
+
+  setBreakpointCondition: (id, location, condition, noSliding) => {
+    return new Promise((resolve, reject) => {
+      resolve({ sourceId: "a", line: 5 });
+    });
   }
 };
 
@@ -17,5 +29,88 @@ describe("breakpoints", () => {
     await dispatch(actions.addBreakpoint({ sourceId: "b", line: 6 }));
 
     expect(selectors.getBreakpoints(getState()).size).to.be(2);
+  });
+
+  it("should remove a breakpoint", async () => {
+    const { dispatch, getState } = createStore(simpleMockThreadClient);
+
+    const loc1 = { sourceId: "a", line: 5 };
+    const loc2 = { sourceId: "b", line: 6 };
+
+    await dispatch(actions.addBreakpoint(loc1));
+    await dispatch(actions.addBreakpoint(loc2));
+
+    await dispatch(actions.removeBreakpoint(loc1));
+
+    expect(selectors.getBreakpoints(getState()).size).to.be(1);
+  });
+
+  it("should disable a breakpoint", async () => {
+    const { dispatch, getState } = createStore(simpleMockThreadClient);
+
+    const loc1 = { sourceId: "a", line: 5 };
+    const loc2 = { sourceId: "b", line: 6 };
+
+    await dispatch(actions.addBreakpoint(loc1));
+    await dispatch(actions.addBreakpoint(loc2));
+
+    await dispatch(actions.disableBreakpoint(loc1));
+
+    expect(selectors.getBreakpoint(getState(), loc1).disabled).to.be(true);
+  });
+
+  it("should enable breakpoint", async () => {
+    const { dispatch, getState } = createStore(simpleMockThreadClient);
+    const loc = { sourceId: "a", line: 5 };
+
+    await dispatch(actions.addBreakpoint(loc));
+    await dispatch(actions.disableBreakpoint(loc));
+
+    expect(selectors.getBreakpoint(getState(), loc).disabled).to.be(true);
+
+    await dispatch(actions.enableBreakpoint(loc));
+
+    expect(selectors.getBreakpoint(getState(), loc).disabled).to.be(false);
+  });
+
+  it("should toggle all the breakpoints", async () => {
+    const { dispatch, getState } = createStore(simpleMockThreadClient);
+
+    const loc1 = { sourceId: "a", line: 5 };
+    const loc2 = { sourceId: "b", line: 6 };
+
+    await dispatch(actions.addBreakpoint(loc1));
+    await dispatch(actions.addBreakpoint(loc2));
+
+    await dispatch(actions.toggleAllBreakpoints(true));
+
+    expect(selectors.getBreakpoint(getState(), loc1).disabled).to.be(true);
+    expect(selectors.getBreakpoint(getState(), loc2).disabled).to.be(true);
+
+    await dispatch(actions.toggleAllBreakpoints());
+
+    expect(selectors.getBreakpoint(getState(), loc1).disabled).to.be(false);
+    expect(selectors.getBreakpoint(getState(), loc2).disabled).to.be(false);
+  });
+
+  it("should set the breakpoint condition", async () => {
+    const { dispatch, getState } = createStore(simpleMockThreadClient);
+
+    const loc = { sourceId: "a", line: 5 };
+
+    await dispatch(actions.addBreakpoint(loc));
+
+    expect(selectors.getBreakpoint(getState(), loc).condition).to.be(null);
+
+    await dispatch(
+      actions.setBreakpointCondition(loc, {
+        condition: "const foo = 0",
+        getTextForLine: () => {}
+      })
+    );
+
+    expect(selectors.getBreakpoint(getState(), loc).condition).to.be(
+      "const foo = 0"
+    );
   });
 });
