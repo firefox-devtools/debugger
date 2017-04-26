473bbf3399b7b19afc5416548d1f121d5bbb6b9b
Nadav Fischer <nadav.fischer@capriza.com>
2017-04-26 20:02:48 +0300

    re #2687 - remove editor preview feature flag

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -328,7 +328,6 @@ class Editor extends Component {
     if (
       !selectedFrame ||
       !sourceText ||
-      !isEnabled("editorPreview") ||
       !selectedSource ||
       selectedFrame.location.sourceId !== selectedSource.get("id")
     ) {
@@ -711,12 +710,7 @@ class Editor extends Component {
       return null;
     }
 
-    if (
-      !isEnabled("editorPreview") ||
-      !selectedToken ||
-      !selectedFrame ||
-      !selectedExpression
-    ) {
+    if (!selectedToken || !selectedFrame || !selectedExpression) {
       return;
     }
 
