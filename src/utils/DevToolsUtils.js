d4be6fcc020dd5bebf641923d2390842ef60b8e9
Peter Schussheim <peter@schussheim.com>
2017-04-28 16:34:19 -0400

    Export 'assert' as default

--- src/utils/DevToolsUtils.js
+++ src/utils/DevToolsUtils.js
@@ -1,12 +1,12 @@
 import assert from "./assert";
 
-function reportException(who, exception) {
+export function reportException(who, exception) {
   let msg = `${who} threw an exception: `;
   console.error(msg, exception);
 }
 
-function executeSoon(fn) {
+export function executeSoon(fn) {
   setTimeout(fn, 0);
 }
 
-export { reportException, executeSoon, assert };
+export default assert;
