68c3e6b4af3eafee85616d7629461018cc85b7df
Sharath Narayan <phsharathnarayan@gmail.com>
2017-05-04 23:21:07 -0500

    #2667-Add key modifiers

--- src/components/ProjectSearch.js
+++ src/components/ProjectSearch.js
@@ -56,7 +56,7 @@ class ProjectSearch extends Component {
       L10N.getStr("sources.search.key"),
       L10N.getStr("symbolSearch.search.key")
     ];
-    searchKeys.forEach(key => shortcuts.off(`CmdOrCtrl+${key}`, this.toggle));
+    searchKeys.forEach(key => shortcuts.off(key, this.toggle));
     shortcuts.off("Escape", this.onEscape);
   }
 
@@ -66,7 +66,7 @@ class ProjectSearch extends Component {
       L10N.getStr("sources.search.key"),
       L10N.getStr("symbolSearch.search.key")
     ];
-    searchKeys.forEach(key => shortcuts.on(`CmdOrCtrl+${key}`, this.toggle));
+    searchKeys.forEach(key => shortcuts.on(key, this.toggle));
     shortcuts.on("Escape", this.onEscape);
   }
 
