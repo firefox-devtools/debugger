69dfe5af4f52f97e5d4c17bb4cdca00cf480fddf
Bryan Clark <clarkbw@gmail.com>
2017-05-08 09:46:13 -0700

    Use jest-localstorage-mock for jest localStorage mocks

--- src/test/tests-setup.js
+++ src/test/tests-setup.js
@@ -1,27 +1,3 @@
-function localStorage() {
-  let storage = {};
-
-  return {
-    setItem: function(key, value) {
-      storage[key] = value || "";
-    },
-    getItem: function(key) {
-      return storage[key] || null;
-    },
-    removeItem: function(key) {
-      delete storage[key];
-    },
-    get length() {
-      return Object.keys(storage).length;
-    },
-    key: function(i) {
-      let keys = Object.keys(storage);
-      return keys[i] || null;
-    }
-  };
-}
-
-global.localStorage = localStorage();
 global.Worker = require("workerjs");
 
 global.L10N = { getStr: () => {} };
