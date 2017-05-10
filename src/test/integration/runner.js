657d5b3019d6d4ef72e9063a43c45c627e5c68da
Hubert Boma Manilla <b4bomsy@gmail.com>
2017-05-10 14:45:02 +0100

    Fix for the last closed tab not getting persisted (#2721)
    * Fix for the last closed tab not getting persisted
    
    * pass url instead of the source id to the location object for the pendingSelectedlocation object, add a getSourceUrlbyId function
    
    * add new closetab command
    
    * fix tab tests
    
    * added reload tests
    
    * fix flow issues
    
    * cleanup
    
    * fix lint issues
    
    * remove console.log
    
    * why did i disable the tests? :)

--- src/test/integration/runner.js
+++ src/test/integration/runner.js
@@ -107,8 +107,10 @@ describe("Tests", () => {
 
   // expected 2 to equal 1
   xit("source maps bogus", async () => await sourceMapsBogus(ctx));
-
-  it("tabs", async () => await tabs(ctx));
+  it("tabs - add tabs", async () => await tabs.addTabs(ctx));
+  it("tabs - reload with tabs", async () => await tabs.reloadWithTabs(ctx));
+  it("tabs - reload with no tabs", async () =>
+    await tabs.reloadWithNoTabs(ctx));
 });
 
 mocha.run();
--- src/test/integration/tests/tabs.js
+++ src/test/integration/tests/tabs.js
@@ -3,14 +3,15 @@ const {
   reload,
   selectSource,
   findElement,
-  waitForDispatch
+  waitForDispatch,
+  closeTab
 } = require("../utils");
 
 function countTabs(dbg) {
   return findElement(dbg, "sourceTabs").children.length;
 }
 
-module.exports = async function(ctx) {
+export async function addTabs(ctx) {
   const { ok, is, info, requestLongerTimeout } = ctx;
 
   let dbg = await initDebugger("doc-scripts.html", "simple1", "simple2");
@@ -28,4 +29,34 @@ module.exports = async function(ctx) {
   await reload(dbg, "simple1", "simple2");
   await waitForDispatch(dbg, "LOAD_SOURCE_TEXT");
   expect(countTabs(dbg)).to.equal(2);
-};
+}
+
+export async function reloadWithTabs(ctx) {
+  const { ok, is, info, requestLongerTimeout } = ctx;
+
+  let dbg = await initDebugger("doc-scripts.html", "simple1", "simple2");
+
+  await selectSource(dbg, "simple1");
+  await selectSource(dbg, "simple2");
+  expect(countTabs(dbg)).to.equal(2);
+
+  // Test reloading the debugger
+  await reload(dbg, "simple1", "simple2");
+  await waitForDispatch(dbg, "LOAD_SOURCE_TEXT");
+  expect(countTabs(dbg)).to.equal(2);
+}
+
+export async function reloadWithNoTabs(ctx) {
+  const { ok, is, info, requestLongerTimeout } = ctx;
+
+  let dbg = await initDebugger("doc-scripts.html", "simple1", "simple2");
+
+  await selectSource(dbg, "simple1");
+  await selectSource(dbg, "simple2");
+  closeTab(dbg, "simple1");
+  closeTab(dbg, "simple2");
+
+  // Test reloading the debugger
+  dbg = await initDebugger("doc-scripts.html", "simple1", "simple2");
+  expect(countTabs(dbg)).to.equal(0);
+}
