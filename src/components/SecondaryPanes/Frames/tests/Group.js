ca3c2a1393a1148b2c98e38f826bed0265f07d74
Yulia Startsev <ystartsev@mozilla.com>
2017-05-16 14:53:56 +0200

    add context menu to groups in frames

---
+++ src/components/SecondaryPanes/Frames/tests/Group.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { shallow } from "enzyme";
+import GroupComponent from "../Group.js";
+const Group = React.createFactory(GroupComponent);
+
+import FrameMenu from "../FrameMenu";
+jest.mock("../FrameMenu", () => jest.fn());
+
+function render(overrides = {}) {
+  const defaultProps = {
+    group: [{ displayName: "foo" }],
+    selectedFrame: {},
+    selectFrame: jest.fn(),
+    copyStackTrace: jest.fn()
+  };
+
+  const props = Object.assign({}, defaultProps, overrides);
+  const component = shallow(new Group(props));
+  return { component, props };
+}
+
+describe("Frame", () => {
+  it("user frame", () => {
+    const { component } = render();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe("mouse events", () => {
+    it("calls FrameMenu on right click", () => {
+      const { component, props } = render();
+      const mockEvent = "mockEvent";
+      component.simulate("contextmenu", mockEvent);
+
+      expect(FrameMenu).toHaveBeenCalledWith(
+        props.group[0],
+        props.copyStackTrace,
+        mockEvent
+      );
+    });
+  });
+});
