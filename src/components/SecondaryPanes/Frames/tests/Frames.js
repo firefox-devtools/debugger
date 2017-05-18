d25d7a25e03487707a91aee8c2d4a57953ebc9fb
Yulia Startsev <ystartsev@mozilla.com>
2017-05-18 14:56:35 +0200

    update Frames with toggleFrameworkGroupingFunction

--- src/components/SecondaryPanes/Frames/tests/Frames.js
+++ src/components/SecondaryPanes/Frames/tests/Frames.js
@@ -3,34 +3,32 @@ import { shallow } from "enzyme";
 import _Frames from "../index.js";
 const Frames = React.createFactory(_Frames.WrappedComponent);
 
+function render(overrides = {}) {
+  const defaultProps = {
+    frames: null,
+    selectedFrame: null,
+    frameworkGroupingOn: false,
+    toggleFrameworkGrouping: jest.fn(),
+    contextTypes: {},
+    selectFrame: jest.fn()
+  };
+
+  const props = Object.assign({}, defaultProps, overrides);
+  const component = shallow(new Frames(props));
+
+  return component;
+}
+
 describe("Frames", () => {
   it("empty frames", () => {
-    const frames = null;
-    const selectedFrame = null;
-    const component = shallow(
-      new Frames({
-        frames,
-        selectedFrame,
-        contextTypes: {},
-        selectFrame: jest.fn()
-      })
-    );
+    const component = render();
     expect(component).toMatchSnapshot();
   });
 
   it("one frame", () => {
     const frames = [{ id: 1 }];
     const selectedFrame = frames[0];
-    const selectFrame = jest.fn();
-
-    const component = shallow(
-      new Frames({
-        frames,
-        selectedFrame,
-        contextTypes: {},
-        selectFrame
-      })
-    );
+    const component = render({ frames, selectedFrame });
 
     expect(component).toMatchSnapshot();
   });
@@ -47,37 +45,43 @@ describe("Frames", () => {
       { id: 8 }
     ];
     const selectedFrame = frames[0];
-    const component = shallow(
-      new Frames({
-        frames,
-        selectedFrame,
-        contextTypes: {},
-        selectFrame: jest.fn()
-      })
-    );
+    const component = render({ frames, selectedFrame });
+
     expect(component).toMatchSnapshot();
     const showMore = component.node.props.children[1];
     expect(showMore.props.className).toEqual("show-more");
   });
 
-  it("library frames", () => {
-    const frames = [
-      { id: 1 },
-      { id: 2, library: "back" },
-      { id: 3, library: "back" },
-      { id: 8 }
-    ];
-    const selectedFrame = frames[0];
-    const component = shallow(
-      new Frames({
+  describe("Library Frames", () => {
+    it("expand framework frames into a multiple frames", () => {
+      const frames = [
+        { id: 1 },
+        { id: 2, library: "back" },
+        { id: 3, library: "back" },
+        { id: 8 }
+      ];
+      const selectedFrame = frames[0];
+      const frameworkGroupingOn = false;
+      const component = render({ frames, frameworkGroupingOn, selectedFrame });
+      expect(component).toMatchSnapshot();
+      // const showMore = component.node.props.children[1];
+      // expect(showMore.props.className).toEqual("show-more");
+    });
+
+    it("collapse framework frames into a single frame", () => {
+      const frames = [
+        { id: 1 },
+        { id: 2, library: "back" },
+        { id: 3, library: "back" },
+        { id: 8 }
+      ];
+      const selectedFrame = frames[0];
+      const component = render({
         frames,
         selectedFrame,
-        contextTypes: {},
-        selectFrame: jest.fn()
-      })
-    );
-    expect(component).toMatchSnapshot();
-    // const showMore = component.node.props.children[1];
-    // expect(showMore.props.className).toEqual("show-more");
+        frameworkGroupingOn: true
+      });
+      expect(component).toMatchSnapshot();
+    });
   });
 });
