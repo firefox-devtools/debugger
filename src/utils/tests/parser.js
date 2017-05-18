3ccdc5927c007b2bafacff9a4d606d6613a342ae
Ryan Duffy <ryan.duffy@lge.com>
2017-05-18 09:22:06 -0500

    add block statements to lexical scope determination

--- src/utils/tests/parser.js
+++ src/utils/tests/parser.js
@@ -233,6 +233,17 @@ describe("parser", () => {
         line: 3
       });
     });
+
+    it("finds variables in block scope", () => {
+      const scope = getClosestScope(getSourceText("resolveToken"), {
+        line: 34,
+        column: 13
+      });
+
+      var vars = getVariablesInLocalScope(scope);
+
+      expect(vars.map(v => v.name)).to.eql(["x"]);
+    });
   });
 
   describe("getVariablesInScope", () => {
@@ -253,6 +264,100 @@ describe("parser", () => {
         "four",
         "math",
         "child"
+      ]);
+    });
+
+    it("finds variables from multiple scopes", () => {
+      let vars;
+      const source = getSourceText("resolveToken");
+
+      vars = getVariablesInScope(
+        getClosestScope(source, {
+          line: 36,
+          column: 19
+        })
+      );
+
+      expect(vars).to.eql([
+        "this",
+        "arguments",
+        "y",
+        "x",
+        "innerScope",
+        "outer",
+        "fromIIFE",
+        "a",
+        "b",
+        "getA",
+        "setB",
+        "plusAB",
+        "withMultipleScopes"
+      ]);
+
+      vars = getVariablesInScope(
+        getClosestScope(source, {
+          line: 34,
+          column: 14
+        })
+      );
+
+      expect(vars).to.eql([
+        "this",
+        "arguments",
+        "x",
+        "innerScope",
+        "outer",
+        "fromIIFE",
+        "a",
+        "b",
+        "getA",
+        "setB",
+        "plusAB",
+        "withMultipleScopes"
+      ]);
+
+      vars = getVariablesInScope(
+        getClosestScope(source, {
+          line: 24,
+          column: 9
+        })
+      );
+
+      expect(vars).to.eql([
+        "this",
+        "arguments",
+        "inner",
+        "innerScope",
+        "outer",
+        "fromIIFE",
+        "a",
+        "b",
+        "getA",
+        "setB",
+        "plusAB",
+        "withMultipleScopes"
+      ]);
+
+      vars = getVariablesInScope(
+        getClosestScope(source, {
+          line: 28,
+          column: 33
+        })
+      );
+
+      expect(vars).to.eql([
+        "this",
+        "arguments",
+        "toIIFE",
+        "innerScope",
+        "outer",
+        "fromIIFE",
+        "a",
+        "b",
+        "getA",
+        "setB",
+        "plusAB",
+        "withMultipleScopes"
       ]);
     });
   });
