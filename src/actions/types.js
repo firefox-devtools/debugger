4d493aeb04a1236c8b2e4bf62b8006d5f3e662b5
Yevgen Safronov <yevgen@cloudflare.com>
2017-06-04 15:44:46 +0100

    switch action types to exact object types

--- src/actions/types.js
+++ src/actions/types.js
@@ -57,61 +57,61 @@ type BreakpointResult = {
 };
 
 type BreakpointAction =
-  | {
+  | {|
       type: "ADD_BREAKPOINT",
       breakpoint: Breakpoint,
       condition: string,
       status: AsyncStatus,
       error: string,
       value: BreakpointResult
-    }
-  | {
+    |}
+  | {|
       type: "REMOVE_BREAKPOINT",
       breakpoint: Breakpoint,
       status: AsyncStatus,
       error: string,
       disabled: boolean
-    }
-  | {
+    |}
+  | {|
       type: "SET_BREAKPOINT_CONDITION",
       breakpoint: Breakpoint,
       condition: string,
       status: AsyncStatus,
       value: BreakpointResult,
       error: string
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_BREAKPOINTS",
       shouldDisableBreakpoints: boolean,
       status: AsyncStatus,
       error: string,
       value: any
-    };
+    |};
 
 type SourceAction =
-  | { type: "ADD_SOURCE", source: Source }
-  | { type: "ADD_SOURCES", sources: Array<Source> }
-  | {
+  | {| type: "ADD_SOURCE", source: Source |}
+  | {| type: "ADD_SOURCES", sources: Array<Source> |}
+  | {|
       type: "SELECT_SOURCE",
       source: Source,
       line?: number,
       tabIndex?: number
-    }
-  | { type: "SELECT_SOURCE_URL", url: string, line?: number }
-  | {
+    |}
+  | {| type: "SELECT_SOURCE_URL", url: string, line?: number |}
+  | {|
       type: "LOAD_SOURCE_TEXT",
       source: Source,
       status: AsyncStatus,
       error: string,
       value: SourceText
-    }
-  | {
+    |}
+  | {|
       type: "BLACKBOX",
       source: Source,
       error: string,
       value: { isBlackBoxed: boolean }
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_PRETTY_PRINT",
       source: Source,
       originalSource: Source,
@@ -122,43 +122,43 @@ type SourceAction =
         sourceText: SourceText,
         frames: Frame[]
       }
-    }
-  | { type: "CLOSE_TAB", url: string, tabs: any }
-  | { type: "CLOSE_TABS", urls: Array<string>, tabs: any };
+    |}
+  | {| type: "CLOSE_TAB", url: string, tabs: any |}
+  | {| type: "CLOSE_TABS", urls: Array<string>, tabs: any |};
 
 export type panelPositionType = "start" | "end";
 
 type UIAction =
-  | {
+  | {|
       type: "TOGGLE_FILE_SEARCH",
       value: boolean
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_PROJECT_SEARCH",
       value: boolean
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_FILE_SEARCH_MODIFIER",
       modifier: "caseSensitive" | "wholeWord" | "regexMatch"
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_FRAMEWORK_GROUPING",
       value: boolean
-    }
-  | {
+    |}
+  | {|
       type: "SHOW_SOURCE",
       sourceUrl: string
-    }
-  | {
+    |}
+  | {|
       type: "TOGGLE_PANE",
       position: panelPositionType,
       paneCollapsed: boolean
-    };
+    |};
 
 type PauseAction =
-  | { type: "BREAK_ON_NEXT", value: boolean }
-  | { type: "RESUME", value: void }
-  | {
+  | {| type: "BREAK_ON_NEXT", value: boolean |}
+  | {| type: "RESUME", value: void |}
+  | {|
       type: "PAUSED",
       pauseInfo: {
         why: Why,
@@ -168,70 +168,70 @@ type PauseAction =
       frames: Frame[],
       selectedFrameId: string,
       loadedObjects: LoadedObject[]
-    }
-  | {
+    |}
+  | {|
       type: "PAUSE_ON_EXCEPTIONS",
       shouldPauseOnExceptions: boolean,
       shouldIgnoreCaughtExceptions: boolean
-    }
-  | { type: "COMMAND", value: void }
-  | { type: "SELECT_FRAME", frame: Frame }
-  | {
+    |}
+  | {| type: "COMMAND", value: void |}
+  | {| type: "SELECT_FRAME", frame: Frame |}
+  | {|
       type: "LOAD_OBJECT_PROPERTIES",
       objectId: string,
       status: string,
       value: Object,
       "@@dispatch/promise": any
-    }
-  | {
+    |}
+  | {|
       type: "ADD_EXPRESSION",
       id: number,
       input: string,
       value: string,
       visible: boolean
-    }
-  | {
+    |}
+  | {|
       type: "EVALUATE_EXPRESSION",
       input: string,
       status: string,
       value: Object,
       visible: boolean,
       "@@dispatch/promise": any
-    }
-  | {
+    |}
+  | {|
       type: "UPDATE_EXPRESSION",
       expression: Expression,
       input: string,
       visible: boolean
-    }
-  | {
+    |}
+  | {|
       type: "DELETE_EXPRESSION",
       input: string
-    };
+    |};
 
 type NavigateAction = { type: "NAVIGATE", url: string };
 
 type ASTAction =
-  | {
+  | {|
       type: "SET_SYMBOLS",
       source: SourceText,
       symbols: SymbolDeclaration[]
-    }
-  | {
+    |}
+  | {|
       type: "OUT_OF_SCOPE_LOCATIONS",
       locations: AstLocation[]
-    }
-  | {
+    |}
+  | {|
       type: "SET_SELECTION",
       value: {
         expression: string,
         result: any,
         location: AstLocation
       }
-    }
-  | {
+    |}
+  | {|
       type: "CLEAR_SELECTION"
-    };
+    |};
 
 /**
  * Actions: Source, Breakpoint, and Navigation
