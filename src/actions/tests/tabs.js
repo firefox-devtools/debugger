3ad2340e73aebec2df655cb10c1319d6bfa7e161
Jason Laster <jason.laster.11@gmail.com>
2017-07-24 04:41:12 -0400

    Fix auto-selecting watch expressions (#3413)

---
+++ src/actions/tests/tabs.js
@@ -0,0 +1,110 @@
+import {
+  actions,
+  selectors,
+  createStore,
+  makeSource
+} from "../../utils/test-head";
+const { getSelectedSource, getSourceTabs } = selectors;
+
+const threadClient = {};
+
+describe("closing tabs", () => {
+  it("closing a tab", async () => {
+    const { dispatch, getState } = createStore(threadClient);
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.closeTab("http://localhost:8000/examples/foo.js"));
+
+    expect(getSelectedSource(getState())).toBe(undefined);
+    expect(getSourceTabs(getState()).size).toBe(0);
+  });
+
+  it("closing the inactive tab", async () => {
+    const { dispatch, getState } = createStore(threadClient);
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    await dispatch(actions.newSource(makeSource("bar.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.selectSource("bar.js"));
+    dispatch(actions.closeTab("http://localhost:8000/examples/foo.js"));
+
+    expect(getSelectedSource(getState()).get("id")).toBe("bar.js");
+    expect(getSourceTabs(getState()).size).toBe(1);
+  });
+
+  it("closing the only tab", async () => {
+    const { dispatch, getState } = createStore(threadClient);
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.closeTab("http://localhost:8000/examples/foo.js"));
+
+    expect(getSelectedSource(getState())).toBe(undefined);
+    expect(getSourceTabs(getState()).size).toBe(0);
+  });
+
+  it("closing the active tab", async () => {
+    const { dispatch, getState } = createStore(threadClient);
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    await dispatch(actions.newSource(makeSource("bar.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.selectSource("bar.js"));
+    dispatch(actions.closeTab("http://localhost:8000/examples/bar.js"));
+
+    expect(getSelectedSource(getState()).get("id")).toBe("foo.js");
+    expect(getSourceTabs(getState()).size).toBe(1);
+  });
+
+  it("closing many inactive tabs", async () => {
+    const { dispatch, getState } = createStore({});
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    await dispatch(actions.newSource(makeSource("bar.js")));
+    await dispatch(actions.newSource(makeSource("bazz.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.selectSource("bar.js"));
+    dispatch(actions.selectSource("bazz.js"));
+    dispatch(
+      actions.closeTabs([
+        "http://localhost:8000/examples/foo.js",
+        "http://localhost:8000/examples/bar.js"
+      ])
+    );
+
+    expect(getSelectedSource(getState()).get("id")).toBe("bazz.js");
+    expect(getSourceTabs(getState()).size).toBe(1);
+  });
+
+  it("closing many tabs including the active tab", async () => {
+    const { dispatch, getState } = createStore({});
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    await dispatch(actions.newSource(makeSource("bar.js")));
+    await dispatch(actions.newSource(makeSource("bazz.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.selectSource("bar.js"));
+    dispatch(actions.selectSource("bazz.js"));
+    dispatch(
+      actions.closeTabs([
+        "http://localhost:8000/examples/bar.js",
+        "http://localhost:8000/examples/bazz.js"
+      ])
+    );
+
+    expect(getSelectedSource(getState()).get("id")).toBe("foo.js");
+    expect(getSourceTabs(getState()).size).toBe(1);
+  });
+
+  it("closing all the tabs", async () => {
+    const { dispatch, getState } = createStore({});
+    await dispatch(actions.newSource(makeSource("foo.js")));
+    await dispatch(actions.newSource(makeSource("bar.js")));
+    dispatch(actions.selectSource("foo.js"));
+    dispatch(actions.selectSource("bar.js"));
+    dispatch(
+      actions.closeTabs([
+        "http://localhost:8000/examples/foo.js",
+        "http://localhost:8000/examples/bar.js"
+      ])
+    );
+
+    expect(getSelectedSource(getState())).toBe(undefined);
+    expect(getSourceTabs(getState()).size).toBe(0);
+  });
+});
