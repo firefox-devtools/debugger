2cf6c8cb72331b939ba4bad1d0c527cadb5573c1
Hubert Boma Manilla <b4bomsy@gmail.com>
2017-07-24 23:30:15 +0100

    fix the tests

--- src/utils/search/tests/project-search.js
+++ src/utils/search/tests/project-search.js
@@ -1,4 +1,4 @@
-import searchSources, { searchSource } from "../project-search";
+import { findSourceMatches } from "../project-search";
 import fromJS from "../../fromJS";
 
 const text = `
@@ -37,25 +37,7 @@ describe("project search", () => {
       url: "http://example.com/foo/bar.js"
     };
 
-    const matches = searchSource(source, "foo");
+    const matches = findSourceMatches(source, "foo");
     expect(matches).toMatchSnapshot();
-  });
-
-  it("searches multiple files", () => {
-    const results = searchSources("foo", testFiles);
-    const matches = results.map(i => i.matches);
-    expect(results).toMatchSnapshot();
-    expect(matches).toHaveLength(2);
-    expect(matches[0]).toHaveLength(2);
-    expect(matches[1]).toHaveLength(1);
-  });
-
-  it("handles no results", () => {
-    const results = searchSources("fuzz", testFiles);
-    const matches = results.map(i => i.matches);
-    expect(results).toMatchSnapshot();
-    expect(matches).toHaveLength(2);
-    expect(matches[0]).toHaveLength(0);
-    expect(matches[1]).toHaveLength(0);
   });
 });
