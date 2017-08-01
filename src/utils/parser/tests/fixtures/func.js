91deb2ba500dd6383fa98d1aaf93c434574251e8
Jan Kuehle <jkuehle90@gmail.com>
2017-08-01 20:33:53 +0200

    Test symbol extraction for async/exported functions

--- src/utils/parser/tests/fixtures/func.js
+++ src/utils/parser/tests/fixtures/func.js
@@ -2,6 +2,18 @@ function square(n) {
   return n * n;
 }
 
+export function exFoo() {
+  return "yay";
+}
+
+async function slowFoo() {
+  return "meh";
+}
+
+export async function exSlowFoo() {
+  return "yay in a bit";
+}
+
 child = function() {};
 
 (function() {
