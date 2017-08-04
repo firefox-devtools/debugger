2a7dea114ed78111a6e8f990d2f3020978317172
codehag <yulia.startsev@gmail.com>
2017-08-04 18:14:28 +0200

    wip debug line

---
+++ src/components/Editor/DebugLine.js
@@ -0,0 +1,78 @@
+// @flow
+import { Component } from "react";
+import { markText, toEditorPosition } from "../../utils/editor";
+import { getDocument } from "../../utils/editor/source-documents";
+
+type props = {
+  editor: Object,
+  selectedFrame: Object,
+  selectedLocation: Object
+};
+
+export default class DebugLine extends Component {
+  props: props;
+  state: {
+    debugExpression: {
+      clear: Function
+    }
+  };
+
+  constructor() {
+    super();
+    this.state = { debugExpression: { clear: () => {} } };
+  }
+
+  componentWillMount() {
+    this.setDebugLine(
+      this.props.selectedFrame,
+      this.props.selectedLocation,
+      this.props.editor
+    );
+  }
+
+  componentWillReceiveProps(nextProps: props) {
+    this.clearDebugLine(this.props.selectedFrame, this.props.editor);
+    this.setDebugLine(
+      nextProps.selectedFrame,
+      nextProps.selectedLocation,
+      nextProps.editor
+    );
+  }
+
+  componentWillUnmount() {
+    this.clearDebugLine(this.props.selectedFrame, this.props.editor);
+  }
+
+  setDebugLine(
+    selectedFrame: Object,
+    selectedLocation: Object,
+    editor: Object
+  ) {
+    const { location, location: { sourceId } } = selectedFrame;
+    const { line, column } = toEditorPosition(sourceId, location);
+
+    getDocument(sourceId).addLineClass(line, "line", "new-debug-line");
+    const debugExpression = markText(editor, "debug-expression", {
+      start: { line, column },
+      end: { line, column: null }
+    });
+    this.setState({ debugExpression });
+  }
+
+  clearDebugLine(selectedFrame: Object, editor: Object) {
+    const { line, sourceId } = selectedFrame.location;
+    const { debugExpression } = this.state;
+    if (debugExpression) {
+      debugExpression.clear();
+    }
+
+    const editorLine = line - 1;
+    getDocument(sourceId).removeLineClass(editorLine, "line", "new-debug-line");
+  }
+
+  render() {
+    return null;
+  }
+}
+
+DebugLine.displayName = "DebugLine";
