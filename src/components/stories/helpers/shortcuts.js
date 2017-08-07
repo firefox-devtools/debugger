d955da2a58c32d51768252e101d7877fa17a5257
Jason Laster <jason.laster.11@gmail.com>
2017-08-07 19:50:14 -0400

    improve text search ux

---
+++ src/components/stories/helpers/shortcuts.js
@@ -0,0 +1,19 @@
+import { PropTypes, Component } from "react";
+
+import { KeyShortcuts } from "devtools-modules";
+const shortcuts = new KeyShortcuts({ window });
+
+class Shortcuts extends Component {
+  getChildContext() {
+    return { shortcuts };
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+Shortcuts.childContextTypes = { shortcuts: PropTypes.object };
+Shortcuts.displayName = "Shortcuts";
+
+export default Shortcuts;
