20d6996aef5aed8606c00eb2cf619d914fe03be6
Jason Laster <jason.laster.11@gmail.com>
2017-08-09 17:29:03 -0400

    Kill the web test runner (#3606)

--- src/test/integration/utils/index.js
+++
@@ -1,7 +0,0 @@
-const wait = require("./wait");
-const assert = require("./assert");
-const mocha = require("./mocha");
-const commands = require("./commands");
-const shared = require("./shared");
-
-module.exports = Object.assign({}, wait, assert, mocha, commands, shared);
