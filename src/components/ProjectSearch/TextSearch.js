firefox-devtools/debugger

commit c8ba076a4b0892465e0d96b46595ecde4e0e1a54

convert ProjectSearch/*.js files to JSX (#3636)

src/components/ProjectSearch/TextSearch.js
@@ -1,13 +1,10 @@
-import { Component, DOM as dom, createFactory, PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 import classnames from "classnames";
 
 import escapeRegExp from "lodash/escapeRegExp";
 import Svg from "../shared/Svg";
-import _ManagedTree from "../shared/ManagedTree";
-const ManagedTree = createFactory(_ManagedTree);
-
-import _SearchInput from "../shared/SearchInput";
-const SearchInput = createFactory(_SearchInput);
+import ManagedTree from "../shared/ManagedTree";
+import SearchInput from "../shared/SearchInput";
 
 import "./TextSearch.css";
 
@@ -74,23 +71,28 @@ export default class TextSearch extends Component {
     if (focused) {
       this.focused = { setExpanded, file, expanded };
     }
-    return dom.div(
-      {
-        className: classnames("file-result", { focused }),
-        key: file.id,
-        onClick: e => setExpanded(file, !expanded)
-      },
-      Svg("arrow", {
-        className: classnames({
-          expanded
-        })
-      }),
-      Svg("file"),
-      dom.span({ className: "file-path" }, getRelativePath(file.filepath)),
-      dom.span(
-        { className: "matches-summary" },
-        ` (${file.matches.length} match${file.matches.length > 1 ? "es" : ""})`
-      )
+    const Arrow = Svg("arrow");
+    const File = Svg("file");
+
+    const matches = ` (${file.matches.length} match${file.matches.length > 1
+      ? "es"
+      : ""})`;
+
+    return (
+      <div
+        className={classnames("file-result", { focused })}
+        key={file.id}
+        onClick={e => setExpanded(file, !expanded)}
+      >
+        <Arrow className={classnames({ expanded })} />
+        <File />
+        <span className="file-path">
+          {getRelativePath(file.filepath)}
+        </span>
+        <span className="matches-summary">
+          {matches}
+        </span>
+      </div>
     );
   }
 
@@ -98,19 +100,16 @@ export default class TextSearch extends Component {
     if (focused) {
       this.focused = { match };
     }
-    return dom.div(
-      {
-        className: classnames("result", { focused }),
-        onClick: () => setTimeout(() => this.selectMatchItem(match), 50)
-      },
-      dom.span(
-        {
-          className: "line-number",
-          key: `${match.line}`
-        },
-        match.line
-      ),
-      this.renderMatchValue(match.value)
+    return (
+      <div
+        className={classnames("result", { focused })}
+        onClick={() => setTimeout(() => this.selectMatchItem(match), 50)}
+      >
+        <span className="line-number" key={match.line}>
+          {match.line}
+        </span>
+        {this.renderMatchValue(match.value)}
+      </div>
     );
   }
 
@@ -128,63 +127,68 @@ export default class TextSearch extends Component {
     matchIndexes.forEach((matchIndex, index) => {
       if (matchIndex > 0 && index === 0) {
         matches.push(
-          dom.span({ className: "line-match" }, value.slice(0, matchIndex))
+          <span className="line-match">
+            {value.slice(0, matchIndex)}
+          </span>
         );
       }
       if (matchIndex > matchIndexes[index - 1] + len) {
         matches.push(
-          dom.span(
-            { className: "line-match" },
-            value.slice(matchIndexes[index - 1] + len, matchIndex)
-          )
+          <span className="line-match">
+            {value.slice(matchIndexes[index - 1] + len, matchIndex)}
+          </span>
         );
       }
       matches.push(
-        dom.span(
-          { className: "query-match", key: index },
-          value.substr(matchIndex, len)
-        )
+        <span className="query-match" key={index}>
+          {value.substr(matchIndex, len)}
+        </span>
       );
       if (index === matchIndexes.length - 1) {
         matches.push(
-          dom.span(
-            {
-              className: "line-match"
-            },
-            value.slice(matchIndex + len, value.length)
-          )
+          <span className="line-match">
+            {value.slice(matchIndex + len, value.length)}
+          </span>
         );
       }
     });
 
-    return dom.span({ className: "line-value" }, ...matches);
+    return (
+      <span className="line-value">
+        {matches}
+      </span>
+    );
   }
 
   renderResults() {
     const { results } = this.props;
     results = results.filter(result => result.matches.length > 0);
+
     function getFilePath(item) {
       return item.filepath
         ? `${item.sourceId}`
         : `${item.sourceId}-${item.line}-${item.column}`;
     }
 
-    return ManagedTree({
-      getRoots: () => results,
-      getChildren: file => {
-        return file.matches || [];
-      },
-      itemHeight: 20,
-      autoExpand: 1,
-      autoExpandDepth: 1,
-      focused: results[0],
-      getParent: item => null,
-      getPath: getFilePath,
-      renderItem: (item, depth, focused, _, expanded, { setExpanded }) =>
-        item.filepath
-          ? this.renderFile(item, focused, expanded, setExpanded)
-          : this.renderMatch(item, focused)
-    });
+    function renderItem(item, depth, focused, _, expanded, { setExpanded }) {
+      return item.filepath
+        ? this.renderFile(item, focused, expanded, setExpanded)
+        : this.renderMatch(item, focused);
+    }
+
+    return (
+      <ManagedTree
+        getRoots={() => results}
+        getChildren={file => file.matches || []}
+        itemHeight={20}
+        autoExpand={1}
+        autoExpandDepth={1}
+        focused={results[0]}
+        getParent={item => null}
+        getPath={getFilePath}
+        renderItem={renderItem}
+      />
+    );
   }
 
   resultCount() {
@@ -202,30 +206,31 @@ export default class TextSearch extends Component {
       resultCount
     );
 
-    return SearchInput({
-      query: this.state.inputValue,
-      count: resultCount,
-      placeholder: L10N.getStr("projectTextSearch.placeholder"),
-      size: "big",
-      summaryMsg,
-      onChange: e => this.inputOnChange(e),
-      onFocus: () => this.setState({ focused: true }),
-      onBlur: () => this.setState({ focused: false }),
-      onKeyDown: e => this.onKeyDown(e),
-      handleClose: this.close,
-      ref: "searchInput"
-    });
+    return (
+      <SearchInput
+        query={this.state.inputValue}
+        count={resultCount}
+        placeholder={L10N.getStr("projectTextSearch.placeholder")}
+        size="big"
+        summaryMsg={summaryMsg}
+        onChange={e => this.inputOnChange(e)}
+        onFocus={() => this.setState({ focused: true })}
+        onBlur={() => this.setState({ focused: false })}
+        onKeyDown={e => this.onKeyDown(e)}
+        handleClose={this.close}
+        ref="searchInput"
+      />
+    );
   }
 
   render() {
     const { searchBottomBar } = this.props;
-    return dom.div(
-      {
-        className: "project-text-search"
-      },
-      this.renderInput(),
-      searchBottomBar,
-      this.renderResults()
+    return (
+      <div className="project-text-search">
+        {this.renderInput()}
+        {searchBottomBar}
+        {this.renderResults()}
+      </div>
     );
   }
 }

src/components/ProjectSearch/SourceSearch.js
@@ -1,10 +1,9 @@
-import { Component, PropTypes, createFactory } from "react";
+import React, { Component, PropTypes } from "react";
 
 import { isPretty, getSourcePath } from "../../utils/source";
 import { endTruncateStr } from "../../utils/utils";
 
-import _Autocomplete from "../shared/Autocomplete";
-const Autocomplete = createFactory(_Autocomplete);
+import Autocomplete from "../shared/Autocomplete";
 
 import type { SourcesMap } from "../../reducers/sources";
 
@@ -67,18 +66,21 @@ export default class SourceSearch extends Component {
 
   render() {
     const { sources, searchBottomBar, selectSource } = this.props;
-    return Autocomplete({
-      selectItem: (e, result) => {
-        selectSource(result.id);
-        this.close();
-      },
-      close: this.close,
-      items: this.searchResults(sources),
-      inputValue: this.state.inputValue,
-      placeholder: L10N.getStr("sourceSearch.search"),
-      size: "big",
-      children: searchBottomBar
-    });
+    return (
+      <Autocomplete
+        selectItem={(e, result) => {
+          selectSource(result.id);
+          this.close();
+        }}
+        close={this.close}
+        items={this.searchResults(sources)}
+        inputValue={this.state.inputValue}
+        placeholder={L10N.getStr("sourceSearch.search")}
+        size="big"
+      >
+        {searchBottomBar}
+      </Autocomplete>
+    );
   }
 }
 

src/components/ProjectSearch/ToggleSearch.js
@@ -1,26 +1,25 @@
-import { Component, DOM as dom, PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 import classnames from "classnames";
 
 export default class ToggleSearch extends Component {
   render() {
     const { kind, toggle } = this.props;
     const isSourcesActive = kind === "sources";
-    return dom.div(
-      { className: "toggle-search" },
-      dom.span(
-        {
-          className: classnames("text", { active: isSourcesActive }),
-          onClick: toggle
-        },
-        L10N.getStr("sourceSearch.search")
-      ),
-      dom.span(
-        {
-          className: classnames("text", { active: !isSourcesActive }),
-          onClick: toggle
-        },
-        L10N.getStr("projectTextSearch.placeholder")
-      )
+    return (
+      <div className="toggle-search">
+        <span
+          className={classnames("text", { active: isSourcesActive })}
+          onClick={toggle}
+        >
+          {L10N.getStr("sourceSearch.search")}
+        </span>
+        <span
+          className={classnames("text", { active: !isSourcesActive })}
+          onClick={toggle}
+        >
+          {L10N.getStr("projectTextSearch.placeholder")}
+        </span>
+      </div>
     );
   }
 }

src/components/ProjectSearch/index.js
@@ -1,10 +1,14 @@
 // @flow
 
-import { DOM as dom, PropTypes, Component, createFactory } from "react";
+import React, { PropTypes, Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import actions from "../../actions";
 import { isEnabled } from "devtools-config";
+import TextSearch from "./TextSearch";
+import SourceSearch from "./SourceSearch";
+import ToggleSearch from "./ToggleSearch";
+
 import {
   getSources,
   getActiveSearchState,
@@ -13,15 +17,6 @@ import {
 } from "../../selectors";
 
 import "./ProjectSearch.css";
-
-import _TextSearch from "./TextSearch";
-const TextSearch = createFactory(_TextSearch);
-
-import _SourceSearch from "./SourceSearch";
-const SourceSearch = createFactory(_SourceSearch);
-
-import _ToggleSearch from "./ToggleSearch";
-const ToggleSearch = createFactory(_ToggleSearch);
 
 class ProjectSearch extends Component {
   state: Object;
@@ -105,15 +100,16 @@ class ProjectSearch extends Component {
 
   renderSourceSearch() {
     const { sources, selectSource, closeActiveSearch } = this.props;
-    return SourceSearch({
-      sources,
-      selectSource,
-      closeActiveSearch,
-      searchBottomBar: ToggleSearch({
-        kind: "sources",
-        toggle: this.toggleProjectTextSearch
-      })
-    });
+    return (
+      <SourceSearch
+        sources={sources}
+        selectSource={selectSource}
+        closeActiveSearch={closeActiveSearch}
+        searchBottomBar={
+          <ToggleSearch kind="sources" toggle={this.toggleProjectTextSearch} />
+        }
+      />
+    );
   }
 
   renderTextSearch() {
@@ -126,18 +122,19 @@ class ProjectSearch extends Component {
       query
     } = this.props;
 
-    return TextSearch({
-      sources,
-      results: results.valueSeq().toJS(),
-      searchSources,
-      closeActiveSearch,
-      selectSource,
-      query,
-      searchBottomBar: ToggleSearch({
-        kind: "project",
-        toggle: this.toggleSourceSearch
-      })
-    });
+    return (
+      <TextSearch
+        sources={sources}
+        results={results.valueSeq().toJS()}
+        searchSources={searchSources}
+        closeActiveSearch={closeActiveSearch}
+        selectSource={selectSource}
+        query={query}
+        searchBottomBar={
+          <ToggleSearch kind="project" toggle={this.toggleSourceSearch} />
+        }
+      />
+    );
   }
 
   render() {
@@ -145,11 +142,12 @@ class ProjectSearch extends Component {
       return null;
     }
 
-    return dom.div(
-      { className: "search-container" },
-      this.isProjectSearchEnabled()
-        ? this.renderTextSearch()
-        : this.renderSourceSearch()
+    return (
+      <div className="search-container">
+        {this.isProjectSearchEnabled()
+          ? this.renderTextSearch()
+          : this.renderSourceSearch()}
+      </div>
     );
   }
 }

commit cb2e38a6a8680655140912272b343fb0f2d20b94

fix problems introduced in text search
* fixes issues introduced by
  https://github.com/devtools-html/debugger.html/pull/3636

src/components/ProjectSearch/TextSearch.js
@@ -1,8 +1,9 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component, DOM as dom, PropTypes } from "react";
 import classnames from "classnames";
 
 import escapeRegExp from "lodash/escapeRegExp";
 import Svg from "../shared/Svg";
+
 import ManagedTree from "../shared/ManagedTree";
 import SearchInput from "../shared/SearchInput";
 
@@ -71,8 +72,14 @@ export default class TextSearch extends Component {
     if (focused) {
       this.focused = { setExpanded, file, expanded };
     }
-    const Arrow = Svg("arrow");
-    const File = Svg("file");
+
+    const svgArrow = Svg("arrow", {
+      className: classnames({
+        expanded
+      })
+    });
+
+    const svgFile = Svg("file");
 
     const matches = ` (${file.matches.length} match${file.matches.length > 1
       ? "es"
@@ -81,11 +88,11 @@ export default class TextSearch extends Component {
     return (
       <div
         className={classnames("file-result", { focused })}
-        key={file.id}
+        key={file.sourceId}
         onClick={e => setExpanded(file, !expanded)}
       >
-        <Arrow className={classnames({ expanded })} />
-        <File />
+        {svgArrow}
+        {svgFile}
         <span className="file-path">
           {getRelativePath(file.filepath)}
         </span>
@@ -153,28 +160,23 @@ export default class TextSearch extends Component {
       }
     });
 
-    return (
-      <span className="line-value">
-        {matches}
-      </span>
-    );
+    return dom.span({ className: "line-value" }, ...matches);
   }
 
   renderResults() {
     const { results } = this.props;
     results = results.filter(result => result.matches.length > 0);
-
     function getFilePath(item) {
       return item.filepath
         ? `${item.sourceId}`
         : `${item.sourceId}-${item.line}-${item.column}`;
     }
 
-    function renderItem(item, depth, focused, _, expanded, { setExpanded }) {
+    const renderItem = (item, depth, focused, _, expanded, { setExpanded }) => {
       return item.filepath
         ? this.renderFile(item, focused, expanded, setExpanded)
         : this.renderMatch(item, focused);
-    }
+    };
 
     return (
       <ManagedTree

src/components/shared/ManagedTree.js
@@ -25,7 +25,7 @@ type Props = {
   onExpand?: (item: any) => void,
   onCollapse?: (item: any) => void,
   renderItem: any,
-  disabledFocus: boolean
+  disabledFocus?: boolean
 };
 
 type ManagedTreeState = {