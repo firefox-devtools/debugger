f9720086c5cd6830d564e379c90e8387acc8f34e
David Walsh <davidwalsh83@gmail.com>
2017-08-24 15:16:28 -0500

    Add React for CallSites JSX (#3774)

--- src/components/Editor/CallSites.js
+++ src/components/Editor/CallSites.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { isEnabled } from "devtools-config";
