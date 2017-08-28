168dfe23377e3768bc9b55d60fdfafcc5d6d4b53
Martin Schmid <martin.schmid@live.com>
2017-08-28 20:26:11 +0200

    add basic component skeleton

---
+++ src/components/SecondaryPanes/Workers.js
@@ -0,0 +1,20 @@
+// @flow
+import React, { Component } from "react";
+import "./Workers.css";
+
+class Workers extends Component {
+  constructor(...args) {
+    super(...args);
+  }
+
+  render() {
+    return (
+      <div>
+        dummy text
+      </div>
+    );
+  }
+}
+
+Workers.displayName = "Workers";
+export default Workers;
