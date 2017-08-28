fd04f6ce13eb0b50097557ce518daf737a8d6424
codehag <yulia.startsev@gmail.com>
2017-08-28 20:09:57 +0200

    update tests

--- src/actions/tests/breakpoints/syncing.js
+++ src/actions/tests/breakpoints/syncing.js
@@ -1,14 +1,7 @@
-import {
-  generateBreakpoint,
-  mockPendingBreakpoint
-} from "../helpers/breakpoints.js";
-
-import {
-  simulateCorrectThreadClient,
-  simpleMockThreadClient
-} from "../helpers/threadClient.js";
-
-import { prefs } from "../../../utils/prefs";
+jest.mock("../../../utils/source-maps", () => ({
+  getGeneratedLocation: jest.fn()
+}));
+import { getGeneratedLocation } from "../../../utils/source-maps";
 
 jest.mock("../../../utils/prefs", () => ({
   prefs: {
@@ -18,17 +11,11 @@ jest.mock("../../../utils/prefs", () => ({
   clear: jest.fn()
 }));
 
-import {
-  createStore,
-  selectors,
-  actions,
-  makeSource
-} from "../../../utils/test-head";
+import { createStore, actions, makeSource } from "../../../utils/test-head";
 
-import {
-  makePendingLocationId,
-  makeLocationId
-} from "../../../utils/breakpoint";
+import { makeLocationId } from "../../../utils/breakpoint";
+
+import { syncClientBreakpoint } from "../../breakpoints/syncBreakpoint.js";
 
 function setBreakpoint(location, condition) {
   const actualLocation = Object.assign({}, location, {
@@ -42,20 +29,80 @@ function setBreakpoint(location, condition) {
   });
 }
 
-describe("reloading", () => {
+const clientBreakpoint = {
+  actualLocation: {
+    sourceUrl: "http://localhost:8000/gen.js",
+    sourceId: "gen.js",
+    line: 3,
+    column: undefined
+  }
+};
+
+const threadClient = {
+  getBreakpointByLocation: () => clientBreakpoint,
+  setBreakpoint,
+  removeBreakpoint: jest.fn()
+};
+
+const sourceMaps = {
+  getOriginalLocation: () => ({}),
+  isOriginalId: () => true,
+  getGeneratedLocation: () => ({})
+};
+
+function pendingBreakpoint(overrides) {
+  return {
+    location: {
+      sourceId: "magic.js",
+      sourceUrl: "http://localhost:8000//magic.js",
+      line: 3,
+      column: undefined
+    },
+    generatedLocation: {
+      sourceId: "gen.js",
+      sourceUrl: "http://localhost:8000//gen.js",
+      line: 3,
+      column: undefined
+    },
+    condition: null,
+    disabled: false,
+    hidden: false,
+    ...overrides
+  };
+}
+
+function newGeneratedLocation(line) {
+  return {
+    sourceUrl: "http://localhost:8000/gen.js",
+    sourceId: "gen.js",
+    line,
+    column: undefined
+  };
+}
+
+describe("reloading debuggee", () => {
+  it("syncs with unchanged source", async () => {
+    getGeneratedLocation.mockImplementation(() => newGeneratedLocation(3));
+
+    const { dispatch, getState } = createStore(threadClient, {}, sourceMaps);
+
+    // add a source without the breakpoints
+    const reloadedSource = makeSource("magic.js");
+    await dispatch(actions.newSource(reloadedSource));
+
+    // manually sync
+    const update = await syncClientBreakpoint(
+      getState,
+      threadClient,
+      sourceMaps,
+      reloadedSource,
+      pendingBreakpoint()
+    );
+    expect(threadClient.removeBreakpoint.mock.calls.length).toBe(0);
+    expect(update).toMatchSnapshot();
+  });
+
   it("updates a corresponding breakpoint for a changed source", async () => {
-    const threadClient = {
-      getBreakpointByLocation: () => clientBreakpoint,
-      setBreakpoint,
-      removeBreakpoint: jest.fn()
-    };
-
-    const sourceMaps = {
-      getOriginalLocation: () => originalLocation,
-      isOriginalId: () => true,
-      getGeneratedLocation: () => ({})
-    };
-
     /*
       This is a complicated test, so bear with us. In this scenario,
 
@@ -63,80 +110,33 @@ describe("reloading", () => {
       and original file magic.js.
       2. The user adds a breakpoint in magic.js#3, which maps to gen.js#3
       3. The user edits their code and reloads
-      4. when magic.js is added, the debugger attempts to sync the saved breakpoint
+      4. when magic.js is added, the debugger syncs the saved breakpoint
          4.a. the debugger checks to see if gen.js#3 still points to magic.js#3,
-              unfortunately it now points to gen.js#1 so it removes the old
+              it now points to gen.js#1 so it removes the old
               breakpoint and creates a new one
     */
-    const newSource = {
-      id: "magic.js",
-      url: "http://localhost:8000//magic.js"
-    };
 
-    const breakpoint = {
-      location: {
-        sourceUrl: "http://localhost:8000/magic.js",
-        sourceId: "gen.js",
-        line: 3,
-        column: undefined
-      },
-      generatedLocation: {
-        sourceUrl: "http://localhost:8000/gen.js",
-        sourceId: "gen.js",
-        line: 3,
-        column: undefined
-      },
-      condition: null,
-      disabled: false,
-      hidden: false
-    };
+    // here we are mocking out what happens when the source changed, and the
+    // new line for originalSource line 3, is the generated Source line 5
 
-    const pendingBreakpoint = {
-      location: {
-        sourceUrl: "http://localhost:8000//magic.js",
-        line: 3,
-        column: undefined
-      },
-      generatedLocation: {
-        sourceUrl: "http://localhost:8000//gen.js",
-        line: 3,
-        column: undefined
-      },
-      condition: null,
-      disabled: false,
-      hidden: false
-    };
+    getGeneratedLocation.mockImplementation(() => newGeneratedLocation(5));
+    // end mocking out
 
-    const originalLocation = {
-      sourceUrl: "http://localhost:8000/magic.js",
-      sourceId: "magic.js",
-      line: 5,
-      column: undefined
-    };
-
-    const clientBreakpoint = {
-      actualLocation: {
-        sourceUrl: "http://localhost:8000/gen.js",
-        sourceId: "gen.js",
-        line: 3,
-        column: undefined
-      }
-    };
-
-    const id = makePendingLocationId(pendingBreakpoint.location);
-    prefs.pendingBreakpoints = { [id]: pendingBreakpoint };
     const { dispatch, getState } = createStore(threadClient, {}, sourceMaps);
 
-    const startBps = selectors.getBreakpoints(getState());
-    expect(startBps.size).toBe(0);
+    // add a source without the breakpoints
+    const reloadedSource = makeSource("magic.js");
+    await dispatch(actions.newSource(reloadedSource));
 
-    await dispatch(actions.newSource(newSource));
-
-    // expect(threadClient.removeBreakpoint.mock.calls.length).toBe(1);
-    //
-    // const endBps = selectors.getBreakpoints(getState());
-    // const expectedId = makeLocationId(bp.location);
-    // const returnedBp = endBps.get(expectedId);
-    // expect(returnedBp).toMatchSnapshot();
+    // manually sync
+    const update = await syncClientBreakpoint(
+      getState,
+      threadClient,
+      sourceMaps,
+      reloadedSource,
+      pendingBreakpoint()
+    );
+    expect(threadClient.removeBreakpoint.mock.calls.length).toBe(1);
+    expect(update).toMatchSnapshot();
   });
 });
