414fd9737af551f5266c70f6cfe835a877cb956b
Jason Laster <jason.laster.11@gmail.com>
2017-08-29 11:28:18 -0400

    cleanup util

--- src/utils/parser/getEmptyLines.js
+++ src/utils/parser/getEmptyLines.js
@@ -1,7 +1,31 @@
-import { uniq } from "lodash";
+import { chain, difference } from "lodash";
 import { getAst } from "./utils/ast";
 
 const commentTokens = ["CommentBlock", "CommentLine"];
+
+function fillRange(start, end) {
+  return Array(end - start + 1).fill().map((item, index) => start + index);
+}
+
+// Populates a pre-filled array of every line number,
+// then removes lines which were found to be executable
+function getLines(ast) {
+  return fillRange(0, ast.tokens[ast.tokens.length - 1].loc.end.line);
+}
+
+// The following sequence stores lines which have executable code
+// (contents other than comments or EOF, regardless of line position)
+function getExecutableLines(ast) {
+  return chain(ast.tokens)
+    .filter(
+      token =>
+        !commentTokens.includes(token.type) &&
+        (!token.type || (token.type.label && token.type.label != "eof"))
+    )
+    .map(token => token.loc.start.line - 1)
+    .uniq()
+    .value();
+}
 
 export default function getEmptyLines(sourceToJS) {
   if (!sourceToJS) {
@@ -13,24 +37,7 @@ export default function getEmptyLines(sourceToJS) {
     return [];
   }
 
-  // The following sequence stores lines which have executable code
-  // (contents other than comments or EOF, regardless of line position)
-  const executableLines = ast.tokens
-    .filter(token => {
-      return (
-        !commentTokens.includes(token.type) &&
-        (!token.type || (token.type.label && token.type.label != "eof"))
-      );
-    })
-    .map(token => token.loc.start.line - 1);
-  executableLines = uniq(executableLines);
-
-  // Populates a pre-filled array of every line number,
-  // then removes lines which were found to be executable
-  const fillRange = (start, end) => {
-    return Array(end - start + 1).fill().map((item, index) => start + index);
-  };
-  const allLines = fillRange(0, ast.tokens[ast.tokens.length - 1].loc.end.line);
-
-  return allLines.filter(i => !executableLines.includes(i));
+  const executableLines = getExecutableLines(ast);
+  const lines = getLines(ast);
+  return difference(lines, executableLines);
 }
