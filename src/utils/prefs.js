584becb3af83aece36370dfcd953f8e37e36a190
Tom Tromey <tom@tromey.com>
2017-08-30 14:25:36 -0600

    Remove devtools.debugger.client-source-maps-enabled pref
    This removes the debugger.debugger.client-source-maps-enabled pref.
    There was no UI for disabling this pref, and it is only checked in a
    single spot.  In M-C I am going to unify all the source-map prefs, and I
    don't think the debugger needs to react to changes to the shared pref;
    so removing this one seemed best.

--- src/utils/prefs.js
+++ src/utils/prefs.js
@@ -8,7 +8,6 @@ const prefsSchemaVersion = "1.0.2";
 const pref = Services.pref;
 
 if (isDevelopment()) {
-  pref("devtools.debugger.client-source-maps-enabled", true);
   pref("devtools.debugger.pause-on-exceptions", false);
   pref("devtools.debugger.ignore-caught-exceptions", false);
   pref("devtools.debugger.call-stack-visible", false);
@@ -29,7 +28,6 @@ if (isDevelopment()) {
 }
 
 export const prefs = new PrefsHelper("devtools", {
-  clientSourceMapsEnabled: ["Bool", "debugger.client-source-maps-enabled"],
   pauseOnExceptions: ["Bool", "debugger.pause-on-exceptions"],
   ignoreCaughtExceptions: ["Bool", "debugger.ignore-caught-exceptions"],
   callStackVisible: ["Bool", "debugger.call-stack-visible"],
