0ba5c4c5e97339b8e0fbd4cb949e5c05b7ceaf88
codehag <yulia.startsev@gmail.com>
2017-09-01 11:19:05 -0600

    clear ast tree on navigate

--- src/utils/parser/worker.js
+++ src/utils/parser/worker.js
@@ -1,6 +1,7 @@
 import { getClosestExpression } from "./utils/closest";
 import { getVariablesInScope } from "./scopes";
 import getSymbols, { clearSymbols } from "./getSymbols";
+import { clearASTs } from "./utils/ast";
 import getOutOfScopeLocations from "./getOutOfScopeLocations";
 import { getNextStep } from "./steps";
 import getEmptyLines from "./getEmptyLines";
@@ -13,6 +14,7 @@ self.onmessage = workerHandler({
   getOutOfScopeLocations,
   getSymbols,
   clearSymbols,
+  clearASTs,
   getVariablesInScope,
   getNextStep,
   getEmptyLines
