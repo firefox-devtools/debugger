37a2b85b2e9e896d83a7855d1096d7fd1bf24926
Julian Descottes <julian.descottes@gmail.com>
2017-09-16 00:47:59 +0200

    synchronize prefs file in GitHub with m-c

--- assets/panel/prefs.js
+++ assets/panel/prefs.js
@@ -1,16 +1,18 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-#ifdef RELEASE_OR_BETA
-pref("devtools.debugger.new-debugger-frontend", false);
-#else
-pref("devtools.debugger.new-debugger-frontend", true);
+// copy-assets-start
+// This is just a workaround for the old docker image which expects a file with
+// preprocessing.
+#if True
 #endif
+// copy-assets-end
+
+pref("devtools.debugger.new-debugger-frontend", true);
 
 // Enable the Debugger
 pref("devtools.debugger.enabled", true);
 pref("devtools.debugger.chrome-debugging-host", "localhost");
-pref("devtools.debugger.chrome-debugging-port", 6080);
 pref("devtools.debugger.chrome-debugging-websocket", false);
 pref("devtools.debugger.remote-host", "localhost");
 pref("devtools.debugger.remote-timeout", 20000);
