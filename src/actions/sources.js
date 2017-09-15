1805081a2df4fd5890a91596765c22c9c52ed50b
Jason Laster <jason.laster.11@gmail.com>
2017-09-14 22:40:01 -0400

    [ProjectSearch] Fix initial search (#4016)
    * Fix project search
    
    * fix the failing tests
    
    * fix lint issues
    
    * fix warning for required prop

--- src/actions/sources.js
+++ src/actions/sources.js
@@ -24,6 +24,7 @@ import { loadSourceText } from "./sources/loadSourceText";
 
 import { prefs } from "../utils/prefs";
 import { removeDocument } from "../utils/editor";
+import { isThirdParty } from "../utils/source";
 import { getGeneratedLocation } from "../utils/source-maps";
 import * as parser from "../utils/parser";
 
@@ -127,13 +128,17 @@ function loadSourceMap(generatedSource) {
     }
 
     const state = getState();
-    const originalSources = urls.map(originalUrl => {
-      return {
-        url: originalUrl,
-        id: sourceMaps.generatedToOriginalId(generatedSource.id, originalUrl),
-        isPrettyPrinted: false
-      };
-    });
+    const originalSources = urls.map(
+      originalUrl =>
+        ({
+          url: originalUrl,
+          id: sourceMaps.generatedToOriginalId(generatedSource.id, originalUrl),
+          isPrettyPrinted: false,
+          isWasm: false,
+          isBlackBoxed: false,
+          loadedState: "unloaded"
+        }: Source)
+    );
 
     dispatch({ type: "ADD_SOURCES", sources: originalSources });
 
@@ -375,11 +380,15 @@ export function loadAllSources() {
     const query = getTextSearchQuery(getState());
     for (const [, src] of sources) {
       const source = src.toJS();
+      if (isThirdParty(source)) {
+        continue;
+      }
+
       await dispatch(loadSourceText(source));
       // If there is a current search query we search
       // each of the source texts as they get loaded
       if (query) {
-        await dispatch(searchSource(source, query));
+        await dispatch(searchSource(source.id, query));
       }
     }
   };
