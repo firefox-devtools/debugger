81b08cbe872d38bb78ee04bb1a4159bf66c4ef73
Jason Laster <jason.laster.11@gmail.com>
2017-09-16 22:57:58 -0400

    jh

--- bin/ci/clone-gecko.sh
+++ bin/ci/clone-gecko.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 hg --version
-if [ ! -d "gecko/.hg" ]; then
-  rm -rf gecko/
-  hg clone https://hg.mozilla.org/mozilla-unified/ gecko
-else
-  cd gecko
-  hg strip --no-backup 'roots(outgoing())'
-  hg pull -u
-  cd ..
-fi
+rm -rf firefox/
+hg clone https://hg.mozilla.org/mozilla-unified/ firefox
+
+cd firefox
+hg co $MC_COMMIT
+cd ..
