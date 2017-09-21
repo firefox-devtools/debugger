58be341d5ae4699cc9ed39fba44e6a51b7c52a00
zacck <coderv63@gmail.com>
2017-09-21 16:48:34 +0200

    remove rebase botches

--- src/components/SecondaryPanes/Scopes.js
+++ src/components/SecondaryPanes/Scopes.js
@@ -58,6 +58,7 @@ class Scopes extends PureComponent {
             autoExpandDepth={1}
             getObjectProperties={id => loadedObjects[id]}
             loadObjectProperties={loadObjectProperties}
+            openLink={openLink}
             disableWrap={true}
             disabledFocus={true}
             dimTopLevelWindow={true}
--- src/components/shared/Button/PaneToggle.js
+++ src/components/shared/Button/PaneToggle.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from "react";
 import classnames from "classnames";
 import Svg from "../Svg";
