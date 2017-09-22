1d43b3c8e53bf2a364d3ea38fa3e67d8038ee423
Johnny Khalil <khal0988@mylaurier.ca>
2017-09-22 13:56:32 -0400

    [Accessibility] "Collapse panes" buttons should be accessible Enter/Space collapses/expands panes Buttons are reachable with the keyboard Buttons have hover/focus styling

--- src/components/shared/Button/PaneToggle.js
+++ src/components/shared/Button/PaneToggle.js
@@ -30,7 +30,7 @@ class PaneToggleButton extends Component {
       : L10N.getStr("collapsePanes");
 
     return (
-      <div
+      <button
         className={classnames(`toggle-button-${position}`, {
           collapsed,
           vertical: horizontal != null ? !horizontal : false
@@ -39,7 +39,7 @@ class PaneToggleButton extends Component {
         title={title}
       >
         <Svg name="togglePanes" />
-      </div>
+      </button>
     );
   }
 }
