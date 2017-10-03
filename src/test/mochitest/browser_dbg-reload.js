cc21f4591d21c5466bee03648fac68f2e8f3676d
Jason Laster <jason.laster.11@gmail.com>
2017-10-03 09:42:02 -0400

    remove unnecessary line

--- src/test/mochitest/browser_dbg-reload.js
+++ src/test/mochitest/browser_dbg-reload.js
@@ -10,18 +10,17 @@
 add_task(async function() {
   const dbg = await initDebugger("reload/doc_reload.html", "sjs_code_reload");
 
-
   const sym = waitForDispatch(dbg, "SET_SYMBOLS");
-  await selectSource(dbg, "sjs_code_reload")
+  await selectSource(dbg, "sjs_code_reload");
   await sym;
 
   await addBreakpoint(dbg, "sjs_code_reload", 2);
 
-  const sync = waitForDispatch(dbg, "SYNC_BREAKPOINT")
-  await reload(dbg,  "sjs_code_reload")
+  const sync = waitForDispatch(dbg, "SYNC_BREAKPOINT");
+  await reload(dbg,  "sjs_code_reload");
   await sync;
 
-  const breakpoints = dbg.selectors.getBreakpoints(dbg.getState())
+  const breakpoints = dbg.selectors.getBreakpoints(dbg.getState());
   const breakpointList = breakpoints.valueSeq().toJS();
   const breakpoint = breakpointList[0];
 
