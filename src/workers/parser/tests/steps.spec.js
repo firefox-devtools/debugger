ede1e2705aa8d28471dab6672427144435548b6f
Jack Cross <jackjocross@gmail.com>
2017-10-05 09:42:59 -0400

    Added step over functionality for yield (#4232) (#4276)
    * Added step over functionality for yield (#4232)
    
    * Consistent parser steps naming and generators fixture

--- src/workers/parser/tests/steps.spec.js
+++ src/workers/parser/tests/steps.spec.js
@@ -2,34 +2,53 @@ import { getNextStep } from "../steps";
 import { getSource } from "./helpers";
 
 describe("getNextStep", () => {
-  it("first await call", () => {
-    const source = getSource("async");
-    const pausePosition = { line: 8, column: 2, sourceId: "async" };
-    expect(getNextStep(source, pausePosition)).toEqual({
-      ...pausePosition,
-      line: 9
+  describe("await", () => {
+    it("first await call", () => {
+      const source = getSource("async");
+      const pausePosition = { line: 8, column: 2, sourceId: "async" };
+      expect(getNextStep(source, pausePosition)).toEqual({
+        ...pausePosition,
+        line: 9
+      });
+    });
+
+    it("first await call expression", () => {
+      const source = getSource("async");
+      const pausePosition = { line: 8, column: 9, sourceId: "async" };
+      expect(getNextStep(source, pausePosition)).toEqual({
+        ...pausePosition,
+        line: 9,
+        column: 2
+      });
+    });
+
+    it("second await call", () => {
+      const source = getSource("async");
+      const pausePosition = { line: 9, column: 2, sourceId: "async" };
+      expect(getNextStep(source, pausePosition)).toEqual(null);
+    });
+
+    it("second call expression", () => {
+      const source = getSource("async");
+      const pausePosition = { line: 9, column: 9, sourceId: "async" };
+      expect(getNextStep(source, pausePosition)).toEqual(null);
     });
   });
 
-  it("first await call expression", () => {
-    const source = getSource("async");
-    const pausePosition = { line: 8, column: 9, sourceId: "async" };
-    expect(getNextStep(source, pausePosition)).toEqual({
-      ...pausePosition,
-      line: 9,
-      column: 2
+  describe("yield", () => {
+    it("first yield call", () => {
+      const source = getSource("generators");
+      const pausePosition = { line: 2, column: 2, sourceId: "generators" };
+      expect(getNextStep(source, pausePosition)).toEqual({
+        ...pausePosition,
+        line: 3
+      });
     });
-  });
 
-  it("second await call", () => {
-    const source = getSource("async");
-    const pausePosition = { line: 9, column: 2, sourceId: "async" };
-    expect(getNextStep(source, pausePosition)).toEqual(null);
-  });
-
-  it("second call expression", () => {
-    const source = getSource("async");
-    const pausePosition = { line: 9, column: 9, sourceId: "async" };
-    expect(getNextStep(source, pausePosition)).toEqual(null);
+    it("second yield call", () => {
+      const source = getSource("generators");
+      const pausePosition = { line: 3, column: 2, sourceId: "generators" };
+      expect(getNextStep(source, pausePosition)).toEqual(null);
+    });
   });
 });
