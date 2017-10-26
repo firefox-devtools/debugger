3f579fbd1ed770b836bc5f13ee0970904a761734
Fischer-L <Fischer-L@users.noreply.github.com>
2017-10-26 23:45:25 +0800

    [Preview] Some lines are incorrectly marked as out of scope

--- src/workers/parser/utils/contains.js
+++ src/workers/parser/utils/contains.js
@@ -3,14 +3,26 @@
 import type { AstLocation, AstPosition } from "../types";
 import type { Node } from "babel-traverse";
 
-export function containsPosition(a: AstLocation, b: AstPosition) {
-  const startsBefore =
-    a.start.line < b.line ||
-    (a.start.line === b.line && a.start.column <= b.column);
-  const endsAfter =
-    a.end.line > b.line || (a.end.line === b.line && a.end.column >= b.column);
+function startsBefore(a: AstLocation, b: AstPosition) {
+  let before = a.start.line < b.line;
+  if (a.start.line === b.line) {
+    before =
+      a.start.column >= 0 && b.column >= 0 ? a.start.column <= b.column : true;
+  }
+  return before;
+}
 
-  return startsBefore && endsAfter;
+function endsAfter(a: AstLocation, b: AstPosition) {
+  let after = a.end.line > b.line;
+  if (a.end.line === b.line) {
+    after =
+      a.end.column >= 0 && b.column >= 0 ? a.end.column >= b.column : true;
+  }
+  return after;
+}
+
+export function containsPosition(a: AstLocation, b: AstPosition) {
+  return startsBefore(a, b) && endsAfter(a, b);
 }
 
 export function containsLocation(a: AstLocation, b: AstLocation) {
