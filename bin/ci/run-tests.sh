c781f3c58fc358f2efe13fc7fee152bd0c51e796
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-03 17:26:50 -0400

    added Mozilla license v2.0 to files in home folder

--- bin/ci/run-tests.sh
+++ bin/ci/run-tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
 
 node ./bin/copy-assets.js --mc firefox
 ./node_modules/.bin/mochii --mc ./firefox --default-test-path devtools/client/debugger/new
--- bin/post-checkout.js
+++ bin/post-checkout.js
@@ -1,8 +1,11 @@
-const [origHead, head, flag] = process.env.GIT_PARAMS.split(' ');
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+const [origHead, head, flag] = process.env.GIT_PARAMS.split(" ");
 
 // Flag is 1 if we moved between branches. Flag is 0 if we merely checked out a file from another branch.
-if (flag !== '1') {
+if (flag !== "1") {
   process.exit();
 }
 
-require('./check-warn-yarn-changed.js')(origHead, head);
+require("./check-warn-yarn-changed.js")(origHead, head);
