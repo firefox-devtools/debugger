3087cd4e41ec8ea8fc8beb5ca874ccdd5a852911
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-05 20:11:22 -0500

    added license header to some files in src/test

--- src/test/mochitest/browser_dbg-console.js
+++ src/test/mochitest/browser_dbg-console.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 // Return a promise with a reference to jsterm, opening the split
 // console if necessary.  This cleans up the split console pref so
 // it won't pollute other tests.
