1e081bc5641ca21db3c3fe909b7610110358f25a
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-05 19:57:22 -0500

    added license header to .js files in src/components

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,5 +1,9 @@
 // @flow
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 import PropTypes from "prop-types";
 import React from "react";
 import type { Node as ReactNode } from "react";
