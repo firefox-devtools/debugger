d3e4c32f29b9eee52215acc40993ac7615ae442b
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-05 19:16:57 -0500

    added license header to .js files in src/actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
 // @flow
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 import * as breakpoints from "./breakpoints";
 import * as expressions from "./expressions";
 import * as eventListeners from "./event-listeners";
--- src/actions/pause/mapScopes.js
+++ src/actions/pause/mapScopes.js
@@ -1,5 +1,9 @@
 // @flow
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 import { getSource, getSelectedFrame } from "../../selectors";
 import { updateScopeBindings } from "../../utils/pause";
 import { isGeneratedId } from "devtools-source-map";
