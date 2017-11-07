e12186b556563e67ee8c1dd37094a1c1be5042bb
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-07 16:21:08 -0500

    Add a license header (#4601)

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,4 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 // @flow
+
 import { getSource, getActiveSearch, getPaneCollapse } from "../selectors";
 import type { ThunkArgs } from "./types";
 import type {
