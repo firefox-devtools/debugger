9f9e7e153c20302be6f18271bcdd31ef53d2ae87
Jason Laster <jason.laster.11@gmail.com>
2017-11-10 18:32:32 -0500

    tweaks

--- src/components/SecondaryPanes/BreakpointsDropdown.js
+++ src/components/SecondaryPanes/BreakpointsDropdown.js
@@ -5,87 +5,6 @@ import classnames from "classnames";
 
 import "./BreakpointsDropdown.css";
 
-function debugBtn(type, tooltip, className, state = "inactive") {
-  return (
-    <button className={classnames(className, state)}>
-      <img className={className} title={tooltip} />
-    </button>
-  );
-}
-
-function renderPause(isWaitingOnBreak) {
-  if (!isWaitingOnBreak) {
-    return debugBtn(
-      "pause",
-      L10N.getFormatStr("pauseButtonTooltip"),
-      "pause-next",
-      "active"
-    );
-  }
-  return debugBtn(
-    "pause",
-    L10N.getFormatStr("pauseButtonTooltip"),
-    "pause-next"
-  );
-}
-
-function renderPauseOnExceptions(
-  shouldPauseOnExceptions,
-  shouldIgnoreCaughtExceptions
-) {
-  if (shouldPauseOnExceptions && shouldIgnoreCaughtExceptions) {
-    return debugBtn(
-      "pause-exceptions",
-      L10N.getStr("pauseOnExceptionsTooltip"),
-      "pause-on-exceptions",
-      "active"
-    );
-  }
-  return debugBtn(
-    "pause-exceptions",
-    L10N.getStr("ignoreExceptionsTooltip"),
-    "pause-on-exceptions"
-  );
-}
-
-function renderPauseOnUncaughtExceptions(
-  shouldPauseOnExceptions,
-  shouldIgnoreCaughtExceptions
-) {
-  if (shouldPauseOnExceptions || shouldIgnoreCaughtExceptions) {
-    return debugBtn(
-      "pause-exceptions",
-      L10N.getStr("pauseOnUncaughtExceptionsTooltip"),
-      "pause-uncaught-exceptions",
-      "active"
-    );
-  }
-  return debugBtn(
-    "pause-exceptions",
-    L10N.getStr("ignoreExceptionsTooltip"),
-    "pause-uncaught-exceptions"
-  );
-}
-
-function renderIgnoreExceptions(
-  shouldPauseOnExceptions,
-  shouldIgnoreCaughtExceptions
-) {
-  if (!shouldPauseOnExceptions && !shouldIgnoreCaughtExceptions) {
-    return debugBtn(
-      "pause-exceptions",
-      L10N.getStr("ignoreExceptionsTooltip"),
-      "ignore-exceptions",
-      "active"
-    );
-  }
-  return debugBtn(
-    "pause-exceptions",
-    L10N.getStr("ignoreExceptionsTooltip"),
-    "ignore-exceptions"
-  );
-}
-
 function handleClick(e) {
   e.stopPropagation();
 }
@@ -100,32 +19,35 @@ export default function renderBreakpointsDropdown(
   const Panel = (
     <ul>
       <li onClick={() => breakOnNext()}>
-        {renderPause(isWaitingOnBreak)}
+        <div
+          className="pause-exceptions"
+          title={L10N.getStr("pauseButtonTooltip")}
+        />
         <span className="icon-spacer">{L10N.getStr("pauseButtonItem")}</span>
       </li>
       <li onClick={() => pauseOnExceptions(true, false)}>
-        {renderPauseOnUncaughtExceptions(
-          shouldPauseOnExceptions,
-          shouldIgnoreCaughtExceptions
-        )}
+        <div
+          className="pause-exceptions uncaught"
+          title={L10N.getStr("pauseOnUncaughtExceptionsTooltip")}
+        />
         <span className="icon-spacer">
           {L10N.getStr("pauseOnUncaughtExceptionsItem")}
         </span>
       </li>
       <li onClick={() => pauseOnExceptions(false, false)}>
-        {renderPauseOnExceptions(
-          shouldPauseOnExceptions,
-          shouldIgnoreCaughtExceptions
-        )}
+        <div
+          className="pause-exceptions.all"
+          title={L10N.getStr("pauseOnExceptionsTooltip")}
+        />
         <span className="icon-spacer">
           {L10N.getStr("pauseOnExceptionsItem")}
         </span>
       </li>
       <li onClick={() => pauseOnExceptions(true, true)}>
-        {renderIgnoreExceptions(
-          shouldPauseOnExceptions,
-          shouldIgnoreCaughtExceptions
-        )}
+        <div
+          className="pause-exceptions"
+          title={L10N.getStr("ignoreExceptionsTooltip")}
+        />
         <span className="icon-spacer">
           {L10N.getStr("ignoreExceptionsItem")}
         </span>
@@ -134,7 +56,7 @@ export default function renderBreakpointsDropdown(
   );
 
   return (
-    <div onClick={e => handleClick(e)}>
+    <div className="breakpoints-dropdown" onClick={e => handleClick(e)}>
       <Dropdown className="dropdown" panel={Panel} icon={<Svg name="plus" />} />
     </div>
   );
