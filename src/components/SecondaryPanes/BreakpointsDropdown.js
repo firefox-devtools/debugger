444ac4f6d84b9649cc2079f9f07b4ba733ed23d0
Gareth <garethjsharpe@gmail.com>
2017-11-15 01:17:04 -0500

    Update breakpoint conditional colouring, simplify css

--- src/components/SecondaryPanes/BreakpointsDropdown.js
+++ src/components/SecondaryPanes/BreakpointsDropdown.js
@@ -6,20 +6,18 @@ import classnames from "classnames";
 import "./BreakpointsDropdown.css";
 
 function renderPause(isWaitingOnBreak) {
-  if (isWaitingOnBreak) {
-    return (
-      <div className={classnames("pause-next", "active")} >
-        <img className="pause-next" />
-        <span className="icon-spacer">{L10N.getStr("pauseButtonItem")}</span>
-      </div>
-      )
-  }
+  const active = isWaitingOnBreak;
   return (
-    <div className={classnames("pause-next", "inactive")} >
-        <img className="pause-next" />
-        <span className="icon-spacer">{L10N.getStr("pauseButtonItem")}</span>
+    <div
+      className={classnames("pause-next", {
+        active: active,
+        inactive: !active
+      })}
+    >
+      <img className="pause-next" />
+      <span className="icon-spacer">{L10N.getStr("pauseButtonItem")}</span>
     </div>
-      )
+  );
 }
 
 function renderPauseOnExceptions(
@@ -27,20 +25,23 @@ function renderPauseOnExceptions(
   shouldIgnoreCaughtExceptions,
   isWaitingOnBreak
 ) {
-  if (!shouldPauseOnExceptions && !shouldIgnoreCaughtExceptions && !isWaitingOnBreak) {
-    return (
-      <div className={classnames("pause-on-exceptions", "active")} >
-        <img className="pause-on-exceptions" />
-        <span className="icon-spacer">{L10N.getStr("pauseOnExceptionsItem")}</span>
-      </div>
-      )
-  }
+  const active =
+    (shouldPauseOnExceptions || shouldIgnoreCaughtExceptions) &&
+    (!shouldPauseOnExceptions || !shouldIgnoreCaughtExceptions) &&
+    !isWaitingOnBreak;
   return (
-    <div className={classnames("pause-on-exceptions", "inactive")} >
+    <div
+      className={classnames("pause-on-exceptions", {
+        active: active,
+        inactive: !active
+      })}
+    >
       <img className="pause-on-exceptions" />
-      <span className="icon-spacer">{L10N.getStr("pauseOnExceptionsItem")}</span>
+      <span className="icon-spacer">
+        {L10N.getStr("pauseOnExceptionsItem")}
+      </span>
     </div>
-      )
+  );
 }
 
 function renderPauseOnUncaughtExceptions(
@@ -48,20 +49,23 @@ function renderPauseOnUncaughtExceptions(
   shouldIgnoreCaughtExceptions,
   isWaitingOnBreak
 ) {
-  if (shouldPauseOnExceptions && !shouldIgnoreCaughtExceptions && !isWaitingOnBreak) {
-    return (
-      <div className={classnames("pause-uncaught-exceptions", "active")} >
-        <img className="pause-uncaught-exceptions" />
-        <span className="icon-spacer">{L10N.getStr("pauseOnUncaughtExceptionsItem")}</span>
-      </div>
-      )
-  }
+  const active =
+    shouldPauseOnExceptions &&
+    shouldIgnoreCaughtExceptions &&
+    !isWaitingOnBreak;
   return (
-      <div className={classnames("pause-uncaught-exceptions", "inactive")} >
-        <img className="pause-uncaught-exceptions" />
-        <span className="icon-spacer">{L10N.getStr("pauseOnUncaughtExceptionsItem")}</span>
-      </div>
-      )
+    <div
+      className={classnames("pause-uncaught-exceptions", {
+        active: active,
+        inactive: !active
+      })}
+    >
+      <img className="pause-uncaught-exceptions" />
+      <span className="icon-spacer">
+        {L10N.getStr("pauseOnUncaughtExceptionsItem")}
+      </span>
+    </div>
+  );
 }
 
 function renderIgnoreExceptions(
@@ -69,20 +73,21 @@ function renderIgnoreExceptions(
   shouldIgnoreCaughtExceptions,
   isWaitingOnBreak
 ) {
-  if (shouldIgnoreCaughtExceptions && !isWaitingOnBreak) {
-    return (
-      <div className={classnames("ignore-exceptions", "active")} >
-        <img className="ignore-exceptions" />
-        <span className="icon-spacer">{L10N.getStr("ignoreExceptionsItem")}</span>
-      </div>
-      )
-  }
+  const active =
+    !shouldPauseOnExceptions &&
+    !shouldIgnoreCaughtExceptions &&
+    !isWaitingOnBreak;
   return (
-      <div className={classnames("ignore-exceptions", "inactive")} >
-        <img className="ignore-exceptions" />
-        <span className="icon-spacer">{L10N.getStr("ignoreExceptionsItem")}</span>
-      </div>
-      )
+    <div
+      className={classnames("ignore-exceptions", {
+        active: active,
+        inactive: !active
+      })}
+    >
+      <img className="ignore-exceptions" />
+      <span className="icon-spacer">{L10N.getStr("ignoreExceptionsItem")}</span>
+    </div>
+  );
 }
 
 function handleClick(e) {
@@ -98,24 +103,22 @@ export default function renderBreakpointsDropdown(
 ) {
   const Panel = (
     <ul>
-      <li onClick={() => breakOnNext()}>
-        {renderPause(isWaitingOnBreak)}
-      </li>
-      <li onClick={() => pauseOnExceptions(true, false)}>
+      <li onClick={() => breakOnNext()}>{renderPause(isWaitingOnBreak)}</li>
+      <li onClick={() => pauseOnExceptions(true, true)}>
         {renderPauseOnUncaughtExceptions(
           shouldPauseOnExceptions,
           shouldIgnoreCaughtExceptions,
           isWaitingOnBreak
         )}
       </li>
-      <li onClick={() => pauseOnExceptions(false, false)}>
+      <li onClick={() => pauseOnExceptions(true, false)}>
         {renderPauseOnExceptions(
           shouldPauseOnExceptions,
           shouldIgnoreCaughtExceptions,
           isWaitingOnBreak
         )}
       </li>
-      <li onClick={() => pauseOnExceptions(true, true)}>
+      <li onClick={() => pauseOnExceptions(false, false)}>
         {renderIgnoreExceptions(
           shouldPauseOnExceptions,
           shouldIgnoreCaughtExceptions,
@@ -125,9 +128,24 @@ export default function renderBreakpointsDropdown(
     </ul>
   );
 
+  const active =
+    shouldPauseOnExceptions || shouldIgnoreCaughtExceptions || isWaitingOnBreak;
+
   return (
     <div className="breakpoints-dropdown" onClick={e => handleClick(e)}>
-      <Dropdown className="dropdown" panel={Panel} icon={<Svg name="plus" />} />
+      <Dropdown
+        className="dropdown"
+        panel={Panel}
+        icon={
+          <Svg
+            name="plus"
+            className={classnames("plus", {
+              active: active,
+              inactive: !active
+            })}
+          />
+        }
+      />
     </div>
   );
 }
