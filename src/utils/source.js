9b0bdfebc4fd976f1cfdb71cdcfe969957764a0e
Assaf Dagan <assafdd@gmail.com>
2017-11-18 23:15:02 +0200

    [PrettyPrint] auto-pretty-print minified sources (#4171) (#4700)
    * [PrettyPrint] auto-pretty-print minified sources (#4171)
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - add flow types
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - move logic to selectSource action
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - move logic to selectSource action
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - fix expressions.spec.js test
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - skip generated files
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - a bit of linting
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171) - more linting
    
    * [PrettyPrint] uncaught exception: SyntaxError: Unexpected character "`" (709:32) (#4619) - bump pretty-fast & acorn to latest
    
    * Revert "[PrettyPrint] uncaught exception: SyntaxError: Unexpected character "`" (709:32) (#4619) - bump pretty-fast & acorn to latest"
    
    This reverts commit b7cbbdd744ba428ed808f6729028a6fa04c6cb95.
    
    * [PrettyPrint] auto-pretty-print minified sources (#4171)

--- src/utils/source.js
+++ src/utils/source.js
@@ -200,6 +200,63 @@ function getSourceLineCount(source: Source) {
   return source.text != undefined ? source.text.split("\n").length : 0;
 }
 
+// Used to detect minification for automatic pretty printing
+const SAMPLE_SIZE = 50;
+const INDENT_COUNT_THRESHOLD = 5;
+const CHARACTER_LIMIT = 250;
+const _minifiedCache = new Map();
+
+/**
+ *
+ * Checks if a source is minified based on some heuristics
+ * @param key
+ * @param text
+ * @return boolean
+ * @memberof utils/source
+ * @static
+ */
+
+function isMinified(key: string, text: string) {
+  if (!key || !text) {
+    return false;
+  }
+
+  if (_minifiedCache.has(key)) {
+    return _minifiedCache.get(key);
+  }
+
+  let lineEndIndex = 0;
+  let lineStartIndex = 0;
+  let lines = 0;
+  let indentCount = 0;
+  let overCharLimit = false;
+
+  // Strip comments.
+  text = text.replace(/\/\*[\S\s]*?\*\/|\/\/(.+|\n)/g, "");
+
+  while (lines++ < SAMPLE_SIZE) {
+    lineEndIndex = text.indexOf("\n", lineStartIndex);
+    if (lineEndIndex == -1) {
+      break;
+    }
+    if (/^\s+/.test(text.slice(lineStartIndex, lineEndIndex))) {
+      indentCount++;
+    }
+    // For files with no indents but are not minified.
+    if (lineEndIndex - lineStartIndex > CHARACTER_LIMIT) {
+      overCharLimit = true;
+      break;
+    }
+    lineStartIndex = lineEndIndex + 1;
+  }
+
+  const minified =
+    indentCount / lines * 100 < INDENT_COUNT_THRESHOLD || overCharLimit;
+
+  _minifiedCache.set(key, minified);
+  return minified;
+}
+
 /**
  *
  * Returns Code Mirror mode for source content type
@@ -264,6 +321,7 @@ function isLoaded(source: Source) {
 }
 
 export {
+  isMinified,
   isJavaScript,
   isPretty,
   isThirdParty,
