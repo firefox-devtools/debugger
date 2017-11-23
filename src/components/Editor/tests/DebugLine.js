a0d00261131e141c3723f6676af679bc0b7a74d3
Assaf Dagan <assaf@capriza.com>
2017-11-23 22:48:16 +0200

    [Pause] Locations should be symmetric  for original and generated files (#4715) - fix debug line tests

--- src/components/Editor/tests/DebugLine.js
+++ src/components/Editor/tests/DebugLine.js
@@ -18,7 +18,7 @@ function generateDefaults(overrides) {
         markText: () => ({ clear: jest.fn() })
       }
     },
-    selectedFrame: {
+    selectedLocation: {
       location: {
         sourceId: "x",
         line: 2
