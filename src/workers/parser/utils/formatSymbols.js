75f54b1ee3f0e486a6c15bd228c23778a979bedc
Davide De Cenzo <decenzodavide@gmail.com>
2017-12-13 21:07:28 +0100

    Modified formatSymbols.js

--- src/workers/parser/utils/formatSymbols.js
+++ src/workers/parser/utils/formatSymbols.js
@@ -4,40 +4,61 @@
 
 import getSymbols from "../getSymbols";
 
+function formatLocation(loc) {
+  if (!loc) {
+    return "";
+  }
+
+  const { start, end } = loc;
+  const startLoc = `(${start.line}, ${start.column})`;
+
+  const endLoc = `(${end.line}, ${end.column})`;
+
+  return `[${startLoc}, ${endLoc}]`;
+}
+
+function summarize(symbol) {
+  if (typeof symbol == "boolean") {
+    return symbol ? "true" : "false";
+  }
+
+  const loc = formatLocation(symbol.location);
+
+  const exprLoc = formatLocation(symbol.expressionLocation);
+
+  const params = symbol.parameterNames
+    ? `(${symbol.parameterNames.join(", ")})`
+    : "";
+
+  const expression = symbol.expression || "";
+
+  const klass = symbol.klass || "";
+
+  const name = symbol.name || "";
+
+  const names = symbol.specifiers ? symbol.specifiers.join(", ") : "";
+
+  const values = symbol.values ? symbol.values.join(", ") : "";
+
+  return `${loc} ${exprLoc} ${expression} ${name}${params} ${klass} ${names} ${
+    values
+  }`.trim(); // eslint-disable-line max-len
+}
+
+function formatKey(name, symbols) {
+  if (name == "hasJSX") {
+    return `hasJSX: ${symbols.hasJSX[0] ? "true" : "false"}`;
+  }
+
+  return `${name}:\n${symbols[name].map(summarize).join("\n")}`;
+}
+
 export function formatSymbols(source: Source) {
   const symbols = getSymbols(source);
 
-  function formatLocation(loc) {
-    if (!loc) {
-      return "";
-    }
-    const { start, end } = loc;
-
-    const startLoc = `(${start.line}, ${start.column})`;
-    const endLoc = `(${end.line}, ${end.column})`;
-    return `[${startLoc}, ${endLoc}]`;
-  }
-
-  function summarize(symbol) {
-    const loc = formatLocation(symbol.location);
-    const exprLoc = formatLocation(symbol.expressionLocation);
-    const params = symbol.parameterNames
-      ? `(${symbol.parameterNames.join(", ")})`
-      : "";
-    const expression = symbol.expression || "";
-    const klass = symbol.klass || "";
-
-    const name = symbol.name || "";
-    const names = symbol.specifiers ? symbol.specifiers.join(", ") : "";
-
-    const values = symbol.values ? symbol.values.join(", ") : "";
-
-    return `${loc} ${exprLoc} ${expression} ${name}${params} ${klass} ${
-      names
-    } ${values}`.trim(); // eslint-disable-line max-len
-  }
-
   return Object.keys(symbols)
-    .map(name => `${name}:\n${symbols[name].map(summarize).join("\n")}`)
+
+    .map(name => formatKey(name, symbols))
+
     .join("\n\n");
 }
