e0c88933c61a6bb92fc39bc1a43186d93c9afc64
Jason Laster <jason.laster.11@gmail.com>
2017-12-21 16:25:21 -0500

    Wait for all original sources to load

--- src/actions/sources.js
+++ src/actions/sources.js
@@ -124,10 +124,13 @@ export function newSources(sources: Source[]) {
     });
 
     for (const source of filteredSources) {
-      dispatch(loadSourceMap(source));
       dispatch(checkSelectedSource(source));
       dispatch(checkPendingBreakpoints(source.id));
     }
+
+    return Promise.all(
+      filteredSources.map(source => dispatch(loadSourceMap(source)))
+    );
   };
 }
 
