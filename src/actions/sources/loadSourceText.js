firefox-devtools/debugger

commit 81b70c614cfbc2c127f0381b1ea724b8faa84d3e

only load sources once (#4753)
* Only fetch requests once

* Improvements

* fix tests

* update load source text and some lint issues

* remove extra variables from loadSourceText.spec.js

src/actions/sources/loadSourceText.js
@@ -8,8 +8,11 @@ import { PROMISE } from "../utils/middleware/promise";
 import { setSymbols } from "../ast";
 import { getSource } from "../../selectors";
 import { setSource } from "../../workers/parser";
+import { isLoading, isLoaded } from "../../utils/source";
 import type { Source } from "../../types";
 import type { ThunkArgs } from "../types";
+
+const requests = new Map();
 
 async function loadSource(source: Source, { sourceMaps, client }) {
   if (sourceMaps.isOriginalId(source.id)) {
@@ -25,17 +28,35 @@ async function loadSource(source: Source, { sourceMaps, client }) {
   };
 }
 
+function defer() {
+  let resolve = () => {};
+  let reject = () => {};
+  const promise = new Promise((_res, _rej) => {
+    resolve = _res;
+    reject = _rej;
+  });
+
+  return { resolve, reject, promise };
+}
+
 /**
  * @memberof actions/sources
  * @static
  */
 export function loadSourceText(source: Source) {
   return async ({ dispatch, getState, client, sourceMaps }: ThunkArgs) => {
+    const deferred = defer();
+
     // Fetch the source text only once.
-    if (source.text) {
+    if (isLoaded(source)) {
       return Promise.resolve(source);
     }
 
+    if (isLoading(source) || requests.has(source.id)) {
+      return requests.get(source.id);
+    }
+
+    requests.set(source.id, deferred.promise);
     await dispatch({
       type: "LOAD_SOURCE_TEXT",
       source: source,
@@ -49,5 +70,9 @@ export function loadSourceText(source: Source) {
 
     await setSource(newSource);
     dispatch(setSymbols(source.id));
+
+    // signal that the action is finished
+    deferred.resolve();
+    requests.delete(source.id);
   };
 }

src/actions/sources/tests/loadSourceText.spec.js
@@ -1,0 +1,59 @@
+import {
+  actions,
+  selectors,
+  createStore,
+  makeSource
+} from "../../../utils/test-head";
+const { getSource } = selectors;
+
+describe("loadSourceText", async () => {
+  it("loads two sources w/ one request", async () => {
+    let resolve;
+    let count = 0;
+    const { dispatch, getState } = createStore({
+      sourceContents: () =>
+        new Promise(r => {
+          count++;
+          resolve = r;
+        })
+    });
+    let source = makeSource("foo", { loadedState: "unloaded" });
+
+    await dispatch(actions.newSource(source));
+
+    source = getSource(getState(), source.id).toJS();
+    dispatch(actions.loadSourceText(source));
+
+    source = getSource(getState(), source.id).toJS();
+    const loading = dispatch(actions.loadSourceText(source));
+
+    resolve({ source: "yay", contentType: "text/javascript" });
+    await loading;
+    expect(count).toEqual(1);
+    expect(getSource(getState(), source.id).toJS().text).toEqual("yay");
+  });
+
+  it("doesn't re-load loaded sources", async () => {
+    let resolve;
+    let count = 0;
+    const { dispatch, getState } = createStore({
+      sourceContents: () =>
+        new Promise(r => {
+          count++;
+          resolve = r;
+        })
+    });
+    let source = makeSource("foo", { loadedState: "unloaded" });
+
+    await dispatch(actions.newSource(source));
+    source = getSource(getState(), source.id).toJS();
+    const loading = dispatch(actions.loadSourceText(source));
+    resolve({ source: "yay", contentType: "text/javascript" });
+    await loading;
+
+    source = getSource(getState(), source.id).toJS();
+    await dispatch(actions.loadSourceText(source));
+    expect(count).toEqual(1);
+    expect(getSource(getState(), source.id).toJS().text).toEqual("yay");
+  });
+});

src/actions/tests/breakpoints.spec.js
@@ -308,8 +308,8 @@ describe("breakpoints", () => {
     };
 
     const url = "a.js";
-    const source = makeSource(url);
-    source.text = "function(a,b,c){return {a,b,c}}";
+    const text = "function(a,b,c){return {a,b,c}}";
+    const source = makeSource(url, { loadedState: "loaded", text });
 
     await dispatch(actions.newSource(source));
     await dispatch(actions.addBreakpoint(loc));

src/actions/tests/project-text-search.spec.js
@@ -65,9 +65,11 @@ describe("project text search", () => {
   it("should search all the loaded sources based on the query", async () => {
     const { dispatch, getState } = createStore(threadClient);
     const mockQuery = "foo";
+    const source1 = makeSource("foo1");
+    const source2 = makeSource("foo2");
 
-    await dispatch(actions.newSource(makeSource("foo1")));
-    await dispatch(actions.newSource(makeSource("foo2")));
+    await dispatch(actions.newSource(source1));
+    await dispatch(actions.newSource(source2));
 
     await dispatch(actions.searchSources(mockQuery));
 

src/actions/tests/sources.spec.js
@@ -177,10 +177,11 @@ describe("sources", () => {
 
   it("should create a source when first toggling pretty print", async () => {
     const { dispatch, getState } = createStore(threadClient);
-    const source = makeSource("foobar.js");
+    const source = makeSource("foobar.js", { loadedState: "loaded" });
     await dispatch(actions.newSource(source));
     await dispatch(actions.togglePrettyPrint(source.id));
     expect(getSources(getState()).size).toEqual(2);
+    // ensure that it doesnt create a second source
     await dispatch(actions.togglePrettyPrint(source.id));
     expect(getSources(getState()).size).toEqual(2);
   });

src/utils/source.js
@@ -336,6 +336,9 @@ function getMode(source: Source, sourceMetaData: SourceMetaDataType) {
 function isLoaded(source: Source) {
   return source.loadedState === "loaded";
 }
+function isLoading(source: Source) {
+  return source.loadedState === "loading";
+}
 
 export {
   isMinified,
@@ -351,5 +354,6 @@ export {
   getSourcePath,
   getSourceLineCount,
   getMode,
-  isLoaded
+  isLoaded,
+  isLoading
 };

src/utils/test-head.js
@@ -50,7 +50,7 @@ function commonLog(msg: string, data: any = {}) {
 function makeSource(name: string, props: any = {}) {
   return {
     id: name,
-    loadedState: "loaded",
+    loadedState: "unloaded",
     url: `http://localhost:8000/examples/${name}`,
     ...props
   };

commit c4f6572f6dc03a0a8ed62ebd829194db46f2c772

Fixes loadSourceText async loading for wasm.
Regressed after 81b70c614cfbc2

src/actions/sources/loadSourceText.js
@@ -89,17 +89,16 @@ export function loadSourceText(source: SourceRecord) {
     });
 
     const newSource = getSource(getState(), source.get("id")).toJS();
-    if (newSource.isWasm) {
-      return;
-    }
 
-    if (isOriginalId(newSource.id)) {
+    if (isOriginalId(newSource.id) && !newSource.isWasm) {
       const generatedSource = getGeneratedSource(getState(), source.toJS());
       await dispatch(loadSourceText(generatedSource));
     }
 
-    await setSource(newSource);
-    dispatch(setSymbols(id));
+    if (!newSource.isWasm) {
+      await setSource(newSource);
+      dispatch(setSymbols(id));
+    }
 
     // signal that the action is finished
     deferred.resolve();