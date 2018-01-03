df34a08989f556a1e8790d1f36057bc069ae1943
Pradeep Gangwar <pradeepgangwar39@gmail.com>
2018-01-03 23:00:40 +0530

    Quickopen '?' shows different options. (#5000)

--- src/components/tests/QuickOpenModal.spec.js
+++ src/components/tests/QuickOpenModal.spec.js
@@ -71,6 +71,23 @@ describe("QuickOpenModal", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  test("Basic render with mount & searchType = shortcuts", () => {
+    const { wrapper } = generateModal(
+      {
+        enabled: true,
+        query: "?",
+        searchType: "shortcuts",
+        symbols: {
+          functions: [],
+          variables: []
+        }
+      },
+      "mount"
+    );
+    expect(wrapper.find("ResultList")).toHaveLength(1);
+    expect(wrapper.find("li")).toHaveLength(3);
+  });
+
   test("closeModal", () => {
     const { wrapper, props } = generateModal({ enabled: true }, "mount");
     expect(wrapper).toMatchSnapshot();
