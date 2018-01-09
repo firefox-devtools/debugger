e1b995a4737dbb1e73c299883d9a51284395e501
Jason Laster <jason.laster.11@gmail.com>
2018-01-09 12:14:59 -0500

    tweaks

--- src/actions/event-listeners.js
+++ src/actions/event-listeners.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global window gThreadClient setNamedTimeout services EVENTS */
+/* global window gThreadClient setNamedTimeout EVENTS */
 /* eslint no-shadow: 0  */
 
 /**
