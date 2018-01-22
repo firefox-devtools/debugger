f12ac981b3d7af8dc42c8f48507b9e923d0aad64
Jason Laster <jason.laster.11@gmail.com>
2018-01-22 17:53:41 -0500

    Disable map scopes in dev

--- src/utils/prefs.js
+++ src/utils/prefs.js
@@ -40,7 +40,7 @@ if (isDevelopment()) {
   pref("devtools.debugger.features.root", true);
   pref("devtools.debugger.features.column-breakpoints", false);
   pref("devtools.debugger.features.chrome-scopes", false);
-  pref("devtools.debugger.features.map-scopes", true);
+  pref("devtools.debugger.features.map-scopes", false);
   pref("devtools.debugger.features.breakpoints-dropdown", true);
   pref("devtools.debugger.features.remove-command-bar-options", true);
   pref("devtools.debugger.features.code-coverage", false);
