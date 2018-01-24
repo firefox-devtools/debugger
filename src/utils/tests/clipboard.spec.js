2dea27a8be199b3ff5d5dc0761e495b162c8e535
Todd <stratigos@users.noreply.github.com>
2018-01-24 15:57:34 -0500

    Add tests for utils/clipboard (#5188)

---
+++ src/utils/tests/clipboard.spec.js
@@ -0,0 +1,41 @@
+import { copyToTheClipboard } from "../clipboard";
+
+let clipboardTestCopyString, expectedCopyEvent;
+const addEventListener = jest.fn();
+const execCommand = jest.fn();
+const removeEventListener = jest.fn();
+
+describe("copyToTheClipboard()", () => {
+  beforeEach(() => {
+    expectedCopyEvent = "copy";
+    clipboardTestCopyString = "content intended for clipboard";
+
+    global.document.addEventListener = addEventListener;
+    global.document.execCommand = execCommand;
+    global.document.removeEventListener = removeEventListener;
+  });
+
+  it("listens for 'copy' event", () => {
+    copyToTheClipboard(clipboardTestCopyString);
+
+    expect(document.addEventListener).toHaveBeenCalledWith(
+      expectedCopyEvent,
+      expect.anything()
+    );
+  });
+
+  it("calls document.execCommand() with 'copy' command", () => {
+    copyToTheClipboard(clipboardTestCopyString);
+
+    expect(execCommand).toHaveBeenCalledWith(expectedCopyEvent, false, null);
+  });
+
+  it("removes event listener for 'copy' event", () => {
+    copyToTheClipboard(clipboardTestCopyString);
+
+    expect(document.removeEventListener).toHaveBeenCalledWith(
+      expectedCopyEvent,
+      expect.anything()
+    );
+  });
+});
