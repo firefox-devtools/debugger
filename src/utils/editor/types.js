0af4bbe731edd3d6bcd1e4e135bdbe60f0778988
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2018-01-24 15:37:28 -0700

    Tests for editor utils (#5129)

--- src/utils/editor/types.js
+++ src/utils/editor/types.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+// @flow
+
 export type EditorPosition = { line: number, column: number };
 
 export type EditorRange = { end: EditorPosition, start: EditorPosition };
