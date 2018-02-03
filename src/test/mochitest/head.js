f8e064e1693b7081d392e6146df2750ddfc9b0ca
Logan Smyth <loganfsmyth@gmail.com>
2018-02-02 16:57:49 -0800

    Add a mochitest for original mappings of Babel's for..of loop, with framework for more.

--- src/test/mochitest/head.js
+++ src/test/mochitest/head.js
@@ -1062,6 +1062,10 @@ function getScopeLabel(dbg, index) {
   return findElement(dbg, "scopeNode", index).innerText;
 }
 
+function getScopeValue(dbg, index) {
+  return findElement(dbg, "scopeValue", index).innerText;
+}
+
 function toggleObjectInspectorNode(node) {
   const objectInspector = node.closest(".object-inspector");
   const properties = objectInspector.querySelectorAll(".node").length;
