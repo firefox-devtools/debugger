887591b59f2227c26248bb3279a8ace3e59e0117
David Walsh <davidwalsh83@gmail.com>
2018-02-05 16:29:11 -0600

    Hack to avoid incorrect test failure

--- src/test/mochitest/browser_dbg-sources.js
+++ src/test/mochitest/browser_dbg-sources.js
@@ -36,7 +36,12 @@ add_task(async function() {
   await clickElement(dbg, "sourceDirectory", 3);
   await assertSourceCount(dbg, 8);
 
-  // Select a source.
+  // Cleanup from opening source directory
+  dbg.win.document
+    .querySelector(".sources-list .focused")
+    .classList.remove("focused");
+
+  // Select a source
   ok(
     !findElementWithSelector(dbg, ".sources-list .focused"),
     "Source is not focused"
