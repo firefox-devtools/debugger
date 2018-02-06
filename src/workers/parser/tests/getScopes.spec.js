53f81904a940f7626df5ddbb7f0e71d16c0a3adf
Logan Smyth <loganfsmyth@gmail.com>
2018-02-06 09:19:01 -0800

    Update tests to use jest-in-case.

--- src/workers/parser/tests/getScopes.spec.js
+++ src/workers/parser/tests/getScopes.spec.js
@@ -3,81 +3,92 @@
 import getScopes from "../getScopes";
 import { setSource } from "../sources";
 import { getOriginalSource } from "./helpers";
+import cases from "jest-in-case";
 
-function parseScopes(name, line: number, column: number) {
-  const source = getOriginalSource(name);
-  setSource(source);
+cases(
+  "Parser.getScopes",
+  ({ name, file, locations }) => {
+    const source = getOriginalSource(file);
+    setSource(source);
 
-  return getScopes({
-    sourceId: source.id,
-    line,
-    column
-  });
-}
+    locations.forEach(([line, column]) => {
+      const scopes = getScopes({
+        sourceId: source.id,
+        line,
+        column
+      });
 
-describe("getScopes", () => {
-  it("finds scope bindings in a module", () => {
-    expect(parseScopes("scopes/simple-module", 7, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for function declarations", () => {
-    expect(parseScopes("scopes/function-declaration", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/function-declaration", 3, 20)).toMatchSnapshot();
-    expect(parseScopes("scopes/function-declaration", 5, 1)).toMatchSnapshot();
-    expect(parseScopes("scopes/function-declaration", 9, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for function expressions", () => {
-    expect(parseScopes("scopes/function-expression", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/function-expression", 3, 23)).toMatchSnapshot();
-    expect(parseScopes("scopes/function-expression", 6, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for arrow functions", () => {
-    expect(parseScopes("scopes/arrow-function", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/arrow-function", 4, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/arrow-function", 7, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/arrow-function", 8, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for class declarations", () => {
-    expect(parseScopes("scopes/class-declaration", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-declaration", 5, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-declaration", 7, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for class expressions", () => {
-    expect(parseScopes("scopes/class-expression", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-expression", 5, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-expression", 7, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for for loops", () => {
-    expect(parseScopes("scopes/for-loops", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 3, 17)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 4, 17)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 5, 25)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 7, 22)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 8, 22)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 9, 23)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 11, 23)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 12, 23)).toMatchSnapshot();
-    expect(parseScopes("scopes/for-loops", 13, 24)).toMatchSnapshot();
-  });
-  it("finds scope bindings for try..catch", () => {
-    expect(parseScopes("scopes/try-catch", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/try-catch", 4, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/try-catch", 7, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for block statements", () => {
-    expect(parseScopes("scopes/block-statement", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/block-statement", 6, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for class properties", () => {
-    expect(parseScopes("scopes/class-property", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-property", 4, 16)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-property", 6, 12)).toMatchSnapshot();
-    expect(parseScopes("scopes/class-property", 7, 0)).toMatchSnapshot();
-  });
-  it("finds scope bindings for switch statements", () => {
-    expect(parseScopes("scopes/switch-statement", 2, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/switch-statement", 5, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/switch-statement", 7, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/switch-statement", 9, 0)).toMatchSnapshot();
-    expect(parseScopes("scopes/switch-statement", 11, 0)).toMatchSnapshot();
-  });
-});
+      expect(scopes).toMatchSnapshot(
+        `getScopes ${name} at line ${line} column ${column}`
+      );
+    });
+  },
+  [
+    {
+      name: "finds scope bindings in a module",
+      file: "scopes/simple-module",
+      locations: [[7, 0]]
+    },
+    {
+      name: "finds scope bindings for function declarations",
+      file: "scopes/function-declaration",
+      locations: [[2, 0], [3, 20], [5, 1], [9, 0]]
+    },
+    {
+      name: "finds scope bindings for function expressions",
+      file: "scopes/function-expression",
+      locations: [[2, 0], [3, 23], [6, 0]]
+    },
+    {
+      name: "finds scope bindings for arrow functions",
+      file: "scopes/arrow-function",
+      locations: [[2, 0], [4, 0], [7, 0], [8, 0]]
+    },
+    {
+      name: "finds scope bindings for class declarations",
+      file: "scopes/class-declaration",
+      locations: [[2, 0], [5, 0], [7, 0]]
+    },
+    {
+      name: "finds scope bindings for class expressions",
+      file: "scopes/class-expression",
+      locations: [[2, 0], [5, 0], [7, 0]]
+    },
+    {
+      name: "finds scope bindings for for loops",
+      file: "scopes/for-loops",
+      locations: [
+        [2, 0],
+        [3, 17],
+        [4, 17],
+        [5, 25],
+        [7, 22],
+        [8, 22],
+        [9, 23],
+        [11, 23],
+        [12, 23],
+        [13, 24]
+      ]
+    },
+    {
+      name: "finds scope bindings for try..catch",
+      file: "scopes/try-catch",
+      locations: [[2, 0], [4, 0], [7, 0]]
+    },
+    {
+      name: "finds scope bindings for block statements",
+      file: "scopes/block-statement",
+      locations: [[2, 0], [6, 0]]
+    },
+    {
+      name: "finds scope bindings for class properties",
+      file: "scopes/class-property",
+      locations: [[2, 0], [4, 16], [6, 12], [7, 0]]
+    },
+    {
+      name: "finds scope bindings for switch statements",
+      file: "scopes/switch-statement",
+      locations: [[2, 0], [5, 0], [7, 0], [9, 0], [11, 0]]
+    }
+  ]
+);
