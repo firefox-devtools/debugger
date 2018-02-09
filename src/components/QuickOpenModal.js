69a5806a1a6c4c721592716828dbdb7751923a7e
atwalg2 <atwalg2@mymacewan.ca>
2018-02-08 19:54:54 -0700

    wrapper for loading indicator

--- src/components/QuickOpenModal.js
+++ src/components/QuickOpenModal.js
@@ -57,7 +57,8 @@ type Props = {
 
 type State = {
   results: ?Array<QuickOpenResult>,
-  selectedIndex: number
+  selectedIndex: number,
+  isLoading: boolean
 };
 
 type GotoLocationType = {
@@ -76,7 +77,7 @@ function filter(values, query) {
 export class QuickOpenModal extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { results: null, selectedIndex: 0 };
+    this.state = { results: null, selectedIndex: 0, isLoading: true };
   }
 
   componentDidMount() {
@@ -266,6 +267,8 @@ export class QuickOpenModal extends Component<Props, State> {
   };
 
   onChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
+    setTimeout(() => this.setState({ isLoading: true }));
+
     const { selectedSource, setQuickOpenQuery } = this.props;
     setQuickOpenQuery(e.target.value);
     const noSource = !selectedSource || !selectedSource.get("text");
@@ -273,6 +276,8 @@ export class QuickOpenModal extends Component<Props, State> {
       return;
     }
     this.updateResults(e.target.value);
+
+    setTimeout(() => this.setState({ isLoading: false }));
   };
 
   onKeyDown = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
@@ -365,11 +370,20 @@ export class QuickOpenModal extends Component<Props, State> {
 
   render() {
     const { enabled, query } = this.props;
-    const { selectedIndex, results } = this.state;
+    let { selectedIndex, results } = this.state;
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      console.log(results);
+      results = null;
+    }
+
+    setTimeout(() => this.setState({ isLoading: false }), 4000); // exists to simulate a situation that requires loading ( can remove set timeouts later )
 
     if (!enabled) {
       return null;
     }
+
     const summaryMsg = L10N.getFormatStr(
       "sourceSearch.resultsSummary1",
       this.getResultCount()
@@ -394,17 +408,19 @@ export class QuickOpenModal extends Component<Props, State> {
           expanded={expanded}
           selectedItemId={expanded ? items[selectedIndex].id : ""}
         />
-        {newResults && (
-          <ResultList
-            key="results"
-            items={items}
-            selected={selectedIndex}
-            selectItem={this.selectResultItem}
-            ref="resultList"
-            expanded={expanded}
-            {...(this.isSourceSearch() ? { size: "big" } : {})}
-          />
-        )}
+        <div className="loading-wrapper">
+          {newResults && (
+            <ResultList
+              key="results"
+              items={items}
+              selected={selectedIndex}
+              selectItem={this.selectResultItem}
+              ref="resultList"
+              expanded={expanded}
+              {...(this.isSourceSearch() ? { size: "big" } : {})}
+            />
+          )}
+        </div>
       </Modal>
     );
   }
