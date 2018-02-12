bc2a6c6587daa9147aa53f3810bb0a962f8d1652
atwalg2 <atwalg2@mymacewan.ca>
2018-02-12 15:12:53 -0700

    consolidate isLoading into setState when reuslts set

--- src/components/QuickOpenModal.js
+++ src/components/QuickOpenModal.js
@@ -114,15 +114,15 @@ export class QuickOpenModal extends Component<Props, State> {
   searchSources = (query: string) => {
     if (query == "") {
       const results = this.props.sources;
-      return this.setState({ results });
+      return this.setState({ results, isLoading: false })
     }
     if (this.isGotoSourceQuery()) {
       const [baseQuery] = query.split(":");
       const results = filter(this.props.sources, baseQuery);
-      this.setState({ results });
+      this.setState({ results, isLoading: false })
     } else {
       const results = filter(this.props.sources, query);
-      this.setState({ results });
+      this.setState({ results, isLoading: false })
     }
   };
 
@@ -134,10 +134,11 @@ export class QuickOpenModal extends Component<Props, State> {
       results = variables;
     }
     if (query === "@" || query === "#") {
-      return this.setState({ results });
+      return this.setState({ results, isLoading: false });
     }
 
     this.setState({
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -165,15 +166,19 @@ export class QuickOpenModal extends Component<Props, State> {
 =======
       results: filter(results, query.slice(1))
 >>>>>>> move loading logic into updateResults
+=======
+      results: filter(results, query.slice(1)), isLoading: false
+>>>>>>> consolidate isLoading into setState when reuslts set
     });
   };
 
   searchShortcuts = (query: string) => {
     const results = formatShortcutResults();
     if (query == "?") {
-      this.setState({ results });
+      this.setState({ results, isLoading: false  });
     } else {
       this.setState({
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -201,6 +206,9 @@ export class QuickOpenModal extends Component<Props, State> {
 =======
         results: filter(results, query.slice(1))
 >>>>>>> move loading logic into updateResults
+=======
+        results: filter(results, query.slice(1)), isLoading: false
+>>>>>>> consolidate isLoading into setState when reuslts set
       });
     }
   };
@@ -209,10 +217,10 @@ export class QuickOpenModal extends Component<Props, State> {
     const { tabs, sources } = this.props;
     if (tabs.length > 0) {
       this.setState({
-        results: sources.filter(source => tabs.includes(source.url))
+        results: sources.filter(source => tabs.includes(source.url)), isLoading: false
       });
     } else {
-      this.setState({ results: sources.slice(0, 100) });
+      this.setState({ results: sources.slice(0, 100), isLoading: false });
     }
   };
 
@@ -222,21 +230,19 @@ export class QuickOpenModal extends Component<Props, State> {
     }
 
     if (query == "") {
-      this.showTopSources();
-      return this.setState({ isLoading: false });
+      return this.showTopSources();
     }
 
     if (this.isSymbolSearch()) {
-      this.searchSymbols(query);
-      return this.setState({ isLoading: false });
+      return this.searchSymbols(query);
     }
 
     if (this.isShortcutQuery()) {
-      this.searchShortcuts(query);
-      return this.setState({ isLoading: false });
+      return this.searchShortcuts(query);
     }
 
     this.searchSources(query);
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -248,6 +254,8 @@ export class QuickOpenModal extends Component<Props, State> {
 =======
     return this.setState({ isLoading: false });
 >>>>>>> move loading logic into updateResults
+=======
+>>>>>>> consolidate isLoading into setState when reuslts set
   };
 
   setModifier = (item: QuickOpenResult) => {
@@ -446,6 +454,7 @@ export class QuickOpenModal extends Component<Props, State> {
     const { enabled, query } = this.props;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     const { selectedIndex, results, isLoading } = this.state;
 =======
     const { enabled, query, symbols } = this.props;
@@ -457,6 +466,9 @@ export class QuickOpenModal extends Component<Props, State> {
 =======
     let { selectedIndex, results, isLoading } = this.state;
 >>>>>>> add newline at eof
+=======
+    const { selectedIndex, results, isLoading } = this.state;
+>>>>>>> consolidate isLoading into setState when reuslts set
 
     if (isLoading) {
       results = null;
@@ -557,7 +569,7 @@ export class QuickOpenModal extends Component<Props, State> {
 >>>>>>> move loading logic into updateResults
 =======
         {isLoading && (
-          <div className="loadIndicator">{L10N.getStr("loadingText")}</div>
+          <div className="loading-indicator">{L10N.getStr("loadingText")}</div>
         )}
 >>>>>>> pad top/bot, and center
         {newResults && (
