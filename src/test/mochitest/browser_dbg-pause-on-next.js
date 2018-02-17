40bf7adf29896f4841fb2ac9145e0fcfd5dd16a2
Lukasz Sobek <lukaszsobek@me.com>
2018-02-17 09:54:42 +0100

    Adds test file

---
+++ src/test/mochitest/browser_dbg-pause-on-next.js
@@ -0,0 +1,49 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests the breakpoints are hit in various situations.
+
+add_task(async function() {
+  // Go to a page
+  const dbg = await initDebugger("doc-scripts.html");
+  const { selectors: { getSelectedSource }, getState } = dbg;
+
+  // Set breakpoint
+  await addBreakpoint(dbg, "scripts.html", 18);
+
+  // click the pause button
+  reload(dbg);
+
+  // then eval a function
+  const source = getSelectedSource(getState()).toJS();
+  ok(!source.url, "It is an eval source");
+
+  // assert we're paused
+  // ok(isPaused(dbg),"It is paused");
+  assertPausedLocation(dbg);
+
+  // Make sure we can set a top-level breakpoint and it will be hit on
+  // reload.
+  // await addBreakpoint(dbg, "scripts.html", 18);
+  // reload(dbg);
+
+  // await waitForDispatch(dbg, "NAVIGATE");
+  // await waitForSelectedSource(dbg, "doc-scripts.html");
+  // await waitForPaused(dbg);
+
+  // assertPausedLocation(dbg);
+  // await resume(dbg);
+
+  // // Create an eval script that pauses itself.
+  // invokeInTab("doEval");
+  // await waitForPaused(dbg);
+
+  // await resume(dbg);
+  // const source = getSelectedSource(getState()).toJS();
+  // ok(!source.url, "It is an eval source");
+
+  // await addBreakpoint(dbg, source, 5);
+  // invokeInTab("evaledFunc");
+  // await waitForPaused(dbg);
+  // assertPausedLocation(dbg);
+});
