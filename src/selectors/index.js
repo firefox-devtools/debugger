b581cc1378df259416edc00235971b4bee0f5426
Łukasz Sobek <lukaszsobek@me.com>
2018-02-26 03:44:40 +0100

    Moves selectors to selector folder (#5344)

---
+++ src/selectors/index.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+
+export * from "../reducers/expressions";
+export * from "../reducers/sources";
+export * from "../reducers/pause";
+export * from "../reducers/debuggee";
+export * from "../reducers/breakpoints";
+export * from "../reducers/pending-breakpoints";
+export * from "../reducers/ui";
+export * from "../reducers/file-search";
+export * from "../reducers/ast";
+export * from "../reducers/coverage";
+export * from "../reducers/project-text-search";
+export * from "../reducers/replay";
+export * from "../reducers/source-tree";
+export { getEventListeners } from "../reducers/event-listeners";
+export {
+  getQuickOpenEnabled,
+  getQuickOpenQuery,
+  getQuickOpenType
+} from "../reducers/quick-open";
+
+export { getBreakpointAtLocation } from "./breakpointAtLocation";
+export { getVisibleBreakpoints } from "./visibleBreakpoints";
+export { isSelectedFrameVisible } from "./isSelectedFrameVisible";
+export { getCallStackFrames } from "./getCallStackFrames";
+export { getVisibleSelectedFrame } from "./visibleSelectedFrame";
