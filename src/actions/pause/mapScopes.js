6c69d223a13dbec848c48b0f5b0cd7eee600dad8
Logan Smyth <loganfsmyth@gmail.com>
2018-02-28 13:31:58 -0800

    Require original scope mapping to be 90% successful to be shown. (#5549)

--- src/actions/pause/mapScopes.js
+++ src/actions/pause/mapScopes.js
@@ -132,7 +132,40 @@ async function buildMappedScopes(
     })
   );
 
-  return generateClientScope(scopes, mappedOriginalScopes);
+  const mappedGeneratedScopes = generateClientScope(
+    scopes,
+    mappedOriginalScopes
+  );
+
+  return isReliableScope(mappedGeneratedScopes) ? mappedGeneratedScopes : null;
+}
+
+/**
+ * Consider a scope and its parents reliable if the vast majority of its
+ * bindings were successfully mapped to generated scope bindings.
+ */
+function isReliableScope(scope: OriginalScope): boolean {
+  let totalBindings = 0;
+  let unknownBindings = 0;
+
+  for (let s = scope; s; s = s.parent) {
+    const vars = (s.bindings && s.bindings.variables) || {};
+    for (const key of Object.keys(vars)) {
+      const binding = vars[key];
+
+      totalBindings += 1;
+      if (
+        binding.value &&
+        typeof binding.value === "object" &&
+        (binding.value.type === "unscoped" || binding.value.type === "unmapped")
+      ) {
+        unknownBindings += 1;
+      }
+    }
+  }
+
+  // As determined by fair dice roll.
+  return totalBindings === 0 || unknownBindings / totalBindings < 0.9;
 }
 
 function generateClientScope(
