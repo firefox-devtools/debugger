29c3bf9146bd2a926c2b7f61f9615f6dd0e8c17d
Jason Laster <jason.laster.11@gmail.com>
2018-03-05 15:12:04 -0500

    Re-enable "try to address empty debugger bug" (#5593)

---
+++ src/test/mochitest/browser_dbg-stepping.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function assertSelectedFile(dbg, url) {
+  const selectedLocation = dbg.selectors.getSelectedSource(dbg.getState());
+  ok(selectedLocation, "The debugger should be at the paused location")
+  const selectedUrl = selectedLocation.get("url");
+  return ok( selectedUrl.includes(url), `The debugger should be paused at ${url}"`);
+}
+
+add_task(async function test() {
+  const dbg = await initDebugger("big-sourcemap.html", "big-sourcemap");
+  invokeInTab("hitDebugStatement");
+  await waitForPaused(dbg);
+  assertSelectedFile(dbg, "bundle.js");
+
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+  await stepIn(dbg);
+
+  assertSelectedFile(dbg, "bundle.js");
+  assertDebugLine(dbg, 42309);
+  assertPausedLocation(dbg);
+});
