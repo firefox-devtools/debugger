8457900a38f6f949628603f5a6a2b7b26b0bd8b7
Jason Laster <jason.laster.11@gmail.com>
2018-03-05 23:30:29 -0500

    some fun refactorings

--- src/utils/frame.js
+++ src/utils/frame.js
@@ -171,25 +171,7 @@ function annotateFrame(frame: Frame) {
 }
 
 function annotateBabelAsyncFrames(frames: Frame[]) {
-  const startIndexes = frames.reduce(
-    (accumulator, frame, index) =>
-      isStartOfBabelGroup(frame) ? [...accumulator, index] : accumulator,
-    []
-  );
-  const endIndexes = frames.reduce(
-    (accumulator, frame, index) =>
-      isEndOfBabelGroup(frame) ? [...accumulator, index] : accumulator,
-    []
-  );
-  if (startIndexes.length != endIndexes.length || startIndexes.length === 0) {
-    return frames;
-  }
-  const babelGroupRanges = zip(startIndexes, endIndexes);
-
-  const babelFrameIndexes = flatMap(babelGroupRanges, babelGroupRange =>
-    range(babelGroupRange[0], babelGroupRange[1] + 1)
-  );
-
+  const babelFrameIndexes = getBabelFrameIndexes(frames);
   const isBabelFrame = frameIndex => babelFrameIndexes.includes(frameIndex);
 
   return frames.map(
@@ -198,17 +180,41 @@ function annotateBabelAsyncFrames(frames: Frame[]) {
   );
 }
 
-function isStartOfBabelGroup(frame: Frame) {
-  return (
-    getFrameUrl(frame).match(/regenerator-runtime/i) &&
-    frame.displayName === "tryCatch"
+// Receives an array of frames and looks for babel async
+// call stack groups.
+function getBabelFrameIndexes(frames) {
+  const startIndexes = getFrameIndices(
+    frames,
+    (displayName, url) =>
+      url.match(/regenerator-runtime/i) && displayName === "tryCatch"
+  );
+
+  const endIndexes = getFrameIndices(
+    frames,
+    (displayName, url) =>
+      displayName === "_asyncToGenerator/<" ||
+      (url.match(/_microtask/i) && displayName === "flush")
+  );
+
+  if (startIndexes.length != endIndexes.length || startIndexes.length === 0) {
+    return frames;
+  }
+
+  // Receives an array of start and end index tuples and returns
+  // an array of async call stack index ranges.
+  // e.g. [[1,3], [5,7]] => [[1,2,3], [5,6,7]]
+  return flatMap(zip(startIndexes, endIndexes), ([startIndex, endIndex]) =>
+    range(startIndex, endIndex + 1)
   );
 }
 
-function isEndOfBabelGroup(frame: Frame) {
-  return (
-    frame.displayName === "_asyncToGenerator/<" ||
-    (getFrameUrl(frame).match(/_microtask/i) && frame.displayName === "flush")
+function getFrameIndices(frames, predicate) {
+  return frames.reduce(
+    (accumulator, frame, index) =>
+      predicate(frame.displayName, getFrameUrl(frame))
+        ? [...accumulator, index]
+        : accumulator,
+    []
   );
 }
 
