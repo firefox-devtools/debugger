d68cdcebf0c02c458f3e10915912a5d43193d3ad
yalong <yalong.xyz@gmail.com>
2018-03-22 12:34:16 -0400

    import type

--- src/workers/parser/steps.js
+++ src/workers/parser/steps.js
@@ -7,7 +7,7 @@
 import * as t from "@babel/types";
 import type { SimplePath } from "./utils/simple-path";
 import type { SourceId } from "../../types";
-import { AstPosition } from "./types";
+import type { AstPosition } from "./types";
 import { getClosestPath } from "./utils/closest";
 import { isAwaitExpression, isYieldExpression } from "./utils/helpers";
 
