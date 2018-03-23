d30162bbb84fe599368f24dbcbdebaa8a51ee806
Jason Laster <jason.laster.11@gmail.com>
2018-03-23 15:37:25 -0400

    tweak pause points (#5771)

--- src/workers/parser/pausePoints.js
+++ src/workers/parser/pausePoints.js
@@ -1,5 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+
 import { traverseAst } from "./utils/ast";
 import * as t from "@babel/types";
+
+import type { AstLocation } from "./types";
+import type { BabelNode } from "@babel/types";
+import type { SimplePath } from "./utils/simple-path";
+
+export type PausePoint = {|
+  location: AstLocation,
+  types: {| breakpoint: boolean, stepOver: boolean |}
+|};
+
+export type PausePoints = PausePoint[];
 
 const isControlFlow = node =>
   t.isForStatement(node) || t.isWhileStatement(node) || t.isIfStatement(node);
@@ -15,62 +32,69 @@ const inExpression = parent =>
   t.isCallExpression(parent.node) ||
   t.isTemplateLiteral(parent.node);
 
-export function getPausePoints(sourceId) {
+export function getPausePoints(sourceId: string) {
   const state = [];
   traverseAst(sourceId, { enter: onEnter }, state);
   return state;
 }
 
-function formatNode(location, types) {
-  return { location, types };
-}
-
-function onEnter(node, ancestors, state) {
+function onEnter(node: BabelNode, ancestors: SimplePath[], state) {
   const parent = ancestors[ancestors.length - 1];
 
   if (
     isAssignment(node) ||
     isImport(node) ||
     isControlFlow(node) ||
-    isReturn(node)
+    t.isDebuggerStatement(node)
   ) {
-    state.push(
-      formatNode(node.loc.start, { breakpoint: true, stepOver: true })
-    );
+    addPoint(state, node.loc.start);
+  }
+
+  if (isReturn(node)) {
+    if (t.isCallExpression(node.argument)) {
+      addEmptyPoint(state, node.loc.start);
+    } else {
+      addPoint(state, node.loc.start);
+    }
   }
 
   if (t.isCallExpression(node)) {
-    state.push(
-      formatNode(node.loc.start, {
-        breakpoint: true,
+    addPoint(state, node.loc.start, {
+      breakpoint: true,
 
-        // NOTE: we do not want to land inside an expression e.g. [], {}, call
-        stepOver: !inExpression(parent)
-      })
-    );
-  }
-
-  if (t.isDebuggerStatement(node)) {
-    state.push(
-      formatNode(node.loc.start, { breakpoint: true, stepOver: true })
-    );
+      // NOTE: we do not want to land inside an expression e.g. [], {}, call
+      stepOver: !inExpression(parent)
+    });
   }
 
   if (t.isFunction(node)) {
     const { line, column } = node.loc.end;
-    state.push(formatNode(node.loc.start, { breakpoint: true }));
-    state.push(
-      formatNode(
-        { line, column: column - 1 },
-        { breakpoint: true, stepOver: true }
-      )
-    );
+    addBreakPoint(state, node.loc.start);
+    addPoint(state, { line, column: column - 1 });
   }
 
   if (t.isProgram(node)) {
     const lastStatement = node.body[node.body.length - 1];
-    state.push(
-      formatNode(lastStatement.loc.end, { breakpoint: true, stepOver: true })
-    );
+    addPoint(state, lastStatement.loc.end);
   }
+}
+
+function formatNode(location, types) {
+  return { location, types };
+}
+
+function addPoint(
+  state,
+  location,
+  types = { breakpoint: true, stepOver: true }
+) {
+  state.push(formatNode(location, types));
+}
+
+function addEmptyPoint(state, location) {
+  addPoint(state, location, { breakpoint: false, stepOver: false });
+}
+
+function addBreakPoint(state, location) {
+  addPoint(state, location, { breakpoint: true, stepOver: false });
 }
