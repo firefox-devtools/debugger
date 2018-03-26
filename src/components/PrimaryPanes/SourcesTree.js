0e11e3c5f677cf63ab73046c04ab8425f01fd1a3
David Walsh <davidwalsh83@gmail.com>
2018-03-26 16:43:52 -0500

    Restore sources tree icons (#5786)

--- src/components/PrimaryPanes/SourcesTree.js
+++ src/components/PrimaryPanes/SourcesTree.js
@@ -219,7 +219,8 @@ class SourcesTree extends Component<Props, State> {
       }
 
       const sourceType = sourceTypes[getExtension(source)];
-      return <img className={sourceType || "file"} />;
+      const classNames = classnames("source-icon", sourceType || "file");
+      return <img className={classNames} />;
     }
 
     return <img className="folder" />;
