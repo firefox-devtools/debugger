c3019d89263b7e485fb8241c8528c72f096c09d4
Logan Smyth <loganfsmyth@gmail.com>
2018-04-05 10:37:24 -0700

    Add an example Ember quickstart application.

---
+++ src/test/mochitest/examples/ember/quickstart/app/router.js
@@ -0,0 +1,14 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+});
+
+"pause here";
+
+export default Router;
