cdb770b1ca959c460cc37cfce6d9e17f1b20ca76
David Walsh <davidwalsh83@gmail.com>
2018-04-05 16:49:22 -0500

    Ensure breakpoint text always highlights

--- src/components/SecondaryPanes/BreakpointItem.js
+++ src/components/SecondaryPanes/BreakpointItem.js
@@ -39,9 +39,14 @@ class BreakpointItem extends Component<Props> {
   componentDidMount() {
     this.setupEditor();
   }
+  componentDidUpdate() {
+    this.setupEditor();
+  }
 
   componentWillUnmount() {
-    this.editor.destroy();
+    if (this.editor) {
+      this.editor.destroy();
+    }
   }
 
   shouldComponentUpdate(nextProps: Props) {
