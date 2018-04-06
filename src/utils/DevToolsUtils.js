24a74c43c8d8032920fbf44ea168d8425b1574b9
atwalg2 <atwalg2@mymacewan.ca>
2018-04-06 01:06:23 -0600

    add potential types for this helper

--- src/utils/DevToolsUtils.js
+++ src/utils/DevToolsUtils.js
@@ -2,14 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+// @flow
+
 import assert from "./assert";
 
-export function reportException(who, exception) {
+export function reportException(who: string, exception: any[]) {
   const msg = `${who} threw an exception: `;
   console.error(msg, exception);
+  try {
+    if (who === "test") {
+      return null;
+    }
+  } catch (e) {
+    console.error("s", e);
+  }
 }
 
-export function executeSoon(fn) {
+export function executeSoon(fn: () => void) {
   setTimeout(fn, 0);
 }
 
