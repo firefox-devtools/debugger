578473338597fa7ea5e374b518ed24197dbf486a
Jason Laster <jason.laster.11@gmail.com>
2018-04-09 14:15:49 -0400

    Revert "Revert "Fix previewing a file w/ no functions (#5390)""
    This reverts commit cc1a9ebeaff7532ff96d1fe7834a69377d185989.

---
+++ src/test/mochitest/browser_dbg-preview-module.js
@@ -0,0 +1,37 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test hovering in a script that is paused on load
+// and doesn't have functions.
+add_task(async function() {
+  const dbg = await initDebugger("doc-scripts.html");
+  const { selectors: { getSelectedSource }, getState } = dbg;
+
+  navigate(dbg, "doc-on-load.html");
+
+  // wait for `top-level.js` to load and to pause at a debugger statement
+  await waitForSelectedSource(dbg);
+  await waitForPaused(dbg);
+
+  const popupPreviewed = waitForDispatch(dbg, "SET_PREVIEW");
+  hoverAtPos(dbg, { line: 1, ch: 6 });
+  await popupPreviewed;
+  await assertPreviewPopup(dbg, {
+    field: "foo",
+    value: "1",
+    expression: "obj"
+  });
+  await assertPreviewPopup(dbg, {
+    field: "bar",
+    value: "2",
+    expression: "obj"
+  });
+
+  // hover over an empty position so that the popup closes
+  hoverAtPos(dbg, { line: 1, ch: 40 });
+
+  const tooltipPreviewed = waitForDispatch(dbg, "SET_PREVIEW");
+  hoverAtPos(dbg, { line: 2, ch: 7 });
+  await tooltipPreviewed;
+  await assertPreviewTooltip(dbg, { result: "3", expression: "func" });
+});
--- src/test/mochitest/browser_dbg-preview.js
+++ src/test/mochitest/browser_dbg-preview.js
@@ -1,46 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-function getCoordsFromPosition(cm, { line, ch }) {
-  return cm.charCoords({ line: ~~line, ch: ~~ch });
-}
-
-function hoverAtPos(dbg, { line, ch }) {
-  const cm = getCM(dbg);
-  const coords = getCoordsFromPosition(cm, { line: line - 1, ch });
-  const tokenEl = dbg.win.document.elementFromPoint(coords.left, coords.top);
-  tokenEl.dispatchEvent(
-    new MouseEvent("mouseover", {
-      bubbles: true,
-      cancelable: true,
-      view: dbg.win
-    })
-  );
-}
-
-async function assertTooltip(dbg, { result, expression }) {
-  const previewEl = await waitForElement(dbg, "tooltip");
-  is(previewEl.innerText, result, "Preview text shown to user");
-
-  const preview = dbg.selectors.getPreview(dbg.getState());
-  is(`${preview.result}`, result, "Preview.result");
-  is(preview.updating, false, "Preview.updating");
-  is(preview.expression, expression, "Preview.expression");
-}
-
-async function assertPreviewPopup(dbg, { field, value, expression }) {
-  const previewEl = await waitForElement(dbg, "popup");
-  const preview = dbg.selectors.getPreview(dbg.getState());
-
-  is(
-    `${preview.result.preview.ownProperties[field].value}`,
-    value,
-    "Preview.result"
-  );
-  is(preview.updating, false, "Preview.updating");
-  is(preview.expression, expression, "Preview.expression");
-}
-
+// Test hovering on an object, which will show a popup and on a
+// simple value, which will show a tooltip.
 add_task(async function() {
   const dbg = await initDebugger("doc-scripts.html");
   const { selectors: { getSelectedSource }, getState } = dbg;
@@ -56,7 +18,7 @@ add_task(async function() {
   const tooltipPreviewed = waitForDispatch(dbg, "SET_PREVIEW");
   hoverAtPos(dbg, { line: 5, ch: 12 });
   await tooltipPreviewed;
-  await assertTooltip(dbg, { result: "3", expression: "result" });
+  await assertPreviewTooltip(dbg, { result: "3", expression: "result" });
 
   const popupPreviewed = waitForDispatch(dbg, "SET_PREVIEW");
   hoverAtPos(dbg, { line: 2, ch: 10 });
