98e2645e7c3e271f6ebd1471ccaa26af41dc3d28
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-10 19:46:04 +0200

    Add missing license headers (#5911)

--- assets/images/Svg.js
+++ assets/images/Svg.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
 const React = require("react");
 import InlineSVG from "svg-inline-react";
 const { isDevelopment } = require("devtools-config");
