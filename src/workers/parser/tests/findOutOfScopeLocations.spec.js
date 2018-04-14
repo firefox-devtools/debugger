4dfda18c58a2e0d1ef4022d9204910ef6357a47f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-14 02:25:48 +0200

    workflow: add eslint rule for license header

--- src/workers/parser/tests/findOutOfScopeLocations.spec.js
+++ src/workers/parser/tests/findOutOfScopeLocations.spec.js
@@ -1,4 +1,7 @@
 /* eslint max-nested-callbacks: ["error", 4]*/
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 import findOutOfScopeLocations from "../findOutOfScopeLocations";
 
