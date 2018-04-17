0a3200c95bee02bf6c76d830f7dc5cda6808126a
Malachi Holden <holdenm@berea.edu>
2018-04-17 13:31:08 -0400

    changed the div to button in the test cases

--- src/components/shared/tests/Button.spec.js
+++ src/components/shared/tests/Button.spec.js
@@ -16,10 +16,10 @@ describe("CloseButton", () => {
     <CloseButton buttonClass="error" handleClick={spy} tooltip={tooltip} />
   );
   it("renders", () => expect(wrapper).toMatchSnapshot());
-  wrapper.find("div").simulate("click");
+  wrapper.find("button").simulate("click");
   it("handleClick is called", () => expect(spy).toHaveBeenCalled());
   it("tooltip is passed through", () =>
-    expect(wrapper.find("div").prop("title")).toEqual(tooltip));
+    expect(wrapper.find("button").prop("title")).toEqual(tooltip));
 });
 
 describe("CommandBarButton", () => {
