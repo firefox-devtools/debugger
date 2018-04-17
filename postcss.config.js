8ef887945a182e52b565dd88859871b570416112
Jason Laster <jason.laster.11@gmail.com>
2018-04-17 17:45:20 -0400

    Use mc help svg (#5942)
    * Use mc help svg
    
    * Remove help asset
    
    * Fix paths for local development

--- postcss.config.js
+++ postcss.config.js
@@ -6,16 +6,17 @@ var mapUrl = require("postcss-url-mapper");
 const debug = require("debug")("launchpad");
 
 function mapUrlProduction(url) {
-  const newUrl = url.replace(
-    /\/images\//,
-    "chrome://devtools/skin/images/debugger/"
-  );
+  const newUrl = url
+    .replace(/\/images\//, "chrome://devtools/skin/images/debugger/")
+    .replace(/\/mc\//, "chrome://devtools/skin/images/");
+
   debug("map url", { url, newUrl });
   return newUrl;
 }
 
 function mapUrlDevelopment(url) {
   const newUrl = url
+    .replace(/mc/, "mc/devtools/client/themes/images")
     .replace(/(chrome:\/\/|resource:\/\/)/, "/mc/")
     .replace(/devtools\/skin/, "devtools/client/themes")
     .replace(/devtools\/content/, "devtools/client");
