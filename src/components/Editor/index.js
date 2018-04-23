93399647045e7a627038371471bfd730f5ab5311
Jason Laster <jason.laster.11@gmail.com>
2018-04-23 04:17:50 -0400

    Highlight non-empty lines on gutter hover

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -63,6 +63,7 @@ import {
 import { resizeToggleButton, resizeBreakpointGutter } from "../../utils/ui";
 
 import "./Editor.css";
+import "./Gutter.css";
 import "./Highlight.css";
 
 import type SourceEditor from "../../utils/editor/source-editor";
@@ -98,7 +99,8 @@ export type Props = {
 };
 
 type State = {
-  editor: SourceEditor
+  editor: SourceEditor,
+  gutterHover: boolean
 };
 
 class Editor extends PureComponent<Props, State> {
@@ -151,20 +153,8 @@ class Editor extends PureComponent<Props, State> {
     codeMirrorWrapper.tabIndex = 0;
     codeMirrorWrapper.addEventListener("keydown", e => this.onKeyDown(e));
     codeMirrorWrapper.addEventListener("click", e => this.onClick(e));
-
-    const toggleFoldMarkerVisibility = e => {
-      if (node instanceof HTMLElement) {
-        node
-          .querySelectorAll(".CodeMirror-guttermarker-subtle")
-          .forEach(elem => {
-            elem.classList.toggle("visible");
-          });
-      }
-    };
-
-    const codeMirrorGutter = codeMirror.getGutterElement();
-    codeMirrorGutter.addEventListener("mouseleave", toggleFoldMarkerVisibility);
-    codeMirrorGutter.addEventListener("mouseenter", toggleFoldMarkerVisibility);
+    codeMirrorWrapper.addEventListener("mouseover", this.onMouseOver);
+    codeMirrorWrapper.addEventListener("mouseleave", this.onMouseLeave);
 
     if (!isFirefox()) {
       codeMirror.on("gutterContextMenu", (cm, line, eventName, event) =>
@@ -380,6 +370,27 @@ class Editor extends PureComponent<Props, State> {
     }
   }
 
+  onMouseOver = e => {
+    if (e.target.classList.contains("CodeMirror-linenumber")) {
+      if (!this.state.gutterHover) {
+        this.setState({ gutterHover: true });
+      }
+    } else {
+      if (
+        this.state.gutterHover &&
+        !e.target.classList.contains("CodeMirror-gutter")
+      ) {
+        this.setState({ gutterHover: false });
+      }
+    }
+  };
+
+  onMouseLeave = e => {
+    if (this.state.gutterHover) {
+      this.setState({ gutterHover: false });
+    }
+  };
+
   toggleConditionalPanel = line => {
     const {
       conditionalPanelLine,
@@ -575,11 +586,13 @@ class Editor extends PureComponent<Props, State> {
 
   render() {
     const { coverageOn } = this.props;
+    const { gutterHover } = this.state;
 
     return (
       <div
         className={classnames("editor-wrapper", {
-          "coverage-on": coverageOn
+          "coverage-on": coverageOn,
+          "gutter-hover": gutterHover
         })}
         ref={c => (this.$editorWrapper = c)}
       >
