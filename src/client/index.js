c51221e585e11812ad18a576bee66284daafb33b
David Walsh <davidwalsh83@gmail.com>
2018-04-25 14:23:24 -0500

    Properly load settings from prefs

--- src/client/index.js
+++ src/client/index.js
@@ -16,9 +16,9 @@ import {
 } from "../utils/bootstrap";
 
 function loadFromPrefs(actions: Object) {
-  const { pauseOnExceptions, ignoreCaughtExceptions } = prefs;
-  if (pauseOnExceptions || ignoreCaughtExceptions) {
-    return actions.pauseOnExceptions(pauseOnExceptions, ignoreCaughtExceptions);
+  const { pauseOnExceptions, pauseOnCaughtExceptions } = prefs;
+  if (pauseOnExceptions || pauseOnCaughtExceptions) {
+    return actions.pauseOnExceptions(pauseOnExceptions, pauseOnCaughtExceptions);
   }
 }
 
