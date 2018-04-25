a8bd08b161d48d4d6a055b4666c368a49e8a6898
mutdmour <mutdmour@gmail.com>
2018-04-25 08:29:44 +0300

    start on oncontextmenu

--- src/components/tests/Outline.spec.js
+++ src/components/tests/Outline.spec.js
@@ -14,22 +14,27 @@ jest.mock("devtools-contextmenu", () => ({ showMenu: jest.fn() }));
 jest.mock("../../utils/clipboard", () => ({ copyToTheClipboard: jest.fn() }));
 
 const sourceId = "id";
+const mockFunctionText = "mock function text";
 
-function generateDefaults(symbols) {
+function generateDefaults(overrides) {
   return {
     selectLocation: jest.genMockFunction(),
     selectedSource: {
       get: () => sourceId
     },
+    getFunctionText: jest.fn().mockReturnValue(mockFunctionText),
+    flashLineRange: jest.fn(),
     isHidden: false,
-    symbols
+    symbols: {},
+    ...overrides
   };
 }
 
-function render(symbols = {}) {
-  const props = generateDefaults(symbols);
+function render(overrides = {}) {
+  const props = generateDefaults(overrides);
   const component = shallow(<Outline.WrappedComponent {...props} />);
-  return { component, props };
+  const instance = component.instance();
+  return { component, props, instance };
 }
 
 describe("Outline", () => {
@@ -51,7 +56,7 @@ describe("Outline", () => {
       ]
     };
 
-    const { component } = render(symbols);
+    const { component } = render({symbols});
     expect(component).toMatchSnapshot();
   });
 
@@ -63,7 +68,7 @@ describe("Outline", () => {
       ]
     };
 
-    const { component } = render(symbols);
+    const { component } = render({symbols});
     expect(component).toMatchSnapshot();
   });
 
@@ -73,15 +78,51 @@ describe("Outline", () => {
       functions: [makeSymbolDeclaration("my_example_function", startLine)]
     };
 
-    const { component, props } = render(symbols);
+    const { component, props } = render({symbols});
 
     const { selectLocation } = props;
     const listItem = component.find("li").first();
     listItem.simulate("click");
-    expect(selectLocation).toHaveBeenCalledWith({ line: 12, sourceId });
+    expect(selectLocation).toHaveBeenCalledWith({ line: startLine, sourceId });
   });
 
-  describe("onContextMenu", () => {
+  describe("onContextMenu of Outline", () => {
+    it("does not show menu with no selected source", async () => {
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        stopPropagation: jest.fn()
+      };
+      const { instance, props } = render({
+        selectedSource: null
+      });
+      await instance.onContextMenu(mockEvent, {});
+      expect(mockEvent.preventDefault).toHaveBeenCalled();
+      expect(mockEvent.stopPropagation).toHaveBeenCalled();
+      expect(showMenu).not.toHaveBeenCalled();
+    });
 
+    it("shows menu to copy function", async () => {
+      const func = makeSymbolDeclaration("my_example_function", 12);
+      const symbols = {
+        functions: [func]
+      };
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        stopPropagation: jest.fn()
+      };
+      const { instance, props } = render({symbols});
+      await instance.onContextMenu(mockEvent, func);
+
+      expect(mockEvent.preventDefault).toHaveBeenCalled();
+      expect(mockEvent.stopPropagation).toHaveBeenCalled();
+
+      const expectedMenuOptions = [{"accesskey": "F", "click": expect.any(Function), "disabled": false, "id": "node-menu-copy-function", "label": "Copy function"}];
+      expect(props.getFunctionText).toHaveBeenCalledWith(12);
+      expect(showMenu).toHaveBeenCalledWith(mockEvent, expectedMenuOptions);
+
+      // showMenu.mock.calls[0][1][0].click();
+      // expect(copyToTheClipboard).toHaveBeenCalledWith(mockFunctionText);
+      // expect(flashLineRange).toHaveBeenCalledWith(null);
+    });
   });
 });
