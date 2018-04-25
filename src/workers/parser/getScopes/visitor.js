6b17f016174124fd4c00dfffd398b81e150706c3
Logan Smyth <loganfsmyth@gmail.com>
2018-04-25 12:11:33 -0700

    [scopes] Use the scope visitor to avoid rewriting shadowed bindings in console expressions (#6087)

--- src/workers/parser/getScopes/visitor.js
+++ src/workers/parser/getScopes/visitor.js
@@ -143,6 +143,10 @@ export function parseSourceScopes(sourceId: SourceId): ?Array<ParsedScope> {
     return null;
   }
 
+  return buildScopeList(ast, sourceId);
+}
+
+export function buildScopeList(ast: Node, sourceId: SourceId) {
   const { global, lexical } = createGlobalScope(ast, sourceId);
 
   const state = {
