1d8ae73aafdc05390a47f76792bb58e390a51460
Jason Laster <jason.laster.11@gmail.com>
2018-04-26 13:53:33 -0400

    [reps] remove some uneeded files

--- packages/devtools-reps/bin/dev-server.js
+++ packages/devtools-reps/bin/dev-server.js
@@ -5,11 +5,31 @@
 const path = require("path");
 const toolbox = require("devtools-launchpad/index");
 const feature = require("devtools-config");
-const getConfig = require("./getConfig");
 const serve = require("express-static");
 
-const envConfig = getConfig();
-feature.setConfig(envConfig);
+feature.setConfig({
+  "title": "Reps",
+  "hotReloading": true,
+  "defaultURL": "https://nchevobbe.github.io/demo/console-test-app.html",
+  "environment": "development",
+  "logging": {
+    "client": false,
+    "firefoxProxy": false,
+    "actions": false
+  },
+  "theme": "light",
+  "firefox": {
+    "webSocketConnection": false,
+    "host": "localhost",
+    "webSocketPort": 9000,
+    "tcpPort": 6080,
+    "mcPath": "./firefox"
+  },
+  "development": {
+    "serverPort": 8000
+  },
+  "features": {}
+});
 
 let webpackConfig = require("../webpack.config");
 
