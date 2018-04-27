127f51d4811356e13835896b1f5014d4041c75da
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2018-04-27 09:28:01 +0200

    Fix reps launchpad.
    We still need a config module since it's passed to
    both the dev server and the webpack config.
    The path to the devtools-components assets needed to
    be updated as well so we go to the root node_modules.

--- packages/devtools-reps/bin/dev-server.js
+++ packages/devtools-reps/bin/dev-server.js
@@ -4,36 +4,12 @@
 
 const path = require("path");
 const toolbox = require("devtools-launchpad/index");
-const feature = require("devtools-config");
 const serve = require("express-static");
-
-feature.setConfig({
-  "title": "Reps",
-  "hotReloading": true,
-  "defaultURL": "https://nchevobbe.github.io/demo/console-test-app.html",
-  "environment": "development",
-  "logging": {
-    "client": false,
-    "firefoxProxy": false,
-    "actions": false
-  },
-  "theme": "light",
-  "firefox": {
-    "webSocketConnection": false,
-    "host": "localhost",
-    "webSocketPort": 9000,
-    "tcpPort": 6080,
-    "mcPath": "./firefox"
-  },
-  "development": {
-    "serverPort": 8000
-  },
-  "features": {}
-});
+const config = require("../config");
 
 let webpackConfig = require("../webpack.config");
 
-let { app } = toolbox.startDevServer(envConfig, webpackConfig, __dirname);
+let { app } = toolbox.startDevServer(config, webpackConfig, __dirname);
 
 // Serve devtools-reps images
 app.use(
@@ -45,5 +21,5 @@ app.use(
 // write in the postCSS config in development mode.
 app.use(
   "/devtools-components/images/",
-  serve(path.join(__dirname, "../node_modules/devtools-components/src/images"))
+  serve(path.join(__dirname, "../../../node_modules/devtools-components/src/images"))
 );
