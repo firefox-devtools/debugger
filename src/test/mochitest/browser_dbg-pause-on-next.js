d8ba6a56aacb51e70d60966820124e336c5ac873
Jason Laster <jason.laster.11@gmail.com>
2018-05-08 14:59:30 -0400

    test tweaks

--- src/test/mochitest/browser_dbg-pause-on-next.js
+++ src/test/mochitest/browser_dbg-pause-on-next.js
@@ -1,30 +1,15 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// Tests the breakpoints are hit in various situations.
-
-const expressionSelectors = {
-  plusIcon: ".watch-expressions-pane button.plus",
-  input: "input.input-expression"
-};
+// Tests that when  pause on next is selected, we  pause on the next execution
 
 add_task(async function() {
   const dbg = await initDebugger("doc-scripts.html");
-  const { selectors: { isPaused }, getState } = dbg;
 
-  log('Searching for simple3.js');
-  await selectSource(dbg, "simple3.js");
-
-  log('Press pause button');
   clickElement(dbg, "pause");
-  await waitForDispatch(dbg, "BREAK_ON_NEXT");
-
-  let src = findSource(dbg, "simple3.js");
-  await addBreakpoint(dbg, src, 2);
+  await waitForState(dbg, state => dbg.selectors.getIsWaitingOnBreak(state))
   invokeInTab("simple");
 
-  // Give the debugger enough time to come to a pause
   await waitForPaused(dbg);
-
   assertPaused(dbg);
 });
