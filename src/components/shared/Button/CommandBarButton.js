e18e6b942575b9aaf56b9ba79d96d47d69542252
Łukasz Sobek <lukaszsobek@me.com>
2018-05-15 14:31:09 +0200

    Cleans up shared buttons (#6287)

--- src/components/shared/Button/CommandBarButton.js
+++ src/components/shared/Button/CommandBarButton.js
@@ -6,7 +6,7 @@
 import classnames from "classnames";
 import React from "react";
 
-import "./CommandBarButton.css";
+import "./styles/CommandBarButton.css";
 
 type Props = {
   children: React$Element<any>,
