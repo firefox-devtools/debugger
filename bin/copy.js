15104936c642f1f9d565d0d8621aed7331c503e0
Jason Laster <jason.laster.11@gmail.com>
2018-05-16 10:32:15 -0400

    Improve the process for copying changes

---
+++ bin/copy.js
@@ -0,0 +1,19 @@
+
+const copyAssets = require("./copy-assets")
+const copyModules = require("./copy-modules")
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(1), {
+  string: ["mc"],
+  boolean: ["watch", "symlink", "assets"]
+});
+
+const mc = args.mc || "./firefox";
+const watch = args.watch;
+const symlink = args.symlink;
+const assets = args.assets
+
+console.log(`Copying Files to ${mc} with params: `, {watch, assets, symlink})
+
+copyAssets({ assets, mc, watch, symlink})
+copyModules({ mc, watch })
