28f2817fd54e41aa1bba0e99ca1cfe27b219b36f
Jason Laster <jason.laster.11@gmail.com>
2018-05-17 16:50:48 -0400

    prettier

--- src/components/Editor/SearchBar.js
+++ src/components/Editor/SearchBar.js
@@ -223,7 +223,10 @@ class SearchBar extends Component<Props, State> {
 
   // Renderers
   buildSummaryMsg() {
-    const { searchResults: { matchIndex, count, index }, query } = this.props;
+    const {
+      searchResults: { matchIndex, count, index },
+      query
+    } = this.props;
 
     if (query.trim() == "") {
       return "";
@@ -290,12 +293,18 @@ class SearchBar extends Component<Props, State> {
   };
 
   shouldShowErrorEmoji() {
-    const { query, searchResults: { count } } = this.props;
+    const {
+      query,
+      searchResults: { count }
+    } = this.props;
     return !!query && !count;
   }
 
   render() {
-    const { searchResults: { count }, searchOn } = this.props;
+    const {
+      searchResults: { count },
+      searchOn
+    } = this.props;
 
     if (!searchOn) {
       return <div />;
