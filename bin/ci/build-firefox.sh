c0c632175d5df78523a322b7692ea5ff85584384
Jason Laster <jason.laster.11@gmail.com>
2018-05-24 16:50:01 -0400

    retry the build 5 times

--- bin/ci/build-firefox.sh
+++ bin/ci/build-firefox.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=5
+  local delay=15
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 cd firefox
 # We have to set that env variable otherwise
 # ./mach build is going to be interactive and expect <ENTER>
@@ -10,7 +32,7 @@ echo $MOZBUILD_STATE_PATH
 
 echo "ac_add_options --enable-artifact-builds" > mozconfig
 echo "mk_add_options AUTOCLOBBER=1" >> mozconfig
-./mach build
+retry ./mach build
 ret=$?
 
 cd ..
