ce649ecd5212c13664235686ecd09f266cbd42f6
Jason Laster <jason.laster.11@gmail.com>
2018-06-17 01:31:20 -0400

    test

--- bin/ci/clone-gecko.sh
+++ bin/ci/clone-gecko.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+python --version
 hg --version
+df -h 
+
 rm -rf firefox/
 hg clone https://hg.mozilla.org/mozilla-unified/ firefox
 
 cd firefox
+hg update --clean
 hg co $MC_COMMIT
 cd ..
