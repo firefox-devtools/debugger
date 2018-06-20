4a41e3b5dbc69ffa5a479bff49aaf1d822885d06
David Walsh <davidwalsh83@gmail.com>
2018-06-20 17:25:47 -0500

    Another test attempt

--- src/test/mochitest/browser_dbg-breakpoint-skipping.js
+++ src/test/mochitest/browser_dbg-breakpoint-skipping.js
@@ -2,28 +2,28 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 add_task(async function() {
-  await pushPref("devtools.features.skip-pausing", true);
+  await pushPref("devtools.debugger.skip-pausing", true);
+  await pushPref("devtools.debugger.features.skip-pausing", true);
 
   let dbg = await initDebugger("doc-scripts.html");
 
   await selectSource(dbg, "simple3");
   await addBreakpoint(dbg, "simple3", 2);
   await addBreakpoint(dbg, "simple3", 3);
+  await waitForDispatch(dbg, "SYNC_BREAKPOINT", 2);
 
-  let syncedBps = waitForDispatch(dbg, "SYNC_BREAKPOINT", 2);
-  await reload(dbg, "simple3");
-  await waitForSelectedSource(dbg, "simple3");
-  await syncedBps;
+  // Click the button to trigger breakpoints
+  //findElementWithSelector(dbg, "button").click();
+  invokeInTab("simple");
 
-  assertNotPaused(dbg);
+  assertPaused(dbg);
 
   // Turn "skip pausing" on
   clickElementWithSelector(dbg, ".command-bar-skip-pausing");
 
-  syncedBps = waitForDispatch(dbg, "SYNC_BREAKPOINT", 2);
-  await reload(dbg, "simple3");
-  await waitForSelectedSource(dbg, "simple3");
-  await syncedBps;
+  // Click the button to trigger breakpoints
+  //findElementWithSelector(dbg, "button").click();
+  invokeInTab("simple");
 
-  assertPaused(dbg);
+  assertNotPaused(dbg);
 });
