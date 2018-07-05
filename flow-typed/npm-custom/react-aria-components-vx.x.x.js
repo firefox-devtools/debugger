be286656a60d9b77a69962d9addad37050d6dd65
Andrew <andrew@huth.me>
2018-07-05 15:26:22 -0700

    Add stub flowtypes for react-aria-components

---
+++ flow-typed/npm-custom/react-aria-components-vx.x.x.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+declare module "react-aria-components/src/tab" {
+  declare module.exports: any;
+}
+
+declare module "react-aria-components/src/tabs" {
+  declare module.exports: any;
+}
+
+declare module "react-aria-components/src/tab-list" {
+  declare module.exports: any;
+}
+
+declare module "react-aria-components/src/tab-panels" {
+  declare module.exports: any;
+}
