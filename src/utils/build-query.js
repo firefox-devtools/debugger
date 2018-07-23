e45fd6b14d84bb2db1df8ccf6497dd4723661825
Jason Laster <jason.laster.11@gmail.com>
2018-07-23 14:28:00 -0400

    [releases] exclude worker sources (#6682)

--- src/utils/build-query.js
+++ src/utils/build-query.js
@@ -5,7 +5,7 @@
 // @flow
 import escapeRegExp from "lodash/escapeRegExp";
 
-import type { SearchModifiers } from "../../types";
+import type { SearchModifiers } from "../types";
 
 type QueryOptions = {
   isGlobal?: boolean,
