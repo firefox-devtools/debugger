38a0d2134dda54b20974db2d3756350cc2ef9310
Tyler Wendlandt <twendlan@ualberta.ca>
2018-10-16 19:03:31 -0600

    7009/warn on yarn copy missing files (#7111)

--- bin/copy.js
+++ bin/copy.js
@@ -2,6 +2,8 @@
 const copyAssets = require("./copy-assets")
 const copyModules = require("./copy-modules")
 const minimist = require("minimist");
+const fs = require("fs");
+const chalk = require("chalk");
 
 const args = minimist(process.argv.slice(1), {
   string: ["mc"],
@@ -16,8 +18,33 @@ const assets = args.assets
 console.log(`Copying Files to ${mc} with params: `, {watch, assets, symlink})
 
 async function start() {
-  await copyAssets({ assets, mc, watch, symlink})
-  await copyModules.run({ mc, watch })
+  if (fs.existsSync(mc)) {
+    try {
+      await copyAssets({ assets, mc, watch, symlink})
+      await copyModules.run({ mc, watch })
+    } catch (e) {
+      console.error(e);
+      if (e.code === "ENOENT") {
+        missingFilesErrorMessage();
+      }
+    }
+  } else {
+    missingFilesErrorMessage();
+  }
+}
+
+function missingFilesErrorMessage() {
+  let errorMessage = [
+    'It looks like you are missing some files. The mozilla-central ',
+    'codebase may be missing at ${mc}. You can clone mozilla-central by ',
+    'running \`./bin/prepare-mochitests-dev\` from the root of the ',
+    'debugger.html repository. You can find more information on bundling ',
+    'or mochitests at ',
+    'https://github.com/devtools-html/debugger.html/blob/master/docs/bundling.md or ',
+    'https://github.com/devtools-html/debugger.html/blob/master/docs/mochitests.md'
+  ].join('');
+
+  console.warn(chalk.yellow(errorMessage));
 }
 
 start();
