9258ac853092649e563dd964cdaf57f678e9e6e5
ravi610 <ravichandora610@gmail.com>
2018-10-24 03:58:27 +0530

    added flow to result.js (#7144)

--- src/utils/result-list.js
+++ src/utils/result-list.js
@@ -2,10 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+// @flow
+
 import { isFirefox } from "devtools-environment";
 import { transitionTimeout } from "../components/shared/Modal";
 
-export function scrollList(resultList, index, delayed = false) {
+export function scrollList(
+  resultList: Element[],
+  index: number,
+  delayed: boolean = false
+): void {
   if (!resultList.hasOwnProperty(index)) {
     return;
   }
@@ -29,18 +35,19 @@ export function scrollList(resultList, index, delayed = false) {
   scroll();
 }
 
-function chromeScrollList(elem, index) {
-  const resultsEl = elem.parentNode;
+function chromeScrollList(elem: Element, index: number): void {
+  const resultsEl: any = elem.parentNode;
+
   if (!resultsEl || resultsEl.children.length === 0) {
     return;
   }
 
-  const resultsHeight = resultsEl.clientHeight;
-  const itemHeight = resultsEl.children[0].clientHeight;
-  const numVisible = resultsHeight / itemHeight;
-  const positionsToScroll = index - numVisible + 1;
-  const itemOffset = resultsHeight % itemHeight;
-  const scroll = positionsToScroll * (itemHeight + 2) + itemOffset;
+  const resultsHeight: number = resultsEl.clientHeight;
+  const itemHeight: number = resultsEl.children[0].clientHeight;
+  const numVisible: number = resultsHeight / itemHeight;
+  const positionsToScroll: number = index - numVisible + 1;
+  const itemOffset: number = resultsHeight % itemHeight;
+  const scroll: number = positionsToScroll * (itemHeight + 2) + itemOffset;
 
   resultsEl.scrollTop = Math.max(0, scroll);
 }
