a2897707c59add6bc04e96eec558be5c1839f556
Kanika Shrivastav <kanika17@navgurukul.org>
2018-10-24 04:48:40 +0530

    Added Flow in src/actions/breakpoints/remapLocations.js (#7124)

--- src/actions/breakpoints/remapLocations.js
+++ src/actions/breakpoints/remapLocations.js
@@ -2,8 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-export default function remapLocations(breakpoints, sourceId, sourceMaps) {
-  const sourceBreakpoints = breakpoints.map(async breakpoint => {
+ // @flow
+
+ import type { Breakpoint } from "../../types";
+ import type { BreakpointsMap } from "../../selectors";
+
+ export default function remapLocations(breakpoints: Breakpoint[], sourceId: string, sourceMaps: Object) {
+  const sourceBreakpoints:BreakpointsMap = breakpoints.map(async breakpoint => {
     if (breakpoint.location.sourceId !== sourceId) {
       return breakpoint;
     }
