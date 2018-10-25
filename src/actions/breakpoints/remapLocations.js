3600b47ba941912a091a9d8e4c8f16ef1f95043c
David Walsh <davidwalsh83@gmail.com>
2018-10-25 10:26:34 -0500

    Fix linting issues (#7168)

--- src/actions/breakpoints/remapLocations.js
+++ src/actions/breakpoints/remapLocations.js
@@ -2,19 +2,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
- // @flow
+// @flow
 
- import type { Breakpoint } from "../../types";
- import type { BreakpointsMap } from "../../selectors";
+import type { Breakpoint } from "../../types";
+import type { BreakpointsMap } from "../../selectors";
 
- export default function remapLocations(breakpoints: Breakpoint[], sourceId: string, sourceMaps: Object) {
-  const sourceBreakpoints:BreakpointsMap = breakpoints.map(async breakpoint => {
-    if (breakpoint.location.sourceId !== sourceId) {
-      return breakpoint;
+export default function remapLocations(
+  breakpoints: Breakpoint[],
+  sourceId: string,
+  sourceMaps: Object
+) {
+  const sourceBreakpoints: BreakpointsMap = breakpoints.map(
+    async breakpoint => {
+      if (breakpoint.location.sourceId !== sourceId) {
+        return breakpoint;
+      }
+      const location = await sourceMaps.getOriginalLocation(
+        breakpoint.location
+      );
+      return { ...breakpoint, location };
     }
-    const location = await sourceMaps.getOriginalLocation(breakpoint.location);
-    return { ...breakpoint, location };
-  });
+  );
 
   return Promise.all(sourceBreakpoints.valueSeq());
 }
