87b541dcd145ba9afc35a92064ee2d79698c63a3
ravi610 <ravichandora610@gmail.com>
2018-10-25 23:48:44 +0530

    some minor modification

--- src/utils/project-search.js
+++ src/utils/project-search.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-// Maybe reuse file search's functions?
+ // @flow
 
-// @flow
+// Maybe reuse file search's functions?
 
 import React from "react";
 import type { Match } from "../components/ProjectSearch";
