b128f259678c18144d478d23084d0f10a11bf0a8
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2018-11-16 17:16:26 +0100

    Adapt tests

--- packages/devtools-reps/src/object-inspector/tests/utils/should-load-item-non-indexed-properties.js
+++ packages/devtools-reps/src/object-inspector/tests/utils/should-load-item-non-indexed-properties.js
@@ -5,6 +5,8 @@
 const Utils = require("../../utils");
 const {
   createNode,
+  createGetterNode,
+  createSetterNode,
   getChildren,
   makeNodesForEntries,
   nodeIsDefaultProperties
@@ -188,22 +190,20 @@ describe("shouldLoadItemNonIndexedProperties", () => {
   });
 
   it("returns true for an accessor <get> node", () => {
-    const accessorNode = createNode({
+    const getNode = createGetterNode({
       name: "root",
-      contents: accessorStubs.get("getter")
+      property: accessorStubs.get("getter")
     });
-    const [getNode] = getChildren({ item: accessorNode });
-    expect(getNode.name).toBe("<get>");
+    expect(getNode.name).toBe("<get root()>");
     expect(shouldLoadItemNonIndexedProperties(getNode)).toBeTruthy();
   });
 
   it("returns true for an accessor <set> node", () => {
-    const accessorNode = createNode({
+    const setNode = createSetterNode({
       name: "root",
-      contents: accessorStubs.get("setter")
+      property: accessorStubs.get("setter")
     });
-    const [setNode] = getChildren({ item: accessorNode });
-    expect(setNode.name).toBe("<set>");
+    expect(setNode.name).toBe("<set root()>");
     expect(shouldLoadItemNonIndexedProperties(setNode)).toBeTruthy();
   });
 
