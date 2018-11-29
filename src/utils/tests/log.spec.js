f8697c7d09a9eb075b9cadbae61be4a422f2f3eb
Jason Laster <jason.laster.11@gmail.com>
2018-11-29 17:13:09 -0500

    Add conditional logging (#7363)

--- src/utils/tests/log.spec.js
+++ src/utils/tests/log.spec.js
@@ -2,44 +2,33 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+import { prefs } from "../prefs";
 import { log } from "../log.js";
 
-let logArgFirst, logArgSecond, logArgArray, logPrefix;
+let logArgFirst, logArgSecond, logArgArray;
 
 describe("log()", () => {
   beforeEach(() => {
     logArgFirst = "my info";
     logArgSecond = "my other info";
     logArgArray = [logArgFirst, logArgSecond];
-    logPrefix = "[log]";
-
     global.console = { log: jest.fn() };
   });
 
-  describe("when isDevelopment", () => {
-    it("prints all arguments, prefixed with a log label", () => {
-      log(logArgArray);
-
-      expect(global.console.log).toHaveBeenCalledWith(logPrefix, logArgArray);
-    });
+  afterEach(() => {
+    prefs.logging = false;
   });
 
-  describe("when not isDevelopment", () => {
-    beforeEach(() => {
-      process.env.NODE_ENV = "production";
-    });
-
-    afterEach(() => {
-      delete process.env.NODE_ENV;
-    });
-
-    it("returns undefined", () => {
-      expect(log(logArgArray)).toEqual(undefined);
-    });
-
-    it("produces no logs", () => {
+  describe("when isDevelopment", () => {
+    it("prints arguments", () => {
+      prefs.logging = true;
       log(logArgArray);
 
+      expect(global.console.log).toHaveBeenCalledWith(logArgArray);
+    });
+
+    it("does not print by default", () => {
+      log(logArgArray);
       expect(global.console.log).not.toHaveBeenCalled();
     });
   });
