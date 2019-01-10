844708cba186aaa431ce64cdd96f3f4da520d70b
petty <36493475+baronKilljoy@users.noreply.github.com>
2019-01-09 20:03:50 -0500

    XHR Breakpoint items text should be label toggle #7700

--- src/components/SecondaryPanes/XHRBreakpoints.js
+++ src/components/SecondaryPanes/XHRBreakpoints.js
@@ -195,6 +195,7 @@ class XHRBreakpoints extends Component<Props, State> {
         key={path}
         title={path}
         onDoubleClick={(items, options) => this.editExpression(index)}
+        onClick={() => this.handleCheckbox(index)}
       >
         <input
           type="checkbox"
