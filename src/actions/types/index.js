3dff3a9b876cdbce9c57c61ba8ff6b1ef382f964
Jason Laster <jlaster@mozilla.com>
2019-01-28 14:51:22 -0500

    [sync] Bug 1518777 - add onReload event to navigated action; r=ochameau,jlast

--- src/actions/types/index.js
+++ src/actions/types/index.js
@@ -42,7 +42,9 @@ export type ThunkArgs = {
   sourceMaps: any,
   openLink: (url: string) => void,
   openWorkerToolbox: (worker: Worker) => void,
-  openElementInInspector: (grip: Object) => void
+  openElementInInspector: (grip: Object) => void,
+  openConsoleAndEvaluate: (input: string) => void,
+  onReload: () => void
 };
 
 export type Thunk = ThunkArgs => any;
