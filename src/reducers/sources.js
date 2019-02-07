9518c1e557793d7344456588396948a9a9c61ec1
derek-li <the.derekli@gmail.com>
2019-02-07 13:48:54 -0800

    Fix for issue#7851
    updates relativeSource on updateSource

--- src/reducers/sources.js
+++ src/reducers/sources.js
@@ -276,8 +276,22 @@ function updateSource(state: SourcesState, source: Object) {
 
   state.sources[source.id] = updatedSource;
 
+  updateExistingRelativeSource(state, source);
   updateSourceUrl(state, source);
   updateOriginalSources(state, source);
+}
+
+function updateExistingRelativeSource(state: SourcesState, source: Object) {
+  const relativeSources = { ...state.relativeSources };
+
+  for (const thread in relativeSources) {
+    relativeSources[thread] = { ...relativeSources[thread] };
+    if (relativeSources[thread][source.id]) {
+      const existingRelativeSource = relativeSources[thread][source.id];
+      const updatedRelativeSource = { ...existingRelativeSource, ...source};
+      state.relativeSources[thread][source.id] = updatedRelativeSource;
+    }
+  }
 }
 
 function updateRelativeSource(
