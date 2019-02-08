f948f78855ca5266983f7af93d5be4e27c3818fa
Jason Laster <jlaster@mozilla.com>
2019-02-08 10:35:15 -0800

    test

--- src/reducers/sources.js
+++ src/reducers/sources.js
@@ -285,8 +285,8 @@ function updateExistingRelativeSource(state: SourcesState, source: Object) {
   const relativeSources = { ...state.relativeSources };
 
   for (const thread in relativeSources) {
-    relativeSources[thread] = { ...relativeSources[thread] };
     if (relativeSources[thread][source.id]) {
+      relativeSources[thread] = { ...relativeSources[thread] };
       const existingRelativeSource = relativeSources[thread][source.id];
       const updatedRelativeSource = { ...existingRelativeSource, ...source };
       state.relativeSources[thread][source.id] = updatedRelativeSource;
