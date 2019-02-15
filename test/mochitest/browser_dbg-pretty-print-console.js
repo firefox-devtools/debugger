085ccb12e86f7f7c15167e21abdf9e7642a07b8d
Jason Laster <jlaster@mozilla.com>
2019-02-14 22:42:17 -0500

    [sync] Bug 1527122 - Update Debugger Frontend (Release 125). r=dwalsh

--- test/mochitest/browser_dbg-pretty-print-console.js
+++ test/mochitest/browser_dbg-pretty-print-console.js
@@ -1,7 +1,6 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
-
-"use strict";
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 async function waitForConsoleLink(dbg, text) {
   const toolbox = dbg.toolbox;
@@ -30,7 +29,7 @@ add_task(async function() {
   invokeInTab("arithmetic");
 
   info("Switch to console and check message");
-  await waitForConsoleLink(dbg, "math.min.js:3:65");
+  await waitForConsoleLink(dbg, "math.min.js:3:73");
 
   info("Switch back to debugger and pretty-print");
   await dbg.toolbox.selectTool("jsdebugger");
