5cb37f3483484c06491cf07c48983af16e9f42ab
David Walsh <davidwalsh83@gmail.com>
2019-02-22 23:53:00 -0600

    Use existing selectors

--- test/mochitest/browser_dbg-debug-line.js
+++ test/mochitest/browser_dbg-debug-line.js
@@ -1,29 +1,26 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 // Test ensures zombie debug lines do not persist
 // https://github.com/firefox-devtools/debugger/issues/7755
 add_task(async function() {
+  // Load test files
   const dbg = await initDebugger("doc-debug-line.html");
-
-  // Ensure test files load
   await waitForSources(dbg, "debug-line-1.js", "debug-line-2.js");
 
   // Add breakpoint to debug-line-2
   await addBreakpoint(dbg, "debug-line-2.js", 5);
 
-  // Reload
+  // Trigger the breakpoint ane ensure we're paused
   invokeInTab("doThing");
-
-  // Ensure we're paused
   await waitForPaused(dbg);
 
   // Click the call stack to get to debugger-line-1
-  const otherLocation = findAllElementsWithSelector(dbg, ".frame")[1];
-  otherLocation.click();
+  await clickElement(dbg, "frame", 2);
   await waitForSelectedSource(dbg, "debug-line-1.js");
 
-  // Resume
+  // Resume, which ends all pausing and would trigger the problem
   resume(dbg);
 
   // Select the source that had the initial debug line
@@ -31,7 +28,7 @@ add_task(async function() {
 
   // Ensure there's no longer a debug line
   is(
-    findAllElementsWithSelector(dbg, ".new-debug-line").length,
+    findAllElements(dbg, "debugLine").length,
     0,
     "Debug line no longer exists!"
   );
