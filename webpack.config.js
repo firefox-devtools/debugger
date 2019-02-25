9b3edfa06babc658ab046ff1304f5cc49fd37ce6
Jason Laster <jlaster@mozilla.com>
2019-02-25 09:28:34 -0500

    [sync] Bug 1521864 -  It should be possible to open the debugger in a tab. r=davidwalsh

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const webpackConfig = {
   entry: {
     // We always generate the debugger bundle, but we will only copy the CSS
     // artifact over to mozilla-central.
-    debugger: getEntry("src/main.js"),
+    debugger: getEntry(isProduction ? "src/main.js" : "src/main.development.js"),
     "parser-worker": getEntry("src/workers/parser/worker.js"),
     "pretty-print-worker": getEntry("src/workers/pretty-print/worker.js"),
     "search-worker": getEntry("src/workers/search/worker.js"),
