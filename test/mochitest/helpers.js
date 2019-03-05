5c739eb4768dabc366c926426a674573264d07e2
David Walsh <davidwalsh83@gmail.com>
2019-03-04 22:30:50 -0600

    [Preview] Open node from the object inspector in popups (#8069)

--- test/mochitest/helpers.js
+++ test/mochitest/helpers.js
@@ -1163,6 +1163,7 @@ const selectors = {
   popup: ".popover",
   tooltip: ".tooltip",
   previewPopup: ".preview-popup",
+  openInspector: "button.open-inspector",
   outlineItem: i =>
     `.outline-list__element:nth-child(${i}) .function-signature`,
   outlineItems: ".outline-list__element",
@@ -1171,7 +1172,7 @@ const selectors = {
   blackbox: ".action.black-box",
   projectSearchCollapsed: ".project-text-search .arrow:not(.expanded)",
   projectSerchExpandedResults: ".project-text-search .result",
-  threadsPaneItems: `.workers-pane .worker`,
+  threadsPaneItems: ".workers-pane .worker",
   threadsPaneItem: i => `.workers-pane .worker:nth-child(${i})`,
   CodeMirrorLines: ".CodeMirror-lines"
 };
