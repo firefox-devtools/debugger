fb02d557c10a5a51656ef88aa519b556e01a9e70
David Walsh <davidwalsh83@gmail.com>
2019-03-07 17:34:01 -0600

    Sync changes from mozilla central

--- src/components/Editor/Preview/index.js
+++ src/components/Editor/Preview/index.js
@@ -49,11 +49,15 @@ function inPopup(e) {
 }
 
 function getElementFromPos(pos: DOMRect) {
+  // We need to use element*s*AtPoint because the tooltip overlays
+  // the token and thus an undesirable element may be returned
   // $FlowIgnore
-  return document.elementFromPoint(
+  const elementsAtPoint = [...document.elementsFromPoint(
     pos.x + pos.width / 2,
     pos.y + pos.height / 2
-  );
+  )];
+
+  return elementsAtPoint.find(el => el.className.startsWith("cm-"));
 }
 
 class Preview extends PureComponent<Props, State> {
