6b38b755b61489ddad04e62b2d13983d29f71a08
David Walsh <davidwalsh83@gmail.com>
2019-03-11 11:12:44 -0500

    Flow ignore the elementsFromPoint

--- src/components/Editor/Preview/index.js
+++ src/components/Editor/Preview/index.js
@@ -49,8 +49,8 @@ function inPopup(e) {
 function getElementFromPos(pos: DOMRect) {
   // We need to use element*s*AtPoint because the tooltip overlays
   // the token and thus an undesirable element may be returned
-  // $FlowIgnore
   const elementsAtPoint = [
+    // $FlowIgnore
     ...document.elementsFromPoint(pos.x + pos.width / 2, pos.y + pos.height / 2)
   ];
 
