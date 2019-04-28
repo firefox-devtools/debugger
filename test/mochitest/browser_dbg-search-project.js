81f0254affffff89e6157b3ffeadd9d7681aee35
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-04-28 16:18:36 +0200

    chore: Fix tests

--- test/mochitest/browser_dbg-search-project.js
+++ test/mochitest/browser_dbg-search-project.js
@@ -2,12 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-function openProjectSearch(dbg) {
+async function openProjectSearch(dbg) {
   synthesizeKeyShortcut("CmdOrCtrl+Shift+F");
-  return waitForState(
+  await waitForState(
     dbg,
     state => dbg.selectors.getActiveSearch(state) === "project"
   );
+  await waitForTime(0);
 }
 
 function closeProjectSearch(dbg) {
